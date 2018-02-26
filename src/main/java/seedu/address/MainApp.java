f029a93775e5d09a815a893141bd0769a2b07bca
kush1509 <kushagra.goel.15@gmail.com>
2018-02-26 18:57:55 +0800

    set up travis

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -52,7 +52,6 @@ public class MainApp extends Application {
     protected UserPrefs userPrefs;
 
 
-    //Testing
     @Override
     public void init() throws Exception {
         logger.info("=============================[ Initializing AddressBook ]===========================");

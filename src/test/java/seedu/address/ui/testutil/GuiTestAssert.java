4dab68ec4e28bc237dd1f6b4cf851ffba1053519
samuelloh <e0031610@u.nus.edu>
2018-03-05 22:29:30 +0800

    further coding style correction

--- src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
+++ src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
@@ -66,7 +66,7 @@ public class GuiTestAssert {
         case "friends":
             return "cyan";
 
-        case "colleages":
+        case "colleagues":
         case "neighbours":
             return "blue";
 

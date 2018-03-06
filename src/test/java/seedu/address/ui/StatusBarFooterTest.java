3c3fb00cad79c9f5be08bc7ed4d8cd5777ea508a
William Gan <ganweiliang@gmail.com>
2018-03-06 16:33:54 +0800

    Changed order of import statements in StatusBarFooter to match style guide

--- src/test/java/seedu/address/ui/StatusBarFooterTest.java
+++ src/test/java/seedu/address/ui/StatusBarFooterTest.java
@@ -3,9 +3,9 @@ package seedu.address.ui;
 import static org.junit.Assert.assertEquals;
 import static seedu.address.testutil.EventsUtil.postNow;
 import static seedu.address.testutil.TypicalPersons.BENSON;
+import static seedu.address.ui.StatusBarFooter.NUMBER_OF_PEOPLE_STATUS;
 import static seedu.address.ui.StatusBarFooter.SYNC_STATUS_INITIAL;
 import static seedu.address.ui.StatusBarFooter.SYNC_STATUS_UPDATED;
-import static seedu.address.ui.StatusBarFooter.NUMBER_OF_PEOPLE_STATUS;
 
 import java.time.Clock;
 import java.time.Instant;

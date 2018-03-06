593f8c00e116d1603af4e96ddddf68d2adf52e6d
Caijun7 <lohcaijun@gmail.com>
2018-03-07 02:16:24 +0800

    Fix updatePerson() to remove unused tags

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -112,7 +112,20 @@ public class AddressBook implements ReadOnlyAddressBook {
         // This can cause the tags master list to have additional tags that are not tagged to any person
         // in the person list.
         persons.setPerson(target, syncedEditedPerson);
+        removeUnusedTags();
     }
+
+    /**
+     * Removes all {@code tag}s that are not used by any {@code person} in this {@code AddressBook}.
+     */
+    private void removeUnusedTags() {
+        Set<Tag> tagsInPersons = persons.asObservableList().stream()
+                .map(Person::getTags)
+                .flatMap(Set::stream)
+                .collect(Collectors.toSet());
+        tags.setTags(tagsInPersons);
+    }
+
 
     /**
      *  Updates the master tag list to include tags in {@code person} that are not in the list.

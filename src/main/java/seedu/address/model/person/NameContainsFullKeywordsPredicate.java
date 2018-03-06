9d3530b025e0d9efe5edd9614eeb0348fa003138
Ng Jun Wei <ng.95.jun.wei@gmail.com>
2018-03-06 22:49:50 +0800

    Add new predicate class

---
+++ src/main/java/seedu/address/model/person/NameContainsFullKeywordsPredicate.java
@@ -0,0 +1,27 @@
+package seedu.address.model.person;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+public class NameContainsFullKeywordsPredicate  implements Predicate<Person> {
+    private final List<String> keywords;
+
+    public NameContainsFullKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof NameContainsFullKeywordsPredicate // instanceof handles nulls
+                && this.keywords.equals(((NameContainsFullKeywordsPredicate) other).keywords)); // state check
+    }
+
+}

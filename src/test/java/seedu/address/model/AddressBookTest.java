7552f64c155ad213b27796e0743bd960171da7da
trafalgarandre <trafalgarandre@gmail.com>
2018-03-07 00:43:14 +0800

    Fix bugs

--- src/test/java/seedu/address/model/AddressBookTest.java
+++ src/test/java/seedu/address/model/AddressBookTest.java
@@ -14,7 +14,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;

eb9898c78d96912e8f071fbdf958cedadd8f114b
iNekox3 <inekox3@hotmail.com>
2018-03-06 20:36:07 +0800

    PersonCard: add global variable for list of tag colors

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -13,6 +13,8 @@ import seedu.address.model.person.Person;
 public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
+    private static final String[] TAG_COLORS =
+        { "red", "orange", "yellow", "green", "blue", "purple" };
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.

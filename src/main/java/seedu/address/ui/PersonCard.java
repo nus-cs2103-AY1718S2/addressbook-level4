0be6747b95890b9e394e3fa6ebf0765eb1ca1064
WyinKok <wyin.kwy@gmail.com>
2018-03-08 01:13:58 +0800

    Initiate tags

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -50,7 +50,7 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
-        person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        initTags(person);
     }
 
     /**

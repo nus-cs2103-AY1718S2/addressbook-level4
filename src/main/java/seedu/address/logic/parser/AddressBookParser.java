1d1d062116cb00f6602986da35616487ff5f3619
chwee <tancwww@gmail.com>
2018-03-07 09:27:44 +0800

    hi

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -49,7 +49,7 @@ public class AddressBookParser {
         switch (commandWord) {
 
         case AddCommand.COMMAND_WORD:
-            case AddCommand.COMMAND_ALIAS:
+        case AddCommand.COMMAND_ALIAS:
             return new AddCommandParser().parse(arguments);
 
         case EditCommand.COMMAND_WORD:

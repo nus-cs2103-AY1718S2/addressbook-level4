4b951f1dce2ed6201ee9559234dc575efe4a9cf6
chialejing <chialejing@users.noreply.github.com>
2018-03-08 23:04:37 +0800

    Added function description and corrected error

--- src/main/java/seedu/address/storage/StorageManager.java
+++ src/main/java/seedu/address/storage/StorageManager.java
@@ -79,7 +79,7 @@ public class StorageManager extends ComponentManager implements Storage {
 
     @Override
     public void backupAddressBook(ReadOnlyAddressBook addressBook) throws IOException {
-        addressBookStorage.saveAddressBook(addressBook);
+        addressBookStorage.backupAddressBook(addressBook);
     }
 
     @Override

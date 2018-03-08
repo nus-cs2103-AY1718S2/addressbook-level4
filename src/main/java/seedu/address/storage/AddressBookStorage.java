d85fbbe11caeebe550b73df633c55ed2eb8f7313
chengyu <chengyu@xfers.com>
2018-03-09 00:34:05 +0800

    Remove unneeded overloaded method

--- src/main/java/seedu/address/storage/AddressBookStorage.java
+++ src/main/java/seedu/address/storage/AddressBookStorage.java
@@ -47,10 +47,5 @@ public interface AddressBookStorage {
      * @throws IOException if there was any problem writing to the file.
      */
     void backupAddressBook(ReadOnlyAddressBook addressBook) throws IOException;
-
-    /**
-     * @see #backupAddressBook(ReadOnlyAddressBook)
-     */
-    void backupAddressBook(ReadOnlyAddressBook addressBook, String filePath) throws IOException;
-
+    
 }

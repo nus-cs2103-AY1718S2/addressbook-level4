d98da78e0e433a85363e91629f5bb495efe4b440
qiu-siqi <cuteyingmei@gmail.com>
2018-03-09 15:29:37 +0800

    Add UI classes to support books

--- src/main/java/seedu/address/ui/BookCard.java
+++ src/main/java/seedu/address/ui/BookCard.java
@@ -5,15 +5,14 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
-import seedu.address.model.person.Person;
+import seedu.address.model.book.Book;
 
 /**
- * An UI component that displays information of a {@code Person}.
+ * An UI component that displays information of a {@code Book}.
  */
-@Deprecated
-public class PersonCard extends UiPart<Region> {
+public class BookCard extends UiPart<Region> {
 
-    private static final String FXML = "PersonListCard.fxml";
+    private static final String FXML = "BookListCard.fxml";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
@@ -23,32 +22,26 @@ public class PersonCard extends UiPart<Region> {
      * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
      */
 
-    public final Person person;
+    public final Book book;
 
     @FXML
     private HBox cardPane;
     @FXML
-    private Label name;
+    private Label title;
     @FXML
     private Label id;
     @FXML
-    private Label phone;
+    private FlowPane authors;
     @FXML
-    private Label address;
-    @FXML
-    private Label email;
-    @FXML
-    private FlowPane tags;
+    private FlowPane categories;
 
-    public PersonCard(Person person, int displayedIndex) {
+    public BookCard(Book book, int displayedIndex) {
         super(FXML);
-        this.person = person;
+        this.book = book;
         id.setText(displayedIndex + ". ");
-        name.setText(person.getName().fullName);
-        phone.setText(person.getPhone().value);
-        address.setText(person.getAddress().value);
-        email.setText(person.getEmail().value);
-        person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        title.setText(book.getTitle().title);
+        book.getAuthors().forEach(author -> authors.getChildren().add(new Label(author.fullName)));
+        book.getCategories().forEach(category -> categories.getChildren().add(new Label(category.category)));
     }
 
     @Override
@@ -59,13 +52,13 @@ public class PersonCard extends UiPart<Region> {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof PersonCard)) {
+        if (!(other instanceof BookCard)) {
             return false;
         }
 
         // state check
-        PersonCard card = (PersonCard) other;
+        BookCard card = (BookCard) other;
         return id.getText().equals(card.id.getText())
-                && person.equals(card.person);
+                && book.equals(card.book);
     }
 }
--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -20,6 +20,7 @@ import seedu.address.model.person.Person;
 /**
  * Panel containing the list of persons.
  */
+@Deprecated
 public class PersonListPanel extends UiPart<Region> {
     private static final String FXML = "PersonListPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);

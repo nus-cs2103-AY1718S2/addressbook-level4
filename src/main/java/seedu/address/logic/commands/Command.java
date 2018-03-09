286632cb6bfe6d63c62ac251e9ab3f17c501ab0f
qiu-siqi <cuteyingmei@gmail.com>
2018-03-09 12:04:17 +0800

    Rename method in Command

--- src/main/java/seedu/address/logic/commands/Command.java
+++ src/main/java/seedu/address/logic/commands/Command.java
@@ -15,12 +15,12 @@ public abstract class Command {
     protected UndoRedoStack undoRedoStack;
 
     /**
-     * Constructs a feedback message to summarise an operation that displayed a listing of persons.
+     * Constructs a feedback message to summarise an operation that displayed a listing of books.
      *
      * @param displaySize used to generate summary
-     * @return summary message for persons displayed
+     * @return summary message for books displayed
      */
-    public static String getMessageForPersonListShownSummary(int displaySize) {
+    public static String getMessageForBookListShownSummary(int displaySize) {
         return String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, displaySize);
     }
 

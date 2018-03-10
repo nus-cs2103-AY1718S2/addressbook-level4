e19fa018cce96492ebb92cdb2b435b803873dc71
yapni <yapni8@gmail.com>
2018-03-11 00:32:24 +0800

    Implement UnfavouriteCommand methods

--- src/main/java/seedu/address/logic/parser/UnfavouriteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UnfavouriteCommandParser.java
@@ -4,7 +4,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.logic.commands.FavouriteCommand;
 import seedu.address.logic.commands.UnfavouriteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 

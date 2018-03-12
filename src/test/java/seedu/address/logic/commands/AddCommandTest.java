37c98e033c1749581461dc38bf33736c9b0a3ab3
Yoochard <yangyuqinghhh@gmail.com>
2018-03-12 23:54:26 +0800

    This @Override should not delete, but Travis CI reported an error on it

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -101,7 +101,6 @@ public class AddCommandTest {
             fail("This method should not be called.");
         }
 
-        @Override
         public void resetData(ReadOnlyAddressBook newData) {
             fail("This method should not be called.");
         }

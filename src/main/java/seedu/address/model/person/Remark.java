c46958e767be5ad1fe4ce0e005cc6ce86719f7d4
YuanQLLer <yuanquan1995@gmail.com>
2018-03-12 17:26:40 +0800

    alter model

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,55 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a task's remark in the task manager.
+ * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ */
+public class Remark {
+
+    public static final String MESSAGE_REMARK_CONSTRAINTS =
+            "Remark of Tasks can take any values.";
+
+    public static final String REMARK_VALIDATION_REGEX = "*";
+
+    public final String value;
+
+    /**
+     * Constructs an {@code Address}.
+     *
+     * @param remark A valid address.
+     */
+    public Remark(String remark) {
+        requireNonNull(remark);
+        checkArgument(isValidAddress(remark), MESSAGE_REMARK_CONSTRAINTS);
+        this.value = remark;
+    }
+
+    /**
+     * Returns true if a given string is a valid person email.
+     */
+    public static boolean isValidAddress(String test) {
+        return test.matches(REMARK_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Remark // instanceof handles nulls
+                && this.value.equals(((Remark) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
+
---
+++ src/main/java/seedu/address/model/person/Task.java
@@ -0,0 +1,91 @@
+package seedu.address.model.person;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Set;
+
+import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.UniqueTagList;
+
+/**
+ * Represents a Person in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Task {
+
+    private final Name name;
+    private final DateTime dateTime;
+    private final Remark remark;
+
+    private final UniqueTagList tags;
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Task(Name name, DateTime dateTime, Remark remark, Set<Tag> tags) {
+        requireAllNonNull(name, dateTime, remark, tags);
+        this.name = name;
+        this.dateTime = dateTime;
+        this.remark = remark;
+        // protect internal tags from changes in the arg list
+        this.tags = new UniqueTagList(tags);
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public DateTime getDateTime() {
+        return dateTime;
+    }
+
+    public Remark getRemark() {
+        return remark;
+    }
+
+    /**
+     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
+     * if modification is attempted.
+     */
+    public Set<Tag> getTags() {
+        return Collections.unmodifiableSet(tags.toSet());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Person)) {
+            return false;
+        }
+
+        Person otherPerson = (Person) other;
+        return otherPerson.getName().equals(this.getName())
+                && otherPerson.getPhone().equals(this.getDateTime())
+                && otherPerson.getEmail().equals(this.getRemark());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name, dateTime, remark, tags);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getName())
+                .append(" Date and Time: ")
+                .append(getDateTime())
+                .append(" Remark: ")
+                .append(getRemark())
+                .append(" Tags: ");
+        getTags().forEach(builder::append);
+        return builder.toString();
+    }
+
+}

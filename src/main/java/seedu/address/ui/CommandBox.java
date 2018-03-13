e77f2c8fdd38ed7c9107e30eef97455c793a5829
mattbuot <matthieu2301@hotmail.fr>
2018-03-13 16:32:51 +0800

    Autocompletion, think about modularizing

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -1,7 +1,13 @@
 package seedu.address.ui;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
+import com.google.common.collect.Lists;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
@@ -11,7 +17,7 @@ import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.NewResultAvailableEvent;
 import seedu.address.logic.ListElementPointer;
 import seedu.address.logic.Logic;
-import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.*;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -55,6 +61,12 @@ public class CommandBox extends UiPart<Region> {
             keyEvent.consume();
             navigateToNextInput();
             break;
+
+        case TAB:
+            keyEvent.consume();
+            autocomplete();
+            break;
+
         default:
             // let JavaFx handle the keypress
         }
@@ -84,6 +96,99 @@ public class CommandBox extends UiPart<Region> {
         }
 
         replaceText(historySnapshot.next());
+    }
+
+    /**
+     * Tries to complete the last word of the text field
+     */
+    private void autocomplete() {
+        String input = commandTextField.getText();
+        String[] words = input.trim().split(" ");
+        Set<String> possibilities = new HashSet<>();
+
+        //trying to complete a command
+        if(words.length == 1 && words[0].length() > 0){
+            String[] commandsList = {AddCommand.COMMAND_WORD,
+                    ClearCommand.COMMAND_WORD,
+                    DeleteCommand.COMMAND_WORD,
+                    EditCommand.COMMAND_WORD,
+                    ExitCommand.COMMAND_WORD,
+                    FindCommand.COMMAND_WORD,
+                    HelpCommand.COMMAND_WORD,
+                    HistoryCommand.COMMAND_WORD,
+                    ListCommand.COMMAND_WORD,
+                    RedoCommand.COMMAND_WORD,
+                    SelectCommand.COMMAND_WORD,
+                    UndoCommand.COMMAND_WORD};
+
+            for(String command: commandsList){
+                if(command.startsWith(words[0])) {
+                    //we only append the end of the command
+                    possibilities.add(command.substring(words[0].length()));
+                }
+            }
+        }
+        //trying to complete a field
+        else if(words.length > 1) {
+            int lastFieldIndex = input.lastIndexOf('/');
+
+            if(lastFieldIndex > 0 && input.substring(lastFieldIndex).length() > 0){
+                String field = input.substring(lastFieldIndex + 1);
+                List<String> fieldsList;
+
+                switch (input.charAt(lastFieldIndex - 1)) {
+                    case 'n':
+                        fieldsList = logic.getFilteredPersonList().stream().map(person -> person.getName().toString()).collect(Collectors.toList());
+                        break;
+
+                    case 'p':
+                        fieldsList = logic.getFilteredPersonList().stream().map(person -> person.getPhone().toString()).collect(Collectors.toList());
+                        break;
+
+                    case 'e':
+                        fieldsList = logic.getFilteredPersonList().stream().map(person -> person.getEmail().toString()).collect(Collectors.toList());
+                        break;
+
+                    case 'a':
+                        fieldsList = logic.getFilteredPersonList().stream().map(person -> person.getAddress().toString()).collect(Collectors.toList());
+                        break;
+
+                    default:
+                        fieldsList = new ArrayList<>();
+                        break;
+                }
+
+                for(String fieldPossibility: fieldsList){
+
+                    if(fieldPossibility.startsWith(field)) {
+                        //we only append the end of the field
+                        possibilities.add(fieldPossibility.substring(field.length()));
+                    }
+                }
+            }
+        }
+
+        if(possibilities.size() > 0) {
+            String p = (String) possibilities.toArray()[0];
+            String longestCommonPrefix = "";
+
+            for (int i = 0; i < p.length(); i++) {
+                boolean isPrefixOkay = true;
+
+                for (String possibility : possibilities) {
+                    if (!possibility.startsWith(longestCommonPrefix + p.charAt(i))) {
+                        isPrefixOkay = false;
+                    }
+                }
+                if (!isPrefixOkay) {
+                    break;
+                }
+
+                longestCommonPrefix += p.charAt(i);
+            }
+
+            commandTextField.appendText(longestCommonPrefix);
+        }
     }
 
     /**

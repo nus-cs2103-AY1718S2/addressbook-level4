22486364ec82f96305a768a8d1a2627adfdee8e8
demitycho <tanweihao94@gmail.com>
2018-03-14 03:13:43 +0800

    Removed useless code

--- src/test/java/seedu/address/logic/commands/FindTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindTagCommandTest.java
@@ -4,9 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
-import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BENSON;
-import static seedu.address.testutil.TypicalPersons.CARL;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.util.Arrays;

8978d08c8f037814046e4a0551d2334f12f4cf2a
tanhengyeow <E0032242@u.nus.edu>
2018-03-13 18:17:22 +0800

    Fixed checkstyle error for FindCommandParser

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.AddressContainsKeywordsPredicate;
@@ -49,56 +50,58 @@ public class FindCommandParser implements Parser<FindCommand> {
         Predicate<Person> finalPredicate;
         boolean isGlobalSearch = false;
 
-        // no prefix used, search for all fields (global search)
-        if (!startWithPrefix(trimmedArgs)) {
-            isGlobalSearch = true;
-            String[] keywords = trimmedArgs.split("\\s+");
+        try {
+            // no prefix used, search for all fields (global search)
+            if (!startWithPrefix(trimmedArgs)) {
+                isGlobalSearch = true;
+                String[] keywords = trimmedArgs.split("\\s+");
 
-            predicateList.add(new NameContainsKeywordsPredicate(Arrays.asList(keywords)));
-            predicateList.add(new PhoneContainsKeywordsPredicate(Arrays.asList(keywords)));
-            predicateList.add(new EmailContainsKeywordsPredicate(Arrays.asList(keywords)));
-            predicateList.add(new AddressContainsKeywordsPredicate(Arrays.asList(keywords)));
-            predicateList.add(new ExpectedGraduationYearContainsKeywordsPredicate(Arrays.asList(keywords)));
+                predicateList.add(new NameContainsKeywordsPredicate(Arrays.asList(keywords)));
+                predicateList.add(new PhoneContainsKeywordsPredicate(Arrays.asList(keywords)));
+                predicateList.add(new EmailContainsKeywordsPredicate(Arrays.asList(keywords)));
+                predicateList.add(new AddressContainsKeywordsPredicate(Arrays.asList(keywords)));
+                predicateList.add(new ExpectedGraduationYearContainsKeywordsPredicate(Arrays.asList(keywords)));
 
-            finalPredicate = combinePredicates(isGlobalSearch, predicateList.toArray(new Predicate[predicateList.size()]));
+                finalPredicate = combinePredicates(isGlobalSearch,
+                        predicateList.toArray(new Predicate[predicateList.size()]));
+                return new FindCommand(finalPredicate);
+            }
+
+            // at least one prefix is used, search for fields that matches prefix only
+            if (!argMultimap.getPreamble().isEmpty()) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            }
+
+            // checks if prefix is present in argMultimap and adds the corresponding predicate to predicateList
+            if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
+                String[] nameKeywords = argMultimap.getValue(PREFIX_NAME).get().split("\\s+");
+                predicateList.add(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+            }
+            if (argMultimap.getValue(PREFIX_PHONE).isPresent()) {
+                String[] phoneKeywords = argMultimap.getValue(PREFIX_PHONE).get().split("\\s+");
+                predicateList.add(new PhoneContainsKeywordsPredicate(Arrays.asList(phoneKeywords)));
+            }
+            if (argMultimap.getValue(PREFIX_EMAIL).isPresent()) {
+                String[] emailKeywords = argMultimap.getValue(PREFIX_EMAIL).get().split("\\s+");
+                predicateList.add(new EmailContainsKeywordsPredicate(Arrays.asList(emailKeywords)));
+            }
+            if (argMultimap.getValue(PREFIX_ADDRESS).isPresent()) {
+                String[] addressKeywords = argMultimap.getValue(PREFIX_ADDRESS).get().split("\\s+");
+                predicateList.add(new AddressContainsKeywordsPredicate(Arrays.asList(addressKeywords)));
+            }
+            if (argMultimap.getValue(PREFIX_EXPECTED_GRADUATION_YEAR).isPresent()) {
+                String[] expectedGraduationYearKeywords =
+                        argMultimap.getValue(PREFIX_EXPECTED_GRADUATION_YEAR).get().split("\\s+");
+                predicateList.add(new ExpectedGraduationYearContainsKeywordsPredicate(
+                                  Arrays.asList(expectedGraduationYearKeywords)));
+            }
+
+            finalPredicate = combinePredicates(isGlobalSearch,
+                                               predicateList.toArray(new Predicate[predicateList.size()]));
             return new FindCommand(finalPredicate);
+        } catch (IllegalValueException ive) {
+            throw new ParseException(ive.getMessage(), ive);
         }
-
-        // at least one prefix is used, search for fields that matches prefix only
-        if (!argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
-        }
-
-        String[] nameKeywords,
-                phoneKeywords,
-                emailKeywords,
-                addressKeywords,
-                expectedGraduationYearKeywords;
-
-        // checks if prefix is present in argMultimap and adds the corresponding predicate to predicateList
-        if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
-            nameKeywords = argMultimap.getValue(PREFIX_NAME).get().split("\\s+");
-            predicateList.add(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
-        }
-        if (argMultimap.getValue(PREFIX_PHONE).isPresent()) {
-            phoneKeywords = argMultimap.getValue(PREFIX_PHONE).get().split("\\s+");
-            predicateList.add(new PhoneContainsKeywordsPredicate(Arrays.asList(phoneKeywords)));
-        }
-        if (argMultimap.getValue(PREFIX_EMAIL).isPresent()) {
-            emailKeywords = argMultimap.getValue(PREFIX_EMAIL).get().split("\\s+");
-            predicateList.add(new EmailContainsKeywordsPredicate(Arrays.asList(emailKeywords)));
-        }
-        if (argMultimap.getValue(PREFIX_ADDRESS).isPresent()) {
-            addressKeywords = argMultimap.getValue(PREFIX_ADDRESS).get().split("\\s+");
-            predicateList.add(new AddressContainsKeywordsPredicate(Arrays.asList(addressKeywords)));
-        }
-        if (argMultimap.getValue(PREFIX_EXPECTED_GRADUATION_YEAR).isPresent()) {
-            expectedGraduationYearKeywords = argMultimap.getValue(PREFIX_EXPECTED_GRADUATION_YEAR).get().split("\\s+");
-            predicateList.add(new ExpectedGraduationYearContainsKeywordsPredicate(Arrays.asList(expectedGraduationYearKeywords)));
-        }
-
-        finalPredicate = combinePredicates(isGlobalSearch, predicateList.toArray(new Predicate[predicateList.size()]));
-        return new FindCommand(finalPredicate);
     }
 
     /**
@@ -107,25 +110,25 @@ public class FindCommandParser implements Parser<FindCommand> {
     private boolean startWithPrefix(String trimmedArgs) {
         String[] args = trimmedArgs.split("\\s+");
 
-        return (args[0].contains(PREFIX_NAME.toString()) ||
-                args[0].contains(PREFIX_PHONE.toString()) ||
-                args[0].contains(PREFIX_EMAIL.toString()) ||
-                args[0].contains(PREFIX_ADDRESS.toString()) ||
-                args[0].contains(PREFIX_EXPECTED_GRADUATION_YEAR.toString()));
+        return (args[0].contains(PREFIX_NAME.toString())
+                || args[0].contains(PREFIX_PHONE.toString())
+                || args[0].contains(PREFIX_EMAIL.toString())
+                || args[0].contains(PREFIX_ADDRESS.toString())
+                || args[0].contains(PREFIX_EXPECTED_GRADUATION_YEAR.toString()));
     }
 
     /**
-     * Combines all predicates in predicateList that matches the corresponding condition to form finalPredicate
+     * Combines all predicates in {@code predicateList} that matches the
+     * corresponding condition to form {@code finalPredicate}
      * @param predicates in the form of varargs
-     * @return finalPredicate of Predicate<Person>
+     * @return {@code finalPredicate} of type {@code Predicate<Person>}
      */
     private Predicate<Person> combinePredicates(boolean isGlobalSearch, Predicate<Person>... predicates) {
         Predicate<Person> finalPredicate;
 
         if (isGlobalSearch) {
             finalPredicate = Stream.of(predicates).reduce(condition -> false, Predicate::or);
-        }
-        else {
+        } else {
             finalPredicate = Stream.of(predicates).reduce(condition -> true, Predicate::and);
         }
 

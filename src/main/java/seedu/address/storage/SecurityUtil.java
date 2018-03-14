952ae3d271e91bf2003f0b2fc3eae82fee68c41c
Yegg <e0036736@u.nus.edu>
2018-03-15 00:55:27 +0800

    Use password instead of key file

--- src/main/java/seedu/address/storage/SecurityUtil.java
+++ src/main/java/seedu/address/storage/SecurityUtil.java
@@ -4,18 +4,17 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.UnsupportedEncodingException;
 import java.security.GeneralSecurityException;
 import java.security.Key;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 import seedu.address.MainApp;
@@ -28,19 +27,19 @@ import seedu.address.commons.util.StringUtil;
 public class SecurityUtil {
     private static final Logger logger = LogsCenter.getLogger(MainApp.class);
     private static String password = new String("test");
-    private static Key secretAESKey = new SecretKeySpec(password.getBytes(), "AES");
-    private static final String AES_KEY_FILEPATH = "data/key.key";
+    private static byte[] hashedPassword;
+    private static Key secretAESKey;
 
     /**
-     * Encrypts the given file using AES key located at {@code AES_KEY_FILEPATH}
-     * if AES key does not exists, create one {@code loadKey}
+     * Encrypts the given file using AES key created by password.
      *
      * @param file Points to a valid file containing data
      * @throws IOException thrown if cannot open file
      */
     public static void encrypt(File file)throws IOException {
-        loadKey();
         try {
+            hashPassword();
+            secretAESKey = createKey(hashedPassword);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, secretAESKey);
             fileProcessor(cipher, file);
@@ -51,15 +50,15 @@ public class SecurityUtil {
     }
 
     /**
-     * Decrypts the given file using AES key located at {@code AES_KEY_FILEPATH}
-     * if AES key does not exists, create one {@code loadKey}
+     * Decrypts the given file using AES key created by password.
      *
      * @param file Points to a valid file containing data
      * @throws IOException thrown if cannot open file
      */
     public static void decrypt(File file) throws IOException {
-        loadKey();
         try {
+            hashPassword();
+            secretAESKey = createKey(hashedPassword);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.DECRYPT_MODE, secretAESKey);
             fileProcessor(cipher, file);
@@ -70,7 +69,7 @@ public class SecurityUtil {
     }
 
     /**
-     * Decrypts or encrypts at the file using cipher passed
+     * Decrypts or encrypts at the file using cipher passed.
      *
      * @param cipher Encrypts or Decrypts file given mode
      * @param file Points to a valid file containing data
@@ -94,69 +93,29 @@ public class SecurityUtil {
         } catch (BadPaddingException e) {
             logger.severe("ERROR: Wrong password length used " + StringUtil.getDetails(e));
         } catch (IllegalBlockSizeException e) {
-            logger.info("Warning: Text already in plain text ");
-            logger.info("Encrypting");
-            //encrypt(file);
-            //decrypt(file);
+            logger.info("Warning: Text already in plain text.");
         }
     }
 
     /**
-     * Loads the key from the given path located at {@code AES_KEY_FILEPATH}
-     * if AES key does not exists, create one
+     * Hashes the password to meet the required length for AES.
      */
-    private static void loadKey() {
+    private static void hashPassword() throws NoSuchAlgorithmException, IOException {
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
         try {
-            ObjectInputStream ois =
-                    new ObjectInputStream(new FileInputStream(AES_KEY_FILEPATH));
-            secretAESKey = (SecretKey) ois.readObject();
-        } catch (IOException oie) {
-            logger.info("Error reading key from file");
-            createKey();
-        } catch (ClassNotFoundException cnfe) {
-            logger.severe("ERROR: Cannot typecast to class SecretKey " + StringUtil.getDetails(cnfe));
-            createKey();
+            md.update(password.getBytes("UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            logger.severe("UTF-8 not supported, using default but may not be able to decrypt in other computer.");
+            md.update(password.getBytes());
         }
+        hashedPassword = md.digest();
+        hashedPassword = Arrays.copyOf(hashedPassword, 16);
     }
 
     /**
-     * Logs and Creates the key
+     * Use the hash to generate a key.
      */
-    private static void createKey() {
-        logger.info("Creating AES key file");
-        initKey();
-        saveKey();
-    }
-
-    /**
-     * Creates an AES key of 128 bits using {@code KeyGenerator}
-     */
-    private static void initKey() {
-        KeyGenerator keyGen = null;
-        try {
-            keyGen = KeyGenerator.getInstance("AES");
-        } catch (NoSuchAlgorithmException nsae) {
-            logger.severe("ERROR: Cannot generate AES keys " + StringUtil.getDetails(nsae));
-            System.exit(1);
-        }
-        keyGen.init(128);
-        secretAESKey = keyGen.generateKey();
-    }
-    /**
-     * Saves the key at {@code AES_KEY_FILEPATH}
-     * Exits the program if unable to save.
-     */
-    public static void saveKey() {
-
-        try {
-            ObjectOutputStream oos =
-                    new ObjectOutputStream(new FileOutputStream(AES_KEY_FILEPATH));
-            oos.writeObject(secretAESKey);
-            oos.close();
-        } catch (IOException oie) {
-            logger.severe("ERROR: Cannot saving AES key to file " + StringUtil.getDetails(oie));
-            System.exit(1);
-        }
-        logger.info("Public key saved to file " + AES_KEY_FILEPATH);
+    private static Key createKey(byte[] hashedPassword) {
+        return new SecretKeySpec(hashedPassword, "AES");
     }
 }

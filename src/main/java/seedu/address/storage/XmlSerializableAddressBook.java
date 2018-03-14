27e6d7b3b0b3231c910a958d1a945c8fc997e2f1
Kyholmes <karen_tankaiyong@hotmail.com>
2018-03-14 14:06:51 +0800

    modify xml serializable address book to include appointment data

--- src/main/java/seedu/address/storage/XmlSerializableAddressBook.java
+++ src/main/java/seedu/address/storage/XmlSerializableAddressBook.java
@@ -21,6 +21,8 @@ public class XmlSerializableAddressBook {
     private List<XmlAdaptedPerson> persons;
     @XmlElement
     private List<XmlAdaptedTag> tags;
+    @XmlElement
+    private List<XmlAdaptedAppointment> appointments;
 
     /**
      * Creates an empty XmlSerializableAddressBook.
@@ -29,6 +31,7 @@ public class XmlSerializableAddressBook {
     public XmlSerializableAddressBook() {
         persons = new ArrayList<>();
         tags = new ArrayList<>();
+        appointments = new ArrayList<>();
     }
 
     /**
@@ -38,6 +41,8 @@ public class XmlSerializableAddressBook {
         this();
         persons.addAll(src.getPersonList().stream().map(XmlAdaptedPerson::new).collect(Collectors.toList()));
         tags.addAll(src.getTagList().stream().map(XmlAdaptedTag::new).collect(Collectors.toList()));
+        appointments.addAll(src.getAppointmentList().stream().map(XmlAdaptedAppointment::new)
+            .collect(Collectors.toList()));
     }
 
     /**
@@ -54,6 +59,10 @@ public class XmlSerializableAddressBook {
         for (XmlAdaptedPerson p : persons) {
             addressBook.addPerson(p.toModelType());
         }
+
+        for (XmlAdaptedAppointment appt : appointments) {
+            addressBook.addAppointment(appt.toModelType());
+        }
         return addressBook;
     }
 
@@ -68,6 +77,7 @@ public class XmlSerializableAddressBook {
         }
 
         XmlSerializableAddressBook otherAb = (XmlSerializableAddressBook) other;
-        return persons.equals(otherAb.persons) && tags.equals(otherAb.tags);
+        return persons.equals(otherAb.persons) && tags.equals(otherAb.tags)
+                && appointments.equals(otherAb.appointments);
     }
 }

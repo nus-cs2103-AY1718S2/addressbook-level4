100eba09659a3614e1ba75c0b73ca51798d55d9f
LeonidAgarth <pvuhung@gmail.com>
2018-03-14 14:48:09 +0800

    Improve the parsing of ChangeTagColorCommand

--- src/main/java/seedu/address/logic/parser/ChangeTagColorCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ChangeTagColorCommandParser.java
@@ -1,13 +1,28 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.ChangeTagColorCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.tag.Tag;
 
 public class ChangeTagColorCommandParser implements Parser<ChangeTagColorCommand>{
 
     @Override
     public ChangeTagColorCommand parse(String userInput) throws ParseException {
         String[] args = userInput.split(" ");
-        return new ChangeTagColorCommand(args[1].toLowerCase().trim(), args[2].toLowerCase().trim());
+        if (args.length != 3) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ChangeTagColorCommand.MESSAGE_USAGE));
+        }
+        try {
+
+            Tag tag = ParserUtil.parseTag(args[1]);
+            String color = ParserUtil.parseColor((args[2]));
+            return new ChangeTagColorCommand(tag.tagName, color);
+        } catch (IllegalValueException ive) {
+            throw new ParseException(ive.getMessage(), ive);
+        }
     }
 }

28234a154392d7fec72727dba4e8dd5cd60bbaac
mattbuot <matthieu2301@hotmail.fr>
2018-03-14 22:28:33 +0800

    Travis correction, adapting autocompletion to the find command

--- src/main/java/seedu/address/logic/Autocompleter.java
+++ src/main/java/seedu/address/logic/Autocompleter.java
@@ -2,6 +2,7 @@ package seedu.address.logic;
 
 import java.util.*;
 import java.util.stream.Collectors;
+
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.DeleteCommand;
@@ -22,30 +23,19 @@ public class Autocompleter {
 
     private Logic logic;
 
-    private  final List<String> commandsList = Arrays.asList(AddCommand.COMMAND_WORD,
-                ClearCommand.COMMAND_WORD,
-                DeleteCommand.COMMAND_WORD,
-                EditCommand.COMMAND_WORD,
-                ExitCommand.COMMAND_WORD,
-                FindCommand.COMMAND_WORD,
-                HelpCommand.COMMAND_WORD,
-                HistoryCommand.COMMAND_WORD,
-                ListCommand.COMMAND_WORD,
-                RedoCommand.COMMAND_WORD,
-                SelectCommand.COMMAND_WORD,
-                UndoCommand.COMMAND_WORD);
-    private List<String> names;
-    private List<String> phones;
-    private List<String> emails;
-    private List<String> adresses;
+    private final Set<String> commandsList = new HashSet<>();
+    private Set<String> names;
+    private Set<String> phones;
+    private Set<String> emails;
+    private Set<String> adresses;
 
     public Autocompleter(Logic logic) {
         this.logic = logic;
+        updateCommands();
         updateFields();
     }
 
     /**
-     *
      * @param input the user query
      * @return the longest possible prefix to append to the last word of the query
      */
@@ -57,18 +47,14 @@ public class Autocompleter {
 
         //trying to complete a command
         if (words.length == 1 && words[0].length() > 0) {
-            for (String command : commandsList) {
-                if (command.startsWith(words[0])) {
-                    //we only append the end of the command
-                    possibilities.add(command.substring(words[0].length()));
-                }
-            }
+            possibilities = generatePossibleSuffixes(words[0], commandsList);
         } else if (words.length > 1) { //trying to complete a field
-            int lastFieldIndex = input.lastIndexOf('/');
 
+            int lastFieldIndex = input.lastIndexOf('/');
+            //testing wether the field is an option starting by 'x/'
             if (lastFieldIndex > 0 && input.substring(lastFieldIndex).length() > 0) {
                 String field = input.substring(lastFieldIndex + 1);
-                List<String> fieldsList;
+                Set<String> fieldsList;
 
                 switch (input.charAt(lastFieldIndex - 1)) {
                     case 'n':
@@ -88,16 +74,16 @@ public class Autocompleter {
                         break;
 
                     default:
-                        fieldsList = new ArrayList<>();
+                        fieldsList = new HashSet<>();
                         break;
                 }
 
-                for (String fieldPossibility : fieldsList) {
-                    if (fieldPossibility.startsWith(field)) {
-                        //we only append the end of the field
-                        possibilities.add(fieldPossibility.substring(field.length()));
-                    }
-                }
+                possibilities = generatePossibleSuffixes(field, fieldsList);
+            } else {
+                //the last word of the query doesn't correspond to a command and doesn't correspond to an option starting with 'x/'
+                //so we're trying to match the last word to a name of the adress book
+
+                possibilities = generatePossibleSuffixes(words[words.length-1], names);
             }
         }
         String longestCommonPrefix = "";
@@ -123,14 +109,41 @@ public class Autocompleter {
         return longestCommonPrefix;
     }
 
+    private Set<String> generatePossibleSuffixes(String lastWord, Set<String> possibleWords) {
+        Set<String> suffixes = new HashSet<>();
+
+        for (String word : possibleWords) {
+            if (word.startsWith(lastWord)) {
+                //we only append the end of the command
+                suffixes.add(word.substring(lastWord.length()));
+            }
+        }
+        return suffixes;
+    }
+
     private void updateFields() {
         names = logic.getFilteredPersonList().stream()
-                .map(person -> person.getName().toString()).collect(Collectors.toList());
+                .map(person -> person.getName().toString()).collect(Collectors.toSet());
         phones = logic.getFilteredPersonList().stream()
-                .map(person -> person.getPhone().toString()).collect(Collectors.toList());
+                .map(person -> person.getPhone().toString()).collect(Collectors.toSet());
         emails = logic.getFilteredPersonList().stream()
-                .map(person -> person.getEmail().toString()).collect(Collectors.toList());
+                .map(person -> person.getEmail().toString()).collect(Collectors.toSet());
         adresses = logic.getFilteredPersonList().stream()
-                .map(person -> person.getAddress().toString()).collect(Collectors.toList());
+                .map(person -> person.getAddress().toString()).collect(Collectors.toSet());
+    }
+
+    private void updateCommands() {
+        commandsList.addAll(Arrays.asList(AddCommand.COMMAND_WORD,
+                ClearCommand.COMMAND_WORD,
+                DeleteCommand.COMMAND_WORD,
+                EditCommand.COMMAND_WORD,
+                ExitCommand.COMMAND_WORD,
+                FindCommand.COMMAND_WORD,
+                HelpCommand.COMMAND_WORD,
+                HistoryCommand.COMMAND_WORD,
+                ListCommand.COMMAND_WORD,
+                RedoCommand.COMMAND_WORD,
+                SelectCommand.COMMAND_WORD,
+                UndoCommand.COMMAND_WORD));
     }
 }

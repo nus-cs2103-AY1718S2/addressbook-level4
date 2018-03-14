c555a59c40e5ce69e804bc0aedb79df9d5e3ff50
Jason Manson-Hing <jason.mansonhing@gmail.com>
2018-03-14 23:57:11 +0800

    Added keyboard shortcupt to open calendar; Cleaned up skeleton code

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -51,6 +51,9 @@ public class MainWindow extends UiPart<Stage> {
     private MenuItem helpMenuItem;
 
     @FXML
+    private MenuItem viewCalendarMenuItem;
+
+    @FXML
     private StackPane personListPanelPlaceholder;
 
     @FXML
@@ -82,6 +85,7 @@ public class MainWindow extends UiPart<Stage> {
 
     private void setAccelerators() {
         setAccelerator(helpMenuItem, KeyCombination.valueOf("F1"));
+        setAccelerator(viewCalendarMenuItem, KeyCombination.valueOf("F8"));
     }
 
     /**

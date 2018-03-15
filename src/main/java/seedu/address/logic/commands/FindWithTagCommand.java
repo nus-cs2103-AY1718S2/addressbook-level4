669b6cd1c392761a0c5279681dcd905ac68d674b
Chuang Cheng Heng <mukevin66@gmail.com>
2018-03-15 19:40:07 +0800

    FindWithTagCommand.java: Implement FindWithTagCommand.

--- src/main/java/seedu/address/logic/commands/FindWithTagCommand.java
+++ src/main/java/seedu/address/logic/commands/FindWithTagCommand.java
@@ -1,4 +1,30 @@
-package seedu.address.logic.commands;
+package seedu.address.model.person;
 
-public class FindWithTagCommand {
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+
+/**
+ * Tests that a {@code Person}'s {@code Tags} matches any of the keywords given.
+ */
+public class TagContainsKeywordsPredicate implements Predicate<Person> {
+    private final List<String> keywords;
+
+    public TagContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getTagsAsString().trim(), keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof TagContainsKeywordsPredicate // instanceof handles nulls
+                && this.keywords.equals(((TagContainsKeywordsPredicate) other).keywords)); // state check
+    }
 }

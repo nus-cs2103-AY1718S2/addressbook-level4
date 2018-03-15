6898c0b5b47543ad3418534613344c7a668d392a
Jacqueline Cheong <jacquelinecjn@gmail.com>
2018-03-15 22:17:15 +0800

    Add command and parser for Change Theme

---
+++ src/main/java/seedu/address/logic/commands/ChangeThemeCommand.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.model.theme.Theme;
+
+//@@author aquarinte
+/**
+ * Change the theme of Medeina
+ */
+public class ChangeThemeCommand extends Command {
+    public static final String COMMAND_WORD = "theme";
+    public static final String COMMAND_ALIAS = "t";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Change Medeina's theme to the"
+            + "specified theme name (case-insensitive)\n"
+            + "Parameters: THEME NAME\n"
+            + "Example: " + COMMAND_WORD + " light";
+
+    private final Theme theme;
+
+    public ChangeThemeCommand(Theme theme) {
+        requireNonNull(theme);
+        this.theme = theme;
+    }
+
+    @Override
+    public CommandResult execute() {
+        String result = ui.changeTheme(theme);
+        return new CommandResult(result);
+    }
+}
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -7,6 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.ChangeThemeCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
@@ -95,6 +96,10 @@ public class AddressBookParser {
         case RedoCommand.COMMAND_ALIAS:
         case RedoCommand.COMMAND_WORD:
             return new RedoCommand();
+
+        case ChangeThemeCommand.COMMAND_ALIAS:
+        case ChangeThemeCommand.COMMAND_WORD:
+            return new ChangeThemeCommandParser().parse(arguments);
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);

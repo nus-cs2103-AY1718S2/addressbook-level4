55e6f0322880875bcb596ebe80c045201210e3a5
Chu Qing Hao <6337103+qinghao1@users.noreply.github.com>
2018-03-15 14:55:39 +0800

    Add basic order class (no tests) and util triple class

---
+++ src/main/java/seedu/address/model/order/Order.java
@@ -0,0 +1,76 @@
+package seedu.address.model.order;
+
+import seedu.address.model.util.Triple;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+/**
+ * Represents a customer's order.
+ * Guarantees: details are present and not null, field values are validated, immutable
+ */
+
+public class Order {
+    private final int personId;
+    private final int id;
+    private final LocalDateTime time;
+    private final ArrayList<Triple<Integer, Integer, Money>> orderList;
+
+    // TODO save this in file when saving orders
+    private static int orderCounter = 0;
+
+    /** Every field must be present and not null.
+     * @param personId id of person (customer) who made the order. Can be thought of as a foreign key
+     * @param orderList ArrayList of triple(product id, number bought, price) to represent the order
+     */
+    public Order(int personId, ArrayList<Triple<Integer, Integer, Money>> orderList) {
+        this.id = ++orderCounter;
+        this.time = LocalDateTime.now();
+        this.personId = personId;
+        this.orderList = orderList;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public ArrayList<Triple<Integer, Integer, Money>> getOrderList() {
+        return orderList;
+    }
+
+    /**
+     * Calculates total price (sum) of an order
+     * @return total price
+     */
+    public Money getOrderTotal() {
+        Money total = new Money();
+        for(Triple<Integer, Integer, Money> product : orderList) {
+            Money price = product.getThird();
+            int numItems = product.getSecond();
+            Money itemTotal = price.multiply(numItems);
+            total = total.add(itemTotal);
+        }
+        return total;
+    }
+
+    /**
+     * Returns a table in string format which is the order summary (product name, number bought, price, total price)
+     * @return
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(Triple<Integer, Integer, Money> product : orderList) {
+            // TODO get product by id, and convert to string
+        }
+        return sb.toString();
+    }
+}

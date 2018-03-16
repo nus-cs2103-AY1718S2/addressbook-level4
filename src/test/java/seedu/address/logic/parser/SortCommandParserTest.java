47b6f8ce0be03645e1e2dd733045005a1ef5f41e
Yoochard <yangyuqinghhh@gmail.com>
2018-03-16 17:11:00 +0800

    fix CheckStyle error

--- src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
@@ -1,10 +1,11 @@
 package seedu.address.logic.parser;
 
-import org.junit.Test;
-import seedu.address.logic.commands.SortCommand;
-
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.SortCommand;
 
 /**
  * Test scope: To test SortCommandParser.

50e7730183c90d0253942456712c5b2132649de4
chenchongsong <chenchongsong@outlook.com>
2018-03-18 12:17:44 +0800

    small change

--- src/test/java/seedu/address/logic/commands/RemoveTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemoveTagCommandTest.java
@@ -23,7 +23,7 @@ import seedu.address.testutil.EditPersonDescriptorBuilder;
 import seedu.address.testutil.PersonBuilder;
 
 /**
- * Contains integration tests (interaction with the Model, UndoCommand and RedoCommand) and unit tests for EditCommand.
+ * Contains integration test (interaction with the Model, UndoCommand and RedoCommand) and unit tests for EditCommand.
  */
 public class RemoveTagCommandTest {
 

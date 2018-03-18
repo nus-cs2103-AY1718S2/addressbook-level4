3eee29855ba56b3d0e276a4b64873c06acd02e6b
Kyholmes <karen_tankaiyong@hotmail.com>
2018-03-18 13:11:33 +0800

    fix checkstyle error in DeleteCommandSystemTest

--- src/test/java/systemtests/DeleteCommandSystemTest.java
+++ src/test/java/systemtests/DeleteCommandSystemTest.java
@@ -73,7 +73,7 @@ public class DeleteCommandSystemTest extends ImdbSystemTest {
         command = DeleteCommand.COMMAND_WORD + " " + invalidIndex;
         assertCommandFailure(command, MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
 
-        /* --------------------- Performing delete operation while a patient card is selected ------------------------ */
+        /* --------------------- Performing delete operation while a patient card is selected ----------------------- */
 
         /* Case: delete the selected patient -> patient list panel selects the patient before the deleted patient */
         showAllPersons();

0e79b77cff9519a80a82185ed6b1291e2693b383
glorialaw <gloria.law@u.nus.edu>
2018-03-19 21:49:19 +0800

    fixed style

--- src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
@@ -9,6 +9,7 @@ import seedu.address.model.person.Address;
 import seedu.address.model.person.CustTimeZone;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
 

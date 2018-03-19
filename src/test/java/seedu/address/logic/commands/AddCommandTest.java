b0c3fe838e877d20f56771740466d566fb8a7f84
nhatquang3112 <trannhatquang3112@gmail.com>
2018-03-19 23:31:58 +0800

    fix modelScrub bug

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -26,6 +26,8 @@ import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.TagNotFoundException;
+import seedu.address.model.todo.ToDo;
+import seedu.address.model.todo.exceptions.DuplicateToDoException;
 import seedu.address.testutil.PersonBuilder;
 
 public class AddCommandTest {
@@ -100,6 +102,11 @@ public class AddCommandTest {
     private class ModelStub implements Model {
         @Override
         public void addPerson(Person person) throws DuplicatePersonException {
+            fail("This method should not be called.");
+        }
+
+        @Override
+        public void addToDo(ToDo todo) throws DuplicateToDoException {
             fail("This method should not be called.");
         }
 

8937e096a5ed210726e373e4141845df4688498a
Kyholmes <karen_tankaiyong@hotmail.com>
2018-03-19 18:17:42 +0800

    add addPatientToQueue method in addCommandTest

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -139,6 +139,11 @@ public class AddCommandTest {
         public void updateFilteredPersonList(Predicate<Patient> predicate) {
             fail("This method should not be called.");
         }
+
+        @Override
+        public void addPatientToQueue(Patient patient) throws DuplicatePatientException {
+            //update later
+        }
     }
 
     /**

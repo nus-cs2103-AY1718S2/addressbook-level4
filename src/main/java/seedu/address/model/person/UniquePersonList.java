61d370cdc5998c213cbac36553689b7eb91c7e46
XavierMaYuqian <32877925+XavierMaYuqian@users.noreply.github.com>
2018-03-19 22:59:44 +0800

    Update UniquePersonList.java

--- src/main/java/seedu/address/model/person/UniquePersonList.java
+++ src/main/java/seedu/address/model/person/UniquePersonList.java
@@ -95,8 +95,7 @@ public class UniquePersonList implements Iterable<Person> {
     }
 
     /**
-     * Sort all persons based on alphabetical order of their full names.
-     *
+     * Sort all persons based on alphabetical order of their full names
      */
     public void sort() {
         internalList.sort((personA, personB) -> (personA.getName().fullName.toLowerCase().compareTo(personB.getName().fullName.toLowerCase())));

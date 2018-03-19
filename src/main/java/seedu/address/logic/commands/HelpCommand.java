13f7f767d19a3d87059849c7b2c54a4740afb2b0
Kyomian <e0031779@u.nus.edu>
2018-03-19 17:04:31 +0800

    Commented out the Commands in DeskBoardParser and HelpCommand

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -1,42 +1,42 @@
-//package seedu.address.logic.commands;
-//
-//import seedu.address.commons.core.EventsCenter;
-//import seedu.address.commons.events.ui.ShowHelpRequestEvent;
-//
-///**
-// * Format full help instructions for every command for display or command requested
-// */
-//public class HelpCommand extends Command {
-//
-//    public static final String COMMAND_ALIAS = "man";
-//    public static final String COMMAND_WORD = "help";
-//
-//    public static final String MESSAGE_USAGE = COMMAND_WORD + "/" + COMMAND_ALIAS + ": "
-//            + "Shows program usage instructions.\n"  + "Example: " + COMMAND_WORD;
-//
-//    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
-//    private String commandRequest = null;
-//
-//
-//    //@@author jasmoon
-//    public HelpCommand()    {
-//    }
-//
-//    public HelpCommand(String args) {
-//        commandRequest = args.trim();
-//    }
-//
-//    @Override
-//    public CommandResult execute() {
-//        if (commandRequest == null)   {
-//            EventsCenter.getInstance().post(new ShowHelpRequestEvent());
-//            return new CommandResult(SHOWING_HELP_MESSAGE);
-//
-//        } else   {
-//            switch(commandRequest) {
-//
-//            case TaskCommand.COMMAND_WORD:
-//                return new CommandResult(TaskCommand.MESSAGE_USAGE);
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.events.ui.ShowHelpRequestEvent;
+
+/**
+ * Format full help instructions for every command for display or command requested
+ */
+public class HelpCommand extends Command {
+
+    public static final String COMMAND_ALIAS = "man";
+    public static final String COMMAND_WORD = "help";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + "/" + COMMAND_ALIAS + ": "
+            + "Shows program usage instructions.\n"  + "Example: " + COMMAND_WORD;
+
+    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+    private String commandRequest = null;
+
+
+    //@@author jasmoon
+    public HelpCommand()    {
+    }
+
+    public HelpCommand(String args) {
+        commandRequest = args.trim();
+    }
+
+    @Override
+    public CommandResult execute() {
+        if (commandRequest == null)   {
+            EventsCenter.getInstance().post(new ShowHelpRequestEvent());
+            return new CommandResult(SHOWING_HELP_MESSAGE);
+
+        } else   {
+            switch(commandRequest) {
+
+            case TaskCommand.COMMAND_WORD:
+                return new CommandResult(TaskCommand.MESSAGE_USAGE);
 //
 //            case EditCommand.COMMAND_WORD:
 //                return new CommandResult(EditCommand.MESSAGE_USAGE);
@@ -49,14 +49,14 @@
 //
 //            case FindCommand.COMMAND_WORD:
 //                return new CommandResult(FindCommand.MESSAGE_USAGE);
-//
-//            default:
-//                return new CommandResult(MESSAGE_USAGE);
-//            }
-//        }
-//    }
-//
-//    public String getCommandRequest() {
-//        return commandRequest;
-//    }
-//}
+
+            default:
+                return new CommandResult(MESSAGE_USAGE);
+            }
+        }
+    }
+
+    public String getCommandRequest() {
+        return commandRequest;
+    }
+}

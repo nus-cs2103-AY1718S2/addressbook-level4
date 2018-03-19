c149f9e534a9b29bda0d52a2f26d8939070f3c08
luca590 <lucasgaylord@protonmail.com>
2018-03-20 02:02:26 +0800

    temporary tag set, change later

--- src/main/java/seedu/address/logic/commands/ImportContactsCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportContactsCommand.java
@@ -1,10 +1,19 @@
 package seedu.address.logic.commands;
 import seedu.address.logic.commands.exceptions.CommandException;
 import static java.util.Objects.requireNonNull;
+import java.util.Set;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.UniqueTagList;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -12,11 +21,10 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-
+//This class borrows from https://www.callicoder.com/java-read-write-csv-file-apache-commons-csv/
+// and https://github.com/callicoder/java-read-write-csv-file
 
 public class ImportContactsCommand extends Command {
-
-    private String fileAddress;
 
     public static final String COMMAND_WORD = "import_contacts"; //is there a list of added commands?
     public static final String COMMAND_ALIAS = "ic"; //is there a list of aliases??
@@ -30,6 +38,10 @@ public class ImportContactsCommand extends Command {
             + "from which to import the file\n";
 
 
+    private String fileAddress;
+    private CSVParser csvParser;
+
+
     public ImportContactsCommand(String _fileAddress) throws IOException {
         requireNonNull(_fileAddress); //This throws IOException if _fileAddress is null
         fileAddress = _fileAddress;
@@ -38,7 +50,7 @@ public class ImportContactsCommand extends Command {
     public CommandResult openFile() throws IOException, CommandException {
         try {
             Reader reader = Files.newBufferedReader(Paths.get(fileAddress));
-            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
+            csvParser = new CSVParser(reader, CSVFormat.DEFAULT
                     .withFirstRecordAsHeader()
                     .withIgnoreHeaderCase()
                     .withTrim());
@@ -56,37 +68,44 @@ public class ImportContactsCommand extends Command {
     }
 
 
-    try (
-    Reader reader = Files.newBufferedReader(Paths.get(fileAddress));
-    CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
-            .withFirstRecordAsHeader()
-            .withIgnoreHeaderCase()
-            .withTrim());
-        ) {
-        Iterable<CSVRecord> csvRecords = csvParser.getRecords();
-
-        for (CSVRecord csvRecord : csvRecords) {
-            // Accessing values by Header names
-
-            String name = csvRecord.get("Name");
-            String email = csvRecord.get("Email");
-            String phone = csvRecord.get("Phone");
-            String country = csvRecord.get("Country");
-
-            System.out.println("Record No - " + csvRecord.getRecordNumber());
-            System.out.println("---------------");
-            System.out.println("Name : " + name);
-            System.out.println("Email : " + email);
-            System.out.println("Phone : " + phone);
-            System.out.println("Country : " + country);
-            System.out.println("---------------\n\n");
-        }
-
+    public void printResult(String n, String e, String p, String a) {
+        System.out.println("---------------");
+        System.out.println("Name : " + n);
+        System.out.println("Email : " + e);
+        System.out.println("Phone : " + p);
+        System.out.println("Address : " + a);
+        System.out.println("---------------\n\n");
+    }
 
     @Override
-    public CommandResult executeUndoableCommand() throws CommandException {
+    public CommandResult execute() throws CommandException {
+        Person personToAdd;
 
+        try {
+            Iterable<CSVRecord> csvRecords = csvParser.getRecords(); //get iterator to go through records in csv
+
+            for (CSVRecord csvRecord : csvRecords) { //iterate through the
+                // Accessing values by Header names
+                String name = csvRecord.get("Name");
+                Name n = new Name (name);
+                String email = csvRecord.get("Email");
+                String phone = csvRecord.get("Phone");
+                String address = csvRecord.get("Address");
+
+                printResult(name, email, phone, address);
+
+                Set<Tag> tagSet = (Set<Tag>) new Tag("friend"); //temporary tag, fix later
+
+                personToAdd = new Person(new Name(name), new Phone(phone), new Email(email),
+                        new Address(address), new tagSet);
+
+            }
+            return new CommandResult(MESSAGE_SUCCESS);
+        }
+        catch (IOException ioe) {
+            throw new CommandException("Aw Fuck.");
+        }
     }
-*/
+
 }
 

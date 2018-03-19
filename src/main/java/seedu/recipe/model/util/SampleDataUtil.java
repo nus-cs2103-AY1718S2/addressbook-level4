d16488fd51ad845710253b87e298488dd4a19269
RyanAngJY <ryanang.jy@gmail.com>
2018-03-19 16:14:33 +0800

    Added Url support for SampleDataUtil.java

--- src/main/java/seedu/recipe/model/util/SampleDataUtil.java
+++ src/main/java/seedu/recipe/model/util/SampleDataUtil.java
@@ -9,6 +9,7 @@ import seedu.recipe.model.recipe.Ingredient;
 import seedu.recipe.model.recipe.Instruction;
 import seedu.recipe.model.recipe.Name;
 import seedu.recipe.model.recipe.PreparationTime;
+import seedu.recipe.model.recipe.Url;
 import seedu.recipe.model.recipe.Recipe;
 import seedu.recipe.model.recipe.exceptions.DuplicateRecipeException;
 import seedu.recipe.model.tag.Tag;
@@ -22,27 +23,38 @@ public class SampleDataUtil {
             new Recipe(new Name("Alex Yeoh"), new PreparationTime("87438807"),
                     new Ingredient("alexyeoh@example.com"),
                     new Instruction("Blk 30 Geylang Street 29, #06-40"),
-                getTagSet("friends")),
+                    new Url("https://www.allrecipes.com/recipe/73634/colleens-slow-cooker-jambalaya/"),
+                    getTagSet("friends")),
             new Recipe(new Name("Bernice Yu"), new PreparationTime("99272758"),
                     new Ingredient("berniceyu@example.com"),
                     new Instruction("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getTagSet("colleagues", "friends")),
+                    new Url("https://www.allrecipes.com/recipe/8722/mexican-chicken-i/?"
+                            + "internalSource=popular&referringContentType=home%20page&clickId=cardslot%203"),
+                    getTagSet("colleagues", "friends")),
             new Recipe(new Name("Charlotte Oliveiro"), new PreparationTime("93210283"),
                     new Ingredient("charlotte@example.com"),
                     new Instruction("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                getTagSet("neighbours")),
+                    new Url("https://www.allrecipes.com/recipe/11901/to-die-for-fettuccini-alfredo/?"
+                            + "internalSource=popular&referringContentType=home%20page&clickId=cardslot%206"),
+                    getTagSet("neighbours")),
             new Recipe(new Name("David Li"), new PreparationTime("91031282"),
                     new Ingredient("lidavid@example.com"),
                     new Instruction("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getTagSet("family")),
+                    new Url("https://www.allrecipes.com/recipe/229110/savory-beef-stir-fry/?"
+                            + "internalSource=popular&referringContentType=home%20page&clickId=cardslot%2014"),
+                    getTagSet("family")),
             new Recipe(new Name("Irfan Ibrahim"), new PreparationTime("92492021"),
                     new Ingredient("irfan@example.com"),
                     new Instruction("Blk 47 Tampines Street 20, #17-35"),
-                getTagSet("classmates")),
+                    new Url("https://www.allrecipes.com/recipe/222615/scrambled-egg-brunch-bread/?"
+                            + "internalSource=popular&referringContentType=home%20page&clickId=cardslot%2020"),
+                    getTagSet("classmates")),
             new Recipe(new Name("Roy Balakrishnan"), new PreparationTime("92624417"),
                     new Ingredient("royb@example.com"),
                     new Instruction("Blk 45 Aljunied Street 85, #11-31"),
-                getTagSet("colleagues"))
+                    new Url("https://www.allrecipes.com/recipe/15917/fudge-truffle-cheesecake/?"
+                            + "internalSource=popular&referringContentType=home%20page&clickId=cardslot%2022"),
+                    getTagSet("colleagues"))
         };
     }
 

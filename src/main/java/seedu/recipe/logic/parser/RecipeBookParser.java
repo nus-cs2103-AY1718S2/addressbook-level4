60e68b27d3b22a2bae5d34a0830f49cd74cedf31
nicholasang95 <nicholasang67@gmail.com>
2018-03-19 17:12:14 +0800

    fixed some merge conflicts

--- src/main/java/seedu/recipe/logic/parser/RecipeBookParser.java
+++ src/main/java/seedu/recipe/logic/parser/RecipeBookParser.java
@@ -6,23 +6,6 @@ import static seedu.recipe.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-<<<<<<< HEAD:src/main/java/seedu/address/logic/parser/AddressBookParser.java
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.HistoryCommand;
-import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.RedoCommand;
-import seedu.address.logic.commands.SelectCommand;
-import seedu.recipe.logic.commands.TagCommand;
-import seedu.address.logic.commands.UndoCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
-=======
 import seedu.recipe.logic.commands.AddCommand;
 import seedu.recipe.logic.commands.ClearCommand;
 import seedu.recipe.logic.commands.Command;
@@ -35,9 +18,9 @@ import seedu.recipe.logic.commands.HistoryCommand;
 import seedu.recipe.logic.commands.ListCommand;
 import seedu.recipe.logic.commands.RedoCommand;
 import seedu.recipe.logic.commands.SelectCommand;
+import seedu.recipe.logic.commands.TagCommand;
 import seedu.recipe.logic.commands.UndoCommand;
 import seedu.recipe.logic.parser.exceptions.ParseException;
->>>>>>> master:src/main/java/seedu/recipe/logic/parser/RecipeBookParser.java
 
 /**
  * Parses user input.

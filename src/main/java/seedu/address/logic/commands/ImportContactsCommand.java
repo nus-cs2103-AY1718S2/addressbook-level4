f05b5c95461756d76bc8a24d43b1e78f91ecb109
luca590 <lucasgaylord@protonmail.com>
2018-03-20 02:49:48 +0800

    alphabetical order shit

--- src/main/java/seedu/address/logic/commands/ImportContactsCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportContactsCommand.java
@@ -1,16 +1,4 @@
 package seedu.address.logic.commands;
-import seedu.address.logic.commands.exceptions.CommandException;
-import static java.util.Objects.requireNonNull;
-import java.util.Set;
-
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-
-import seedu.address.model.person.*;
-import seedu.address.model.person.exceptions.DuplicatePersonException;
-import seedu.address.model.tag.Tag;
-import seedu.address.model.tag.UniqueTagList;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -18,7 +6,20 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
+
 import java.util.Date;
+import static java.util.Objects.requireNonNull;
+import java.util.Set;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.person.*;
+import seedu.address.model.tag.Tag;
+
+
 
 //This class borrows from https://www.callicoder.com/java-read-write-csv-file-apache-commons-csv/
 // and https://github.com/callicoder/java-read-write-csv-file

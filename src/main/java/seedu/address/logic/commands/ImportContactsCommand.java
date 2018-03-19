b3ea307cb551ea8db5d9d855b52faecd3bc7f126
luca590 <lucasgaylord@protonmail.com>
2018-03-20 03:39:21 +0800

    fixing stupid checkstyle... again

--- src/main/java/seedu/address/logic/commands/ImportContactsCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportContactsCommand.java
@@ -15,11 +15,11 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.person.Person;
 import seedu.address.model.person.DateAdded;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
 

1e65eb5098d0be92be27fa92984af486eabbdf83
Kyholmes <karen_tankaiyong@hotmail.com>
2018-03-19 22:43:22 +0800

    add new test in AddPatientQueueCommandTest

--- src/test/java/seedu/address/logic/commands/AddPatientQueueCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddPatientQueueCommandTest.java
@@ -1,15 +1,20 @@
 package seedu.address.logic.commands;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.testutil.TypicalPatients.getTypicalAddressBook;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -54,4 +59,25 @@ public class AddPatientQueueCommandTest {
         // different patient -> returns false
         assertFalse(addQueueFirstCommand.equals(addQueueSecondCommand));
     }
+
+    @Test
+    public void execute_zeroKeywords_noPersonFound() throws Exception {
+        String expectedMessage = String.format(AddPatientQueueCommand.MESSAGE_PERSON_NOT_FOUND);
+        AddPatientQueueCommand command = prepareCommand(" ");
+        thrown.expect(CommandException.class);
+        thrown.expectMessage(AddPatientQueueCommand.MESSAGE_PERSON_NOT_FOUND);
+
+        command.executeUndoableCommand();
+    }
+
+    /**
+     * Parses {@code userInput} into a {@code AddPatientQueueCommand}.
+     */
+    private AddPatientQueueCommand prepareCommand(String userInput) {
+        AddPatientQueueCommand command =
+                new AddPatientQueueCommand(new NameContainsKeywordsPredicate(Arrays.asList(userInput.split("\\s+"))));
+        command.setData(model, new CommandHistory(), new UndoRedoStack());
+        return command;
+    }
+
 }

f1c5529c81e499838162b5aad3a15af9450e32f4
Liwen <e0134124@u.nus.edu>
2018-03-21 03:01:14 +0800

    add a uploadCommandParser method

--- src/main/java/seedu/progresschecker/logic/parser/UploadCommandParser.java
+++ src/main/java/seedu/progresschecker/logic/parser/UploadCommandParser.java
@@ -1,4 +1,29 @@
 package seedu.progresschecker.logic.parser;
 
-public class UploadCommandParser {
+import static seedu.progresschecker.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.nio.file.Paths;
+
+import seedu.progresschecker.logic.commands.UploadCommand;
+import seedu.progresschecker.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new UploadCommand object
+ */
+public class UploadCommandParser  implements Parser<UploadCommand>  {
+    /**
+     * Parses the given {@code String} of arguments in the context of the UploadCommand
+     * and returns an UploadCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public UploadCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, UploadCommand.MESSAGE_USAGE));
+        }
+        String[] path = trimmedArgs.split("\\s");
+        return new UploadCommand(Paths.get(path[1]));
+    }
+
 }

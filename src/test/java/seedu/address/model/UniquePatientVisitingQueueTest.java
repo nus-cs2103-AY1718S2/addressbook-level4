a9781cde086de9ece8c956c7d417f1c708b5ca72
Kyholmes <karen_tankaiyong@hotmail.com>
2018-03-20 12:50:23 +0800

    add new test in UniquePatientVisitingQueue (in progress)

--- src/test/java/seedu/address/model/UniquePatientVisitingQueueTest.java
+++ src/test/java/seedu/address/model/UniquePatientVisitingQueueTest.java
@@ -1,11 +1,19 @@
 package seedu.address.model;
 
+import static seedu.address.testutil.TypicalPatients.getTypicalAddressBook;
+
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class UniquePatientVisitingQueueTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
-  
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_addPatient_addSuccessful() {
+        
+    }
 }

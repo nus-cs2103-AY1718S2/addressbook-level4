7959a548219f316a72207a329ede48d8469e2dca
Kyholmes <karen_tankaiyong@hotmail.com>
2018-03-21 00:07:53 +0800

    add RemovePatientQueueCommandParser class

---
+++ src/main/java/seedu/address/logic/parser/RemovePatientQueueCommandParser.java
@@ -0,0 +1,20 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.RemovePatientQueueCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new RemovePatientQueueCommand object
+ */
+public class RemovePatientQueueCommandParser implements Parser<RemovePatientQueueCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the RemovePatientQueueCommand
+     * and returns an RemovePatientQueueCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    @Override
+    public RemovePatientQueueCommand parse(String userInput) throws ParseException {
+        return null;
+    }
+}

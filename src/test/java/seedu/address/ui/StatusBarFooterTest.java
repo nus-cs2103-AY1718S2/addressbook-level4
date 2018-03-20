cd8195cd415dec6767ad615110f90d448165307b
XavierMaYuqian <32877925+XavierMaYuqian@users.noreply.github.com>
2018-03-20 23:35:30 +0800

    Update StatusBarFooterTest.java

--- src/test/java/seedu/address/ui/StatusBarFooterTest.java
+++ src/test/java/seedu/address/ui/StatusBarFooterTest.java
@@ -2,8 +2,8 @@ package seedu.address.ui;
 
 import static org.junit.Assert.assertEquals;
 import static seedu.address.testutil.EventsUtil.postNow;
-import static seedu.address.ui.StatusBarFooter.SYNC_STATUS_INITIAL;
 import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.ui.StatusBarFooter.SYNC_STATUS_INITIAL;
 import static seedu.address.ui.StatusBarFooter.SYNC_STATUS_UPDATED;
 import static seedu.address.ui.StatusBarFooter.TOTAL_PERSONS_STATUS;
 

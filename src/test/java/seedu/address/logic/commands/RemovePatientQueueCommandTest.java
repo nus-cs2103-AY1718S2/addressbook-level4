2650873130c682ad8827a929c254d0666a8b98e4
Kyholmes <karen_tankaiyong@hotmail.com>
2018-03-21 01:35:12 +0800

    add test in RemovePatientQueueCommandTest

--- src/test/java/seedu/address/logic/commands/RemovePatientQueueCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemovePatientQueueCommandTest.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import static org.junit.Assert.assertEquals;
 import static seedu.address.testutil.TypicalPatients.getTypicalAddressBook;
 
 import org.junit.Rule;
@@ -12,6 +13,8 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.patient.exceptions.DuplicatePatientException;
+import seedu.address.testutil.TypicalPatients;
 
 public class RemovePatientQueueCommandTest {
 
@@ -22,13 +25,30 @@ public class RemovePatientQueueCommandTest {
 
     @Test
     public void execute_emptyQueue_throwsCommandException() throws CommandException {
-        RemovePatientQueueCommand removeEmptyQueueCommand = prepareCommand();
+        RemovePatientQueueCommand removeEmptyQueueCommand = prepareEmptyQueueCommand();
         thrown.expect(CommandException.class);
         thrown.expectMessage(RemovePatientQueueCommand.MESSAGE_PERSON_NOT_FOUND_QUEUE);
         removeEmptyQueueCommand.execute();
     }
 
-    private RemovePatientQueueCommand prepareCommand() {
+    @Test
+    public void execute_patientExist_removeSuccessful() throws CommandException, DuplicatePatientException {
+        RemovePatientQueueCommand command = prepreCommand();
+        CommandResult commandResult = command.execute();
+        assertEquals(String.format(RemovePatientQueueCommand.MESSAGE_REMOVE_SUCCESS,
+                TypicalPatients.FIONA.getName().toString()), commandResult.feedbackToUser);
+    }
+
+    private RemovePatientQueueCommand prepareEmptyQueueCommand() {
+        RemovePatientQueueCommand command = new RemovePatientQueueCommand();
+        command.setData(model, new CommandHistory(), new UndoRedoStack());
+        return command;
+    }
+    /**
+     * Parses {@code userInput} into a {@code RemovePatientQueueCommand}.
+     */
+    private RemovePatientQueueCommand prepreCommand() throws DuplicatePatientException {
+        model.addPatientToQueue(TypicalPatients.FIONA);
         RemovePatientQueueCommand command = new RemovePatientQueueCommand();
         command.setData(model, new CommandHistory(), new UndoRedoStack());
         return command;

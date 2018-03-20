ba43ec82d3887f7c528b5ba2ec834be20a2e3d4d
demitycho <tanweihao94@gmail.com>
2018-03-20 17:31:52 +0800

    Start of schedule modules

---
+++ src/main/java/seedu/address/logic/commands/ScheduleCommand.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_STUDENTS;
+import static seedu.address.model.Model.PREDICATE_SHOW_FAVOURITE_STUDENTS;
+
+/**
+ * Displays the user's schedule.
+ */
+public class ScheduleCommand extends Command {
+
+    public static final String COMMAND_WORD = "schedule";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD +
+            ": Views your weekly schedule";
+
+    public static final String MESSAGE_SUCCESS = "Schedule displayed";
+
+    public ScheduleCommand() {}
+
+    @Override
+    public CommandResult execute() {
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -20,6 +20,7 @@ import seedu.address.logic.commands.HistoryCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.SelectCommand;
+import seedu.address.logic.commands.ScheduleCommand;
 import seedu.address.logic.commands.UndoCommand;
 import seedu.address.logic.commands.UnfavouriteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -93,6 +94,9 @@ public class AddressBookParser {
 
         case RedoCommand.COMMAND_WORD:
             return new RedoCommand();
+
+        case ScheduleCommand.COMMAND_WORD:
+            return new ScheduleCommand();
 
         case UnfavouriteCommand.COMMAND_WORD:
             return new UnfavouriteCommandParser().parse(arguments);

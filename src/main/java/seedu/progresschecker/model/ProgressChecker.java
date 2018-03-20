f054daaef72973c408e1869fa2c087f5c7cf2f4e
Liwen <e0134124@u.nus.edu>
2018-03-21 03:02:52 +0800

    add a new method to upload photo in progressChecker

--- src/main/java/seedu/progresschecker/model/ProgressChecker.java
+++ src/main/java/seedu/progresschecker/model/ProgressChecker.java
@@ -2,6 +2,11 @@ package seedu.progresschecker.model;
 
 import static java.util.Objects.requireNonNull;
 
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -9,6 +14,10 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import javafx.collections.ObservableList;
 import seedu.progresschecker.model.person.Person;
@@ -154,6 +163,27 @@ public class ProgressChecker implements ReadOnlyProgressChecker {
             return true;
         } else {
             throw new PersonNotFoundException();
+        }
+    }
+
+    /**
+     * Upload image from this {@Path path}.
+     * @throws FileNotFoundException if the file of this {@Path path} is not found.
+     */
+    public void uploadPhoto(Path path) throws IOException {
+        try {
+            BufferedImage img = ImageIO.read(path.toFile());
+            ImageIcon icon = new ImageIcon(img);
+            JFrame frame = new JFrame();
+            frame.setLayout(new FlowLayout());
+            frame.setSize(20, 20);
+            JLabel lbl = new JLabel();
+            lbl.setIcon(icon);
+            frame.add(lbl);
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        } catch (FileNotFoundException FNF){
+            throw new FileNotFoundException();
         }
     }
 

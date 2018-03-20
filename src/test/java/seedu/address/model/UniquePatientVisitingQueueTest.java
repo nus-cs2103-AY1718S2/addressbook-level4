9fc76cd1075dc3467417730814230fac7e7a5c6c
Kyholmes <karen_tankaiyong@hotmail.com>
2018-03-20 12:44:14 +0800

    add UniquePatientVisitngQueueTest, removed unused imports in UniquePatientList

---
+++ src/test/java/seedu/address/model/UniquePatientVisitingQueueTest.java
@@ -0,0 +1,11 @@
+package seedu.address.model;
+
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+public class UniquePatientVisitingQueueTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+  
+}

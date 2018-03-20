af67348d7851c42bffe4a2677135d33aa2a82867
Ngo Chanh Anh Minh <ncaminh.study@gmail.com>
2018-03-20 14:41:56 +0800

    Fix gradle typo

--- src/main/java/seedu/address/logic/RouteOptimization.java
+++ src/main/java/seedu/address/logic/RouteOptimization.java
@@ -1,20 +1,15 @@
 package seedu.address.logic;
 
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.maps.DistanceMatrixApi;
-import com.google.maps.DistanceMatrixApiRequest;
-import com.google.maps.GeoApiContext;
-import com.google.maps.GeocodingApi;
-import com.google.maps.errors.ApiException;
-import com.google.maps.model.Distance;
-import com.google.maps.model.DistanceMatrix;
-import com.google.maps.model.DistanceMatrixElement;
-import com.google.maps.model.GeocodingResult;
-
 import java.io.IOException;
 
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DistanceMatrix;
+
+/**
+ * Testing retrieve distance data
+ */
 public class RouteOptimization {
     public static void main(String []agrs) throws InterruptedException, ApiException, IOException {
         GeoApiContext context = new GeoApiContext.Builder()
@@ -27,7 +22,7 @@ public class RouteOptimization {
         DistanceMatrix matrix =
                 DistanceMatrixApi.getDistanceMatrix(context, origin, destination).await();
         String distance = matrix.rows[0].elements[0].distance.toString();
-        String distanceOnlyNumber = distance.substring(0,distance.length()-3);
+        String distanceOnlyNumber = distance.substring(0, distance.length() - 3);
         System.out.println(Double.parseDouble(distanceOnlyNumber));
     }
 }

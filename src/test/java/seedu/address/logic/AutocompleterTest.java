02f7366f067e1d2cf4fae75153c1de7f4c8812f1
mattbuot <matthieu2301@hotmail.fr>
2018-03-20 13:58:39 +0800

    Correction to autocompleter test

--- src/test/java/seedu/address/logic/AutocompleterTest.java
+++ src/test/java/seedu/address/logic/AutocompleterTest.java
@@ -10,7 +10,12 @@ import org.junit.Test;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-import seedu.address.model.person.*;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.DelivDate;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.person.Name;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 
 public class AutocompleterTest {

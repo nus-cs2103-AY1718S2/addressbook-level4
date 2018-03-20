06bfda1cbb73f1a46988b5c004baa73a2c7d3eb9
IzHoBX <hobingxuan97@gmail.com>
2018-03-21 02:38:18 +0800

    Fix style issues

--- src/main/java/seedu/address/logic/CreateNewCalendar.java
+++ src/main/java/seedu/address/logic/CreateNewCalendar.java
@@ -12,7 +12,6 @@ import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;

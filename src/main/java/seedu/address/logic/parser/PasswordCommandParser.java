8b87a66c68cb23d49a51be9b4ccb87b9e47e3cbb
Yegg <e0036736@u.nus.edu>
2018-03-20 14:52:47 +0800

    Remove unused method

--- src/main/java/seedu/address/logic/parser/PasswordCommandParser.java
+++ src/main/java/seedu/address/logic/parser/PasswordCommandParser.java
@@ -1,7 +1,5 @@
 package seedu.address.logic.parser;
 
-import java.util.stream.Stream;
-
 import seedu.address.logic.commands.PasswordCommand;
 
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -16,20 +14,11 @@ public class PasswordCommandParser implements Parser<PasswordCommand> {
      * and returns an AddCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public PasswordCommand parse(String args) throws ParseException {
-        args = args.trim();
+    public PasswordCommand parse(String arguments) throws ParseException {
+        String args = arguments.trim();
         if (args.equals("")) {
             throw new ParseException(PasswordCommand.INVALID_PASSWORD);
         }
         return new PasswordCommand(args);
     }
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
-
 }

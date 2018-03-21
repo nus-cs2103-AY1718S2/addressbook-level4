05277f8d14c31a0a890003933ae4f73742d2c5c3
mattbuot <matthieu2301@hotmail.fr>
2018-03-21 19:37:33 +0800

    Travis corrections

--- src/main/java/seedu/address/commons/core/Mailer.java
+++ src/main/java/seedu/address/commons/core/Mailer.java
@@ -4,22 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
-
+/**
+ * Send personalized emails to drivers and customers
+ */
 public class Mailer {
 
-    private static String PIGEONS_MAIL = "pigeonscs2103@gmail.com";
-    private static String PASSWORD = "Pigeons2103";
+    private static String pigeonsMail = "pigeonscs2103@gmail.com";
+    private static String password = "Pigeons2103";
     //private static String RECIPIENT = "delepine.matthieu@gmail.com";
     private static String RECIPIENT = "matthieu2301@hotmail.fr";
 
+    /**
+     * Used for testing
+     * @param args
+     */
     public static void main(String[] args) {
 
         List<String> to = new ArrayList<>();
@@ -28,13 +33,16 @@ public class Mailer {
         email(to);
     }
 
+    /**
+     * Send an email to the @param list of recipients
+     */
     private static void email(List<String> recipients) {
         Properties props = System.getProperties();
         String host = "smtp.gmail.com";
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", host);
-        props.put("mail.smtp.user", PIGEONS_MAIL);
-        props.put("mail.smtp.password", PASSWORD);
+        props.put("mail.smtp.user", pigeonsMail);
+        props.put("mail.smtp.password", password);
         props.put("mail.smtp.port", "587");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
@@ -45,20 +53,20 @@ public class Mailer {
         Session session = Session.getDefaultInstance(props);
         MimeMessage message = new MimeMessage(session);
         try {
-            message.setFrom(new InternetAddress(PIGEONS_MAIL));
+            message.setFrom(new InternetAddress(pigeonsMail));
 
-            for(String destination: recipients) {
+            for (String destination: recipients) {
                 message.addRecipient(Message.RecipientType.TO, new InternetAddress(destination));
             }
 
             message.setSubject(subject);
             message.setText(body);
             Transport transport = session.getTransport("smtp");
-            transport.connect(host, PIGEONS_MAIL, PASSWORD);
+            transport.connect(host, pigeonsMail, password);
             transport.sendMessage(message, message.getAllRecipients());
             transport.close();
-        }
-        catch (MessagingException ae) {
+
+        } catch (MessagingException ae) {
             ae.printStackTrace();
         }
         System.out.println("Message sent !");

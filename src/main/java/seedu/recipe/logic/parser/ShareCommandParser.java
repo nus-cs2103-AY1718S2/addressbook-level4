ff010cf560a7c5ff01ea2127ca4f81af5ec76628
RyanAngJY <ryanang.jy@gmail.com>
2018-03-21 13:08:52 +0800

    Added share command parser

---
+++ src/main/java/seedu/recipe/logic/parser/ShareCommandParser.java
@@ -0,0 +1,30 @@
+package seedu.recipe.logic.parser;
+
+import static seedu.recipe.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.recipe.commons.core.index.Index;
+import seedu.recipe.commons.exceptions.IllegalValueException;
+import seedu.recipe.logic.commands.SelectCommand;
+import seedu.recipe.logic.commands.ShareCommand;
+import seedu.recipe.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new ShareCommand object
+ */
+public class ShareCommandParser {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the ShareCommand
+     * and returns an ShareCommand object for execution.
+     * @throws ParseException if the user input does not conform to the expected format
+     */
+    public ShareCommand parse(String args) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            return new ShareCommand(index);
+        } catch (IllegalValueException ive) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
+        }
+    }
+}

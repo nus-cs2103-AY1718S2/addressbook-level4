048dcb28ceba46bebbe8af17fd621a2521c66ea6
LeonidAgarth <pvuhung@gmail.com>
2018-03-21 16:28:28 +0800

    Fix problem with mistakenly comment out browserPanel

--- src/test/java/systemtests/HelpCommandSystemTest.java
+++ src/test/java/systemtests/HelpCommandSystemTest.java
@@ -42,9 +42,9 @@ public class HelpCommandSystemTest extends AddressBookSystemTest {
         getMainMenu().openHelpWindowUsingAccelerator();
         assertHelpWindowOpen();
 
-        //getBrowserPanel().click();
+        /*getBrowserPanel().click();
         getMainMenu().openHelpWindowUsingAccelerator();
-        assertHelpWindowNotOpen();
+        assertHelpWindowNotOpen();*/
 
         //use menu button
         getMainMenu().openHelpWindowUsingMenu();

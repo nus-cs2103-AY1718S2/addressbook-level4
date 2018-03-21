05412fb08efe6fc8d867607e455d1be8eb9ce0fb
jaronchan <jaron.chan.jin.jia@gmail.com>
2018-03-22 03:07:58 +0800

    Corrected styling errors

--- src/main/java/seedu/address/commons/util/GeocodeUtil.java
+++ src/main/java/seedu/address/commons/util/GeocodeUtil.java
@@ -1,3 +1,4 @@
+//@@author jaronchan
 package seedu.address.commons.util;
 
 import com.lynden.gmapsfx.javascript.object.LatLong;
@@ -5,30 +6,43 @@ import com.lynden.gmapsfx.service.geocoding.GeocoderStatus;
 import com.lynden.gmapsfx.service.geocoding.GeocodingResult;
 import com.lynden.gmapsfx.service.geocoding.GeocodingService;
 
+/**
+ * Helps with retrieving the geocode from given address.
+ */
+
 public class GeocodeUtil {
 
     private static GeocodingService geocodingService;
     private static LatLong geocode;
+
+    /**
+     *  @return geocode
+     */
 
     public static LatLong getGeocode(String address) {
         convertAddressToGeocode(address);
         return geocode;
     }
 
+    /**
+     *  Calls on geocodingService to update geocode.
+     */
+
     private static void convertAddressToGeocode(String address) {
         geocodingService = new GeocodingService();
-        geocodingService.geocode(address,
-                (GeocodingResult[] results, GeocoderStatus status) -> {
+        geocodingService.geocode(address, (GeocodingResult[] results, GeocoderStatus status) -> {
 
-                    geocode = null;
-                    if (status == GeocoderStatus.ZERO_RESULTS) {
-                        return;
-                    } else if (results.length > 1) {
-                        geocode = new LatLong(results[0].getGeometry().getLocation().getLatitude(), results[0].getGeometry().getLocation().getLongitude());
-                    } else {
-                        geocode = new LatLong(results[0].getGeometry().getLocation().getLatitude(), results[0].getGeometry().getLocation().getLongitude());
-                    }
-                });
+            geocode = null;
+            if (status == GeocoderStatus.ZERO_RESULTS) {
+                return;
+            } else if (results.length > 1) {
+                geocode = new LatLong(results[0].getGeometry().getLocation().getLatitude(),
+                        results[0].getGeometry().getLocation().getLongitude());
+            } else {
+                geocode = new LatLong(results[0].getGeometry().getLocation().getLatitude(),
+                        results[0].getGeometry().getLocation().getLongitude());
+            }
+        });
         return;
     }
 }
--- src/main/java/seedu/address/ui/PersonDetailsPanel.java
+++ src/main/java/seedu/address/ui/PersonDetailsPanel.java
@@ -1,9 +1,10 @@
 package seedu.address.ui;
 
-import java.net.URL;
-import java.util.logging.Logger;
-import java.util.ResourceBundle;
+import static seedu.address.commons.util.GeocodeUtil.getGeocode;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
 
 import com.google.common.eventbus.Subscribe;
 import com.lynden.gmapsfx.GoogleMapView;
@@ -15,17 +16,13 @@ import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
 import com.lynden.gmapsfx.javascript.object.Marker;
 import com.lynden.gmapsfx.javascript.object.MarkerOptions;
 
-import javafx.application.Platform;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.Region;
-import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.model.person.Person;
-
-import static seedu.address.commons.util.GeocodeUtil.getGeocode;
 
 //@@author jaronchan
 /**
@@ -58,6 +55,10 @@ public class PersonDetailsPanel extends UiPart<Region>
         mapView.addMapInializedListener(this);
 
     }
+
+    /**
+     * Update the map based on new selection event.
+     */
 
     private void loadPersonMapAddress(Person person) {
         LatLong addressAsGeocode = getGeocode(person.getAddress().toString());
@@ -102,8 +103,6 @@ public class PersonDetailsPanel extends UiPart<Region>
     public void freeResources() {
         map = null;
     }
-
-
 
     @Subscribe
     private void handlePersonPanelSelectionChangedEvent(PersonPanelSelectionChangedEvent event) {

050d76e2d6a506c6df1a54992c84b6cc72a568ec
demitycho <tanweihao94@gmail.com>
2018-03-21 17:05:29 +0800

    Fixed Time validation issues and refactored a little

--- src/main/java/seedu/address/model/lesson/Time.java
+++ src/main/java/seedu/address/model/lesson/Time.java
@@ -4,40 +4,40 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Student's email in the address book.
+ * Represents a Student's time in a lesson in the Schedule.
  * Guarantees: immutable; is valid as declared in {@link #isValidTime String)}
  */
 public class Time {
 
-    public static final String MESSAGE_EMAIL_CONSTRAINTS = "Time should be of the format HH:MM "
+    public static final String MESSAGE_TIME_CONSTRAINTS = "Time should be of the format HH:MM "
             + "and adhere to the following constraints:\n"
             + "1. The hour HH should only contain numbers and be in range [00, 24] inclusive\n"
             + "2. This is followed by a ':' and then minutes MM. "
             + "3. The minutes MM should only contain numbers and be in range [00, 60] inclusive\n";
     // alphanumeric and special characters
-    private static final String HOUR_PART_REGEX = "^(\\d?[1-2]|[1-9]0)$";
-    private static final String MINUTE_PART_REGEX = "^(\\d?[1-6]|[1-9]0)$";
-    public static final String EMAIL_VALIDATION_REGEX = HOUR_PART_REGEX + ":"
+    private static final String HOUR_PART_REGEX = "([01]?[0-9]|2[0-3])";
+    private static final String MINUTE_PART_REGEX = "([0-5][0-9])";
+    public static final String TIME_VALIDATION_REGEX = HOUR_PART_REGEX + ":"
             + MINUTE_PART_REGEX;
 
     public final String value;
 
-    /**
-     * Constructs an {@code Email}.
+    /**T
+     * Constructs an {@code Time}.
      *
      * @param time A valid time string.
      */
     public Time(String time) {
         requireNonNull(time);
-        checkArgument(isValidTime(time), MESSAGE_EMAIL_CONSTRAINTS);
+        checkArgument(isValidTime(time), MESSAGE_TIME_CONSTRAINTS);
         this.value = time;
     }
 
     /**
-     * Returns if a given string is a valid student email.
+     * Returns if a given string is a valid student time.
      */
     public static boolean isValidTime(String test) {
-        return true;
+        return test.matches(TIME_VALIDATION_REGEX);
     }
 
     @Override

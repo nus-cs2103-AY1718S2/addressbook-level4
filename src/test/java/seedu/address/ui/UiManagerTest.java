55f5bf63469e64bf0c55182234cf7894491a40ad
ngshikang <ngshikang@gmail.com>
2018-03-22 13:30:12 +0800

    Fix UiManager test

--- src/test/java/seedu/address/ui/UiManagerTest.java
+++ src/test/java/seedu/address/ui/UiManagerTest.java
@@ -1,8 +1,6 @@
 package seedu.address.ui;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Rule;

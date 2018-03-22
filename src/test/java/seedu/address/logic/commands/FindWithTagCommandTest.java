2ac92b7df2582a2a706f3760b5619212f28c2105
Chuang Cheng Heng <mukevin66@gmail.com>
2018-03-23 02:34:26 +0800

    FindWithTagCommandTest.java: Style correction, remove unused import.

--- src/test/java/seedu/address/logic/commands/FindWithTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindWithTagCommandTest.java
@@ -21,7 +21,6 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.TagContainsKeywordsPredicate;
 

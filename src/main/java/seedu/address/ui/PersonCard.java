9a257e4e26b24f4dd7c624e14d4b527d5d2d8ffc
jonleeyz <jonleeyz@outlook.com>
2018-03-22 12:41:33 +0800

    [PersonCard.java] Add call to initTags().

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -49,7 +49,7 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
-        person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        initTags(person);
     }
 
     // given a tagName, returns the String representation of a colour style

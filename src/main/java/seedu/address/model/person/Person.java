209c9e4e272e5cf2697460ab96b24ed13164c599
Chuang Cheng Heng <mukevin66@gmail.com>
2018-03-23 02:27:48 +0800

    Person.java: Attempt to solve conflict with team repo locally.

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -71,20 +71,6 @@ public class Person {
     }
 
     //@@author
-
-    /**
-     * Returns the tag names of tags of a person as String.
-     * @return a string of all the tag names of tags of a person.
-     */
-    public String getTagsAsString() {
-        final StringBuilder builder = new StringBuilder();
-        for (String tag : tags.arrayOfTags()) {
-            builder.append(tag);
-            builder.append(" ");
-        }
-        return builder.toString();
-    }
-
     @Override
     public boolean equals(Object other) {
         if (other == this) {
@@ -120,6 +106,19 @@ public class Person {
                 .append(getAddress())
                 .append(" Tags: ");
         getTags().forEach(builder::append);
+        return builder.toString();
+    }
+
+    /**
+     * Returns the tag names of tags of a person as String.
+     * @return a string of all the tag names of tags of a person.
+     */
+    public String getTagsAsString() {
+        final StringBuilder builder = new StringBuilder();
+        for (String tag : tags.arrayOfTags()) {
+            builder.append(tag);
+            builder.append(" ");
+        }
         return builder.toString();
     }
 

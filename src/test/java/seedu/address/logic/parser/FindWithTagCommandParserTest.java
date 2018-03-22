103aeedb442427cd7856dd92bc74bff8098f0742
Chuang Cheng Heng <mukevin66@gmail.com>
2018-03-23 02:36:05 +0800

    FindWithTagCommandParserTest.java: Style correction, place line break for line that is longer than 120 characters

--- src/test/java/seedu/address/logic/parser/FindWithTagCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindWithTagCommandParserTest.java
@@ -17,7 +17,8 @@ public class FindWithTagCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindWithTagCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                FindWithTagCommand.MESSAGE_USAGE));
     }
 
     @Test

4b40f18ec91d7606f023de643f6acb9376221628
Chuang Cheng Heng <mukevin66@gmail.com>
2018-03-23 02:21:25 +0800

    Person.java: Attempt to solve conflict with team repo locally.

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -67,7 +67,7 @@ public class Person {
      */
     public static Comparator<Person> nameComparator() {
         return Comparator.comparing((Person p) -> p.getName().toString(), (
-                s1, s2) -> (s1.compareToIgnoreCase(s2) == 0) ? s1.compareTo(s2) : s1.compareToIgnoreCase(s2));
+            s1, s2) -> (s1.compareToIgnoreCase(s2) == 0) ? s1.compareTo(s2) : s1.compareToIgnoreCase(s2));
     }
 
     //@@author

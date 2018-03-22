82714e21d451c10ec5a214f1da569b85edec114f
jonleeyz <jonleeyz@outlook.com>
2018-03-23 02:14:05 +0800

    [ResultDisplayTest.java] Add import statements.

--- src/test/java/seedu/address/ui/ResultDisplayTest.java
+++ src/test/java/seedu/address/ui/ResultDisplayTest.java
@@ -3,6 +3,9 @@ package seedu.address.ui;
 import static org.junit.Assert.assertEquals;
 import static seedu.address.testutil.EventsUtil.postNow;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 

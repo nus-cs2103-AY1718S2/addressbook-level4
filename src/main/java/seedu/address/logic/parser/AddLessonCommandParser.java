8b49d3a9fcf97d1fda4cd34691ba7ca46c86d850
demitycho <tanweihao94@gmail.com>
2018-03-22 11:31:51 +0800

    pls work

--- src/main/java/seedu/address/logic/parser/AddLessonCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddLessonCommandParser.java
@@ -1,11 +1,11 @@
 package seedu.address.logic.parser;
 
+import static java.util.Objects.requireNonNull;
+
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DAY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_END_TIME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_START_TIME;
-
-import static java.util.Objects.requireNonNull;
 
 import seedu.address.commons.core.index.Index;
 

fb0efb1f31bf9d1397c1cf900fc69eda0d869e4c
ZacZequn <zhangzequn96@gmail.com>
2018-03-22 21:38:47 +0800

    Update formatting issues

--- src/main/java/seedu/address/model/dish/Dish.java
+++ src/main/java/seedu/address/model/dish/Dish.java
@@ -2,9 +2,7 @@ package seedu.address.model.dish;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.util.Collections;
 import java.util.Objects;
-import java.util.Set;
 
 
 

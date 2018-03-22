3092c7e467a75599cd85c9c1e21d22405d9df0b0
LeonidAgarth <pvuhung@gmail.com>
2018-03-22 17:01:43 +0800

    Add in Event classes, which are used for Calendar, TodoList, and NUSMods

---
+++ src/main/java/seedu/address/model/event/WeeklyEvent.java
@@ -0,0 +1,38 @@
+package seedu.address.model.event;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.address.model.module.Module;
+import seedu.address.model.module.Schedule;
+
+public class WeeklyEvent extends Event {
+    private String day;
+    private String[] details;
+
+    public WeeklyEvent(String name, String venue, String start, String end, String... details) {
+        super(name, venue, start, end);
+        this.day = details[0];          //Placeholder command
+        this.details = details;
+    }
+
+    public WeeklyEvent(Module mod, Schedule schedule) {
+        this.name = mod.getModuleCode();
+        this.venue = schedule.getClassNo();
+        this.startTime = schedule.getStartTime();
+        this.endTime = schedule.getEndTime();
+        this.day = schedule.getDayText();
+        this.details = null;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public ObservableList<String> getDetails() {
+        ArrayList<String> temp = new ArrayList<String>(Arrays.asList(details));
+        return FXCollections.observableArrayList(temp);
+    }
+}

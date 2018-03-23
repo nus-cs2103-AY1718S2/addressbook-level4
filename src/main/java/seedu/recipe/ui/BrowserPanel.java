7dfff0d24409f386eda3843a455d9d9fcb2b4f79
RyanAngJY <ryanang.jy@gmail.com>
2018-03-23 09:31:25 +0800

    clean up comments

--- src/main/java/seedu/recipe/ui/BrowserPanel.java
+++ src/main/java/seedu/recipe/ui/BrowserPanel.java
@@ -36,17 +36,8 @@ public class BrowserPanel extends UiPart<Region> {
     private final String APP_ID = "177615459696708";
     private final String ACCESS_RIGHTS = "publish_actions";
 
-
-    /*  private final String AUTH_URL = "https://graph.facebook.com/oauth/authorize?type=user_agent&client_id=" + APP_ID + "&redirect_uri=" + DOMAIN + "&scope=user_about_me,"
-            + "user_actions.books,user_actions.fitness,user_actions.music,user_actions.news,user_actions.video,user_birthday,user_education_history,"
-            + "user_events,user_photos,user_friends,user_games_activity,user_hometown,user_likes,user_location,user_photos,user_relationship_details,"
-            + "user_relationships,user_religion_politics,user_status,user_tagged_places,user_videos,user_website,user_work_history,ads_management,ads_read,email,"
-            + "manage_pages,publish_actions,read_insights,read_page_mailboxes,rsvp_event";
-*/
-
     private final String AUTHENTICATION_URL = "https://graph.facebook.com/oauth/authorize?type=user_agent&client_id="
             + APP_ID + "&redirect_uri=" + REDIRECT_DOMAIN + "&scope=" + ACCESS_RIGHTS;
-    // https://www.facebook.com/v2.12/dialog/oauth?client_id=615711762098255&redirect_uri=https://meusicate.herokuapp.com/&state={}
 
     private Recipe recipeToShare;
     //@@author

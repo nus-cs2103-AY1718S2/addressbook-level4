8d293bb0fbf36b02d23c95036335ddd84837e521
wynonaK <35650845+wynonaK@users.noreply.github.com>
2018-03-23 13:51:17 +0800

    Added Typical Person.

--- src/test/java/seedu/address/testutil/TypicalAppointments.java
+++ src/test/java/seedu/address/testutil/TypicalAppointments.java
@@ -11,10 +11,10 @@ import seedu.address.model.appointment.Appointment;
  */
 public class TypicalAppointments {
 
-    public static final Appointment ALLY = new AppointmentBuilder().withOwner("Ally Lee")
+    public static final Appointment ALLY = new AppointmentBuilder().withOwner(TypicalPersons.ALICE)
             .withRemark("Requires Home Visit").withDateTime("2018-12-31 12:30")
             .withTags("checkup").build();
-    public static final Appointment BENNY = new AppointmentBuilder().withOwner("Benny Lee")
+    public static final Appointment BENNY = new AppointmentBuilder().withOwner(TypicalPersons.BENSON)
             .withRemark("May require isolation").withDateTime("2018-12-31 14:30")
             .withTags("surgery").build();
 

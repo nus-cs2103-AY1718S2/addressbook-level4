c72616abd7655573c690048c2193dbb0f0209e0a
mhq199657 <mhq199657@163.com>
2018-03-23 16:08:12 +0800

    Make final declaration only be edited once

--- src/main/java/seedu/address/model/person/Status.java
+++ src/main/java/seedu/address/model/person/Status.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import javafx.scene.paint.Color;
 
@@ -46,27 +47,29 @@ public class Status {
 
     private static final HashMap<Integer, String> STATUS_MAP;
     static {
-        STATUS_MAP = new HashMap<Integer, String>() {{
-                put(INDEX_STATUS_NEW, STATUS_NEW);
-                put(INDEX_STATUS_INTERVIEW_FIRST_ROUND, STATUS_INTERVIEW_FIRST_ROUND);
-                put(INDEX_STATUS_INTERVIEW_SECOND_ROUND, STATUS_INTERVIEW_SECOND_ROUND);
-                put(INDEX_STATUS_REJECTED, STATUS_REJECTED);
-                put(INDEX_STATUS_WAITLIST, STATUS_WAITLIST);
-                put(INDEX_STATUS_OFFERED, STATUS_OFFERED);
-                put(INDEX_STATUS_ACCEPTED, STATUS_ACCEPTED);
-                put(INDEX_STATUS_WITHDRAWN, STATUS_WITHDRAWN); }};
+        Map<Integer, String> initMap = new HashMap<Integer, String>();
+        initMap.put(INDEX_STATUS_NEW, STATUS_NEW);
+        initMap.put(INDEX_STATUS_INTERVIEW_FIRST_ROUND, STATUS_INTERVIEW_FIRST_ROUND);
+        initMap.put(INDEX_STATUS_INTERVIEW_SECOND_ROUND, STATUS_INTERVIEW_SECOND_ROUND);
+        initMap.put(INDEX_STATUS_REJECTED, STATUS_REJECTED);
+        initMap.put(INDEX_STATUS_WAITLIST, STATUS_WAITLIST);
+        initMap.put(INDEX_STATUS_OFFERED, STATUS_OFFERED);
+        initMap.put(INDEX_STATUS_ACCEPTED, STATUS_ACCEPTED);
+        initMap.put(INDEX_STATUS_WITHDRAWN, STATUS_WITHDRAWN);
+        STATUS_MAP = new HashMap<>(initMap);
     }
     private static final HashMap<String, Color> COLOR_MAP;
     static {
-        COLOR_MAP = new HashMap<String, Color>() {{
-                put(STATUS_NEW, COLOR_NEW);
-                put(STATUS_INTERVIEW_FIRST_ROUND, COLOR_INTERVIEW_FIRST_ROUND);
-                put(STATUS_INTERVIEW_SECOND_ROUND, COLOR_INTERVIEW_SECOND_ROUND);
-                put(STATUS_REJECTED, COLOR_REJECTED);
-                put(STATUS_WAITLIST, COLOR_WAITLIST);
-                put(STATUS_OFFERED, COLOR_OFFERED);
-                put(STATUS_ACCEPTED, COLOR_ACCEPTED);
-                put(STATUS_WITHDRAWN, COLOR_WITHDRAWN); }};
+        Map<String, Color> initMap = new HashMap<String, Color>();
+        initMap.put(STATUS_NEW, COLOR_NEW);
+        initMap.put(STATUS_INTERVIEW_FIRST_ROUND, COLOR_INTERVIEW_FIRST_ROUND);
+        initMap.put(STATUS_INTERVIEW_SECOND_ROUND, COLOR_INTERVIEW_SECOND_ROUND);
+        initMap.put(STATUS_REJECTED, COLOR_REJECTED);
+        initMap.put(STATUS_WAITLIST, COLOR_WAITLIST);
+        initMap.put(STATUS_OFFERED, COLOR_OFFERED);
+        initMap.put(STATUS_ACCEPTED, COLOR_ACCEPTED);
+        initMap.put(STATUS_WITHDRAWN, COLOR_WITHDRAWN);
+        COLOR_MAP = new HashMap<String, Color>(initMap);
     }
     public final String value;
     public final Color color;

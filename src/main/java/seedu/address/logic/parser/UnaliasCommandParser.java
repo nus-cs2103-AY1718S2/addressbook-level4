6f34dd0836731f6ba7d5716ed97bc8671330f4c6
Jing <ojyy73@gmail.com>
2018-03-23 18:49:41 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/UnaliasCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UnaliasCommandParser.java
@@ -2,12 +2,8 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import java.util.Arrays;
-import java.util.List;
-
 import seedu.address.logic.commands.UnaliasCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.alias.Alias;
 
 /**
  * Parses input arguments and creates a new AddCommand object

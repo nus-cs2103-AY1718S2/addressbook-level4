2fb918d621e939aa83dd557cc5bf2ee43aa24e51
demitycho <tanweihao94@gmail.com>
2018-03-25 22:30:24 +0800

    Update sample data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -11,6 +11,7 @@ import seedu.address.model.student.Email;
 import seedu.address.model.student.Name;
 import seedu.address.model.student.Phone;
 import seedu.address.model.student.Student;
+import seedu.address.model.student.UniqueKey;
 import seedu.address.model.student.exceptions.DuplicateStudentException;
 import seedu.address.model.tag.Tag;
 
@@ -20,24 +21,24 @@ import seedu.address.model.tag.Tag;
 public class SampleDataUtil {
     public static Student[] getSampleStudents() {
         return new Student[] {
-            new Student(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"), new ProgrammingLanguage("C"),
-                getTagSet("friends")),
-            new Student(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new ProgrammingLanguage("Java"),
-                getTagSet("colleagues", "friends")),
-            new Student(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new ProgrammingLanguage("JavaScript"),
-                getTagSet("neighbours")),
-            new Student(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new ProgrammingLanguage("Python"),
-                getTagSet("family")),
-            new Student(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #17-35"), new ProgrammingLanguage("Swift"),
-                getTagSet("classmates")),
-            new Student(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Address("Blk 45 Aljunied Street 85, #11-31"), new ProgrammingLanguage("PHP"),
-                getTagSet("colleagues"))
+            new Student(new UniqueKey("54ef49"), new Name("Alex Yeoh"), new Phone("87438807"),
+                    new Email("alexyeoh@example.com"), new Address("Blk 30 Geylang Street 29, #06-40"),
+                    new ProgrammingLanguage("C"), getTagSet("friends")),
+            new Student(new UniqueKey("54a1ee"), new Name("Bernice Yu"), new Phone("99272758"),
+                    new Email("berniceyu@example.com"), new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
+                    new ProgrammingLanguage("Java"), getTagSet("colleagues", "friends")),
+            new Student(new UniqueKey("7d68ec"), new Name("Charlotte Oliveiro"), new Phone("93210283"),
+                    new Email("charlotte@example.com"), new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+                    new ProgrammingLanguage("JavaScript"), getTagSet("neighbours")),
+            new Student(new UniqueKey("cc8bce"), new Name("David Li"), new Phone("91031282"),
+                    new Email("lidavid@example.com"), new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
+                    new ProgrammingLanguage("Python"), getTagSet("family")),
+            new Student(new UniqueKey("a7aa65"), new Name("Irfan Ibrahim"), new Phone("92492021"),
+                    new Email("irfan@example.com"), new Address("Blk 47 Tampines Street 20, #17-35"),
+                    new ProgrammingLanguage("Swift"), getTagSet("classmates")),
+            new Student(new UniqueKey("43a242"), new Name("Roy Balakrishnan"), new Phone("92624417"),
+                    new Email("royb@example.com"), new Address("Blk 45 Aljunied Street 85, #11-31"),
+                    new ProgrammingLanguage("PHP"), getTagSet("colleagues"))
         };
     }
 

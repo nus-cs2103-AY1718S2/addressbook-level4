ac68983c074c0deac8324a6deb5bf682ea0dcdc1
nhatquang3112 <trannhatquang3112@gmail.com>
2018-03-27 16:46:50 +0800

    update ToDoTest with status

--- src/test/java/seedu/address/model/todo/ToDoTest.java
+++ src/test/java/seedu/address/model/todo/ToDoTest.java
@@ -10,13 +10,17 @@ import seedu.address.testutil.ToDoBuilder;
 public class ToDoTest {
     @Test
     public void equals() {
-        ToDo todoA = new ToDoBuilder().withContent("ToDo A").build();
-        ToDo todoB = new ToDoBuilder().withContent("ToDo A").build();
+        ToDo todoA = new ToDoBuilder().withContent("Something to do").withStatus("undone").build();
+        ToDo todoB = new ToDoBuilder().withContent("Something to do").withStatus("undone").build();
+        ToDo todoC = new ToDoBuilder().withContent("Something to do").withStatus("done").build();
 
         // different types -> returns false
         assertFalse(todoA.equals(1));
 
         // same content -> returns true
         assertTrue(todoA.hashCode() == todoB.hashCode());
+
+        // same content, different status -> returns true
+        assertTrue(todoA.equals(todoC));
     }
 }

22742493dd1b0c1bd83f8d5d38d02e658c871b60
Yegg <yeggasd@gmail.com>
2018-03-27 23:22:39 +0800

    Update Comment in PasswordCommandParser

--- src/test/java/seedu/address/logic/parser/PasswordCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/PasswordCommandParserTest.java
@@ -11,7 +11,7 @@ import seedu.address.logic.commands.PasswordCommand;
 /**
  * As we are only doing white-box testing, our test cases do not cover path variations
  * outside of the PasswordCommand code. For example, inputs "1" and "1 abc" take the
- * same path through the DeleteCommand, and therefore we test only one of them.
+ * same path through the PasswordCommand, and therefore we test only one of them.
  * The path variation for those two cases occur inside the ParserUtil, and
  * therefore should be covered by the ParserUtilTest.
  */

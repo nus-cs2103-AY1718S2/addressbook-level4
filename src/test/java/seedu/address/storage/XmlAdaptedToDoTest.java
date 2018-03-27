37207237a87f3ab0c0a675144cbad120e8284cd4
nhatquang3112 <trannhatquang3112@gmail.com>
2018-03-27 19:08:25 +0800

    include Status test for XmlAdaptedToDoTest

--- src/test/java/seedu/address/storage/XmlAdaptedToDoTest.java
+++ src/test/java/seedu/address/storage/XmlAdaptedToDoTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_CONTENT;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_STATUS;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_CONTENT;
 import static seedu.address.storage.XmlAdaptedToDo.MISSING_FIELD_MESSAGE_FORMAT;
 import static seedu.address.testutil.TypicalToDos.TODO_A;
 
@@ -11,6 +13,7 @@ import org.junit.Test;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.todo.Content;
+import seedu.address.model.todo.Status;
 import seedu.address.testutil.Assert;
 
 public class XmlAdaptedToDoTest {
@@ -33,6 +36,14 @@ public class XmlAdaptedToDoTest {
     public void toModelType_nullContent_throwsIllegalValueException() {
         XmlAdaptedToDo todo = new XmlAdaptedToDo((String) null);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Content.class.getSimpleName());
+        Assert.assertThrows(IllegalValueException.class, expectedMessage, todo::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidStatus_throwsIllegalValueException() {
+        XmlAdaptedToDo todo =
+                new XmlAdaptedToDo(VALID_CONTENT, INVALID_STATUS);
+        String expectedMessage = Status.MESSAGE_STATUS_CONSTRAINTS;
         Assert.assertThrows(IllegalValueException.class, expectedMessage, todo::toModelType);
     }
 

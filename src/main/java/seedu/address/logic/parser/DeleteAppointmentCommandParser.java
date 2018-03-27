63579c222b1a227565dd2949d7c5bdfdfeff217d
Kyholmes <karen_tankaiyong@hotmail.com>
2018-03-28 01:24:46 +0800

    implement parse function in delete appointment command parser

--- src/main/java/seedu/address/logic/parser/DeleteAppointmentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteAppointmentCommandParser.java
@@ -1,7 +1,15 @@
 package seedu.address.logic.parser;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.DeleteAppointmentCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.patient.NameContainsKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new DeleteAppointmentCommand object
@@ -13,8 +21,25 @@ public class DeleteAppointmentCommandParser implements Parser<DeleteAppointmentC
      * @throws ParseException if the user input does not conform the expected format
      */
     @Override
-    public DeleteAppointmentCommand parse(String userInput) throws ParseException {
+    public DeleteAppointmentCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    DeleteAppointmentCommand.MESSAGE_USAGE));
+        }
 
-        return null;
+        String[] nameKeywords = trimmedArgs.split("\\s");
+
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(nameKeywords[nameKeywords.length - 1]);
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    DeleteAppointmentCommand.MESSAGE_USAGE));
+        }
+
+        return new DeleteAppointmentCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)), index);
     }
 }

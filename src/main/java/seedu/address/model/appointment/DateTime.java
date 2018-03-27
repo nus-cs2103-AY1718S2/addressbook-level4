68b80b3c0d540116c3101c722699b8a3443ef32f
Kyholmes <karen_tankaiyong@hotmail.com>
2018-03-27 18:52:50 +0800

    add static method to compare date in DateTime

--- src/main/java/seedu/address/model/appointment/DateTime.java
+++ src/main/java/seedu/address/model/appointment/DateTime.java
@@ -1,5 +1,8 @@
 package seedu.address.model.appointment;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Appointment DateTime in Imdb
  * Gurantees: details are present and not null, field values are validated, immutable
@@ -7,14 +10,35 @@ package seedu.address.model.appointment;
 public class DateTime {
 
     public static final int APPOINTMENT_DURATION = 30;
-    private String appointmentDateTime;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private String dateString;
+    private String timeString;
 
     public DateTime(String appointmentDateTime) {
-        this.appointmentDateTime = appointmentDateTime;
+        String trimmedArgs = appointmentDateTime.trim();
+
+        String[] dateTimeKeys = trimmedArgs.split("\\s");
+        this.dateString = dateTimeKeys[0];
+        this.timeString = dateTimeKeys[1];
     }
 
     public String toString() {
-        return this.appointmentDateTime;
+        return dateString + " " + timeString;
+    }
+
+    /**
+     * Check if the date is past compared with today
+     * @param dateTimeString
+     * @return true if the date is already past
+     */
+    public static boolean isBefore(String dateTimeString) {
+        String trimmedArgs = dateTimeString.trim();
+
+        String[] dateTimeKeys = trimmedArgs.split("\\s");
+
+        LocalDate compareDate = LocalDate.parse(dateTimeKeys[0], formatter);
+
+        return compareDate.isBefore(LocalDate.now());
     }
 
 }

e173a01c48344f43b475cc6361d05c41b5c82f07
Liwen <e0134124@u.nus.edu>
2018-03-27 09:53:43 +0800

    add a method in personCard to update the profile photo

--- src/main/java/seedu/progresschecker/ui/PersonCard.java
+++ src/main/java/seedu/progresschecker/ui/PersonCard.java
@@ -2,6 +2,8 @@ package seedu.progresschecker.ui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
@@ -15,7 +17,6 @@ public class PersonCard extends UiPart<Region> {
     private static final String FXML = "PersonListCard.fxml";
     private static final String[] TAG_COLORS =
         { "red", "orange", "yellow", "green", "blue", "purple" };
-
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
      * As a consequence, UI elements' variable names cannot be set to such keywords
@@ -44,6 +45,8 @@ public class PersonCard extends UiPart<Region> {
     private Label username;
     @FXML
     private FlowPane tags;
+    @FXML
+    private ImageView profile;
 
     public PersonCard(Person person, int displayedIndex) {
         super(FXML);
@@ -79,6 +82,13 @@ public class PersonCard extends UiPart<Region> {
             sum += c;
         }
         return sum;
+    }
+
+    /**
+     * Updates the profile photo
+     */
+    public void updatePhoto() {
+        profile.setImage(Image.impl_fromPlatformImage(person.getImage()));
     }
 
     @Override

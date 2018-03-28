237dd5791d10ab5b71b5e75cf9a0104601ef6b9b
Sebry9 <e0031720@u.nus.edu>
2018-03-29 00:54:16 +0800

    add need prefix

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -34,7 +34,8 @@ public class AddCommand extends UndoableCommand {
             + PREFIX_APPOINTMENT + "05-05-2018 "
             + PREFIX_GROUP + "PRIORITY"
             + PREFIX_TAG + "friends "
-            + PREFIX_TAG + "owesMoney";
+            + PREFIX_TAG + "owesMoney"
+            + PREFIX_INSURANCE + "Health";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";

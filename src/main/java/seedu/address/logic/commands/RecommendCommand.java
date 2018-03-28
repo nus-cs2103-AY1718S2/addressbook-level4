0e0e2ef9501c9358500e7ff78c248357f4dd9f2c
Low Jia Jin <pixelrife@hotmail.com>
2018-03-29 00:41:32 +0800

    Recommender can parse Person attributes

--- src/main/java/seedu/address/logic/commands/RecommendCommand.java
+++ src/main/java/seedu/address/logic/commands/RecommendCommand.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.recommender.Recommender;
 import seedu.address.model.person.Person;
 
 import java.util.List;
@@ -11,12 +12,16 @@ public class RecommendCommand extends Command {
 
     public static final String COMMAND_WORD = "recommend";
 
-    public static final String MESSAGE_SUCCESS = "Recommendations for: %1$s";
+    public static final String MESSAGE_SUCCESS = "Recommendations for: %1$s\n" +
+            "Format: [<product id, probability of buying>, <...>, ...]\n" +
+            "%2$s";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds the products most likely to be bought by the " +
             "person identified by the index number used in the last person listing.\n" +
             "Parameters: INDEX (must be a positive integer)\n" +
             "Example:" + COMMAND_WORD + " 1";
+
+    private static final String ARFF_NAME = "data/Orders.arff";
 
     private final Index targetIndex;
 
@@ -36,6 +41,9 @@ public class RecommendCommand extends Command {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
 
-        return new CommandResult(String.format(MESSAGE_SUCCESS, personToRecommendFor.getName()));
+        Recommender recommender = new Recommender(ARFF_NAME);
+        String recommendations = recommender.getRecommendations(personToRecommendFor);
+
+        return new CommandResult(String.format(MESSAGE_SUCCESS, personToRecommendFor.getName(), recommendations));
     }
 }

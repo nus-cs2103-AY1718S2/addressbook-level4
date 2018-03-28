55f5b24c8073129cdcd40b7fe3dd6f09a9029f01
Lim Zheng Kai <limzk1994@hotmail.com>
2018-03-28 17:11:48 +0800

    Added Group class

--- src/main/java/seedu/address/model/ReadOnlyAddressBook.java
+++ src/main/java/seedu/address/model/ReadOnlyAddressBook.java
@@ -3,6 +3,7 @@ package seedu.address.model;
 import javafx.collections.ObservableList;
 import seedu.address.model.person.Person;
 import seedu.address.model.tag.Tag;
+import seedu.address.model.group.Group;
 
 /**
  * Unmodifiable view of an address book
@@ -20,5 +21,9 @@ public interface ReadOnlyAddressBook {
      * This list will not contain any duplicate tags.
      */
     ObservableList<Tag> getTagList();
-
+    /**
+     * Returns an unmodifiable view of the groups list.
+     * This list will not contain any duplicate groups.
+     */
+    ObservableList<Group> getGroupList();
 }

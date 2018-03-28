508379b94f55e36b34747f4f33ebb977df14ec5d
LeonidAgarth <pvuhung@gmail.com>
2018-03-28 15:23:00 +0800

    Fix coding style

--- src/test/java/seedu/address/ui/CalendarTest.java
+++ src/test/java/seedu/address/ui/CalendarTest.java
@@ -1,20 +1,12 @@
 package seedu.address.ui;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import org.junit.Test;
-
-import seedu.address.model.event.Event;
-import seedu.address.testutil.EventBuilder;
 
 public class CalendarTest extends GuiUnitTest {
 
-    /*@Test
+    @Test
     public void equals() {
-        Calendar calendar = new Calendar(null);
-        assertEquals(calendar, new Calendar(null));
-    }*/
+        /*Calendar calendar = new Calendar(null);
+        assertEquals(calendar, new Calendar(null));*/
+    }
 }

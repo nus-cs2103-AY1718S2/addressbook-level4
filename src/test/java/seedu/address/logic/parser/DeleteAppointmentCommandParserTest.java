0c3b524dabf256e1c35a3b1625ec01f76de8da4e
Kyholmes <karen_tankaiyong@hotmail.com>
2018-03-28 11:52:01 +0800

    add new test in delete appointment command parser test

--- src/test/java/seedu/address/logic/parser/DeleteAppointmentCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteAppointmentCommandParserTest.java
@@ -2,10 +2,15 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import java.util.Arrays;
 
 import org.junit.Test;
 
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.DeleteAppointmentCommand;
+import seedu.address.model.patient.NameContainsKeywordsPredicate;
 
 public class DeleteAppointmentCommandParserTest {
     private DeleteAppointmentCommandParser parser = new DeleteAppointmentCommandParser();
@@ -14,5 +19,13 @@ public class DeleteAppointmentCommandParserTest {
     public void parse_empty_throwParseException() {
         assertParseFailure(parser, "   ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                 DeleteAppointmentCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnDeleteAppointmentCommand() throws IllegalValueException {
+        DeleteAppointmentCommand expectedCommand = new DeleteAppointmentCommand(
+                new NameContainsKeywordsPredicate(Arrays.asList("Alice")), ParserUtil.parseIndex("1"));
+        assertParseSuccess(parser, "Alice 1", expectedCommand);
+        assertParseSuccess(parser, "\n Alice 1 \n", expectedCommand);
     }
 }

3bd77788f90d2fb9e8106f57546c97d1d9564036
Gilbert Emerson <emer7@users.noreply.github.com>
2018-03-28 13:42:13 +0800

    Checkstyle: Fix violation

--- src/test/java/seedu/address/logic/commands/FindCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindCommandTest.java
@@ -26,7 +26,6 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.FieldContainKeyphrasesPredicate;
-import seedu.address.model.person.NameContainsKeyphrasesPredicate;
 import seedu.address.model.person.Person;
 
 /**

f3afdb348da775f7a51ff4846a5da2caf4fc0a1d
jonleeyz <jonleeyz@outlook.com>
2018-03-28 12:42:20 +0800

    [PopulateRequestEvent.java] Refactor: rearrange import statements.

--- src/main/java/seedu/address/commons/events/ui/PopulatePrefixesRequestEvent.java
+++ src/main/java/seedu/address/commons/events/ui/PopulatePrefixesRequestEvent.java
@@ -1,9 +1,18 @@
 package seedu.address.commons.events.ui;
 
-import seedu.address.commons.events.BaseEvent;
-import seedu.address.logic.commands.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import static seedu.address.logic.parser.CliSyntax.*;
+import seedu.address.commons.events.BaseEvent;
+
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.LocateCommand;
 
 /**
  * Indicates that a new result is available.

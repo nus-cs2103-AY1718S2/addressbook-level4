c06b2a514faf0efc275221463297a888fb80c4bf
luca590 <lucasgaylord@protonmail.com>
2018-03-29 19:34:56 +0800

    finished test cases

--- src/test/java/seedu/address/logic/commands/ImportContactsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ImportContactsCommandTest.java
@@ -1,4 +1,172 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import javafx.collections.ObservableList;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.FindCommandParser;
+import seedu.address.logic.parser.ImportContactsCommandParser;
+import seedu.address.model.ModelManager;
+import seedu.address.model.person.DateAdded;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.UniquePersonList;
+import seedu.address.model.tag.Tag;
+import seedu.address.testutil.PersonBuilder;
+
+
+
 public class ImportContactsCommandTest {
+    private static ImportContactsCommandParser imparse;
+    private static ImportContactsCommand importValidPath;
+    private static ImportContactsCommand importIllegalPath;
+
+    //featureUnderTest_testScenario_expectedBehavior()
+    @Rule
+    public final ExpectedException thrown = ExpectedException.none();
+    private FindCommandParser parser = new FindCommandParser();
+
+    static {
+        try {
+            importValidPath = new ImportContactsCommand("/Users/lucasgaylord/Desktop/Test_contacts.csv");
+            importIllegalPath = new ImportContactsCommand("...");
+
+            imparse = new ImportContactsCommandParser();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Used for debugging
+     */
+    public void printResult(final String n, final String e,
+                            final String p, final String a) {
+        System.out.println("---------------");
+        System.out.println("Name : " + n);
+        System.out.println("Email : " + e);
+        System.out.println("Phone : " + p);
+        System.out.println("Address : " + a);
+        System.out.println("---------------\n\n");
+    }
+
+    @Test
+    public void execute_validPathToTestFile_printCsvContents() throws IOException {
+        Reader reader = Files.newBufferedReader(Paths.get("/Users/lucasgaylord/Desktop/Test_contacts.csv"));
+
+        CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
+                .withFirstRecordAsHeader()
+                .withIgnoreHeaderCase()
+                .withTrim());
+
+        Iterable<CSVRecord> csvRecords = csvParser.getRecords();
+        requireNonNull(csvRecords);
+
+        String name = null;
+        String email = null;
+        String phone = null;
+        String address = null;
+
+        for (CSVRecord csvRecord : csvRecords) { //iterate through the
+            // Accessing values by Header names
+            name = csvRecord.get("Name");
+            email = csvRecord.get("Email");
+            phone = csvRecord.get("Phone");
+            address = csvRecord.get("Address");
+
+            printResult(name, email, phone, address);
+        }
+    }
+
+    @Test
+    public void execute_validPersonToAdd_addPeople() throws CommandException {
+        PersonBuilder pb = new PersonBuilder();
+        Person personToAdd = pb.build();
+
+        requireNonNull(personToAdd);
+
+        String name;
+        String email;
+        String phone;
+        String address;
+        Set<Tag> tagSet = new HashSet<>();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+        DateAdded addDate;
+
+        //initialize UniquePersonList for test case
+        UniquePersonList upl = new UniquePersonList();
+
+        try {
+            upl.add(personToAdd);
+        } catch (Exception e) {
+            throw new CommandException("Failed to add person in ImportContactsCommand, execute()\n"
+                    + e);
+        }
+
+    }
+
+    @Test
+    public void parse_inputCorrectPath_returnsImportContactsCommandWithCorrectFilePath() throws Exception {
+        ImportContactsCommand icc = imparse.parse("/Users/lucasgaylord/Desktop/Test_contacts.csv");
+        assertEquals(icc.getFileAddress(), "/Users/lucasgaylord/Desktop/Test_contacts.csv");
+    }
+
+    @Test
+    public void openFile_inputCorrectPath_returnsCommandResult() throws Exception {
+        ImportContactsCommand icc = new ImportContactsCommand("/Users/lucasgaylord/Desktop/Test_contacts.csv");
+        CommandResult cr = icc.openFile();
+        requireNonNull(cr);
+        System.out.println(cr.equals(importValidPath.openFile()));
+    }
+
+    @Test
+    public void openFile_inputIllegalPath_throwsException() throws Exception { //what type of exception
+        thrown.expect(CommandException.class);
+        importIllegalPath.openFile();
+    }
+
+
+    /*
+     * This test will fail if contacts are imported from a file containing
+     * different contacts
+     */
+    @Test
+    public void execute_inputValidFileAddress_addsPersonsToAddressBook() throws CommandException {
+        importValidPath.model = new ModelManager();
+        importValidPath.executeUndoableCommand();
+        ObservableList<Person> plist = importValidPath.model.getFilteredPersonList();
+
+        requireNonNull(importValidPath.model);
+        assertFalse(plist.isEmpty());
+        Object[] parray = plist.toArray();
+        assertEquals(parray[1].toString(),
+                "Bob Phone: 32134558 Email: bob@badasbob.com Address: "
+                        + "324 green st Date Added: 29/03/2018 Tags: [friend]");
+        assertEquals(parray[2].toString(), "Carol Phone: 91442333 Email: "
+                + "carol@carlcaronna.com Address: 509 blue st Date Added: 29/03/2018 Tags: [friend]");
+        assertEquals(parray[3].toString(), "Dave Phone: 77004352 Email: "
+                + "dave@doggod.com Address: 409 yellow st Date Added: 29/03/2018 Tags: [friend]");
+        assertEquals(parray[4].toString(), "Edward Phone: 15432349 Email: "
+                + "ed@edible.com Address: 909 grey st Date Added: 29/03/2018 Tags: [friend]");
+    }
 }

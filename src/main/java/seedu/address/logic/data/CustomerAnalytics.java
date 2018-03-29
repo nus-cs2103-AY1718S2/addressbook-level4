3bf02ba67d77d47d5238fcd2a89d48275f0d69aa
Chu Qing Hao <6337103+qinghao1@users.noreply.github.com>
2018-03-29 15:00:55 +0800

    Change ArrayList to generic List

--- src/main/java/seedu/address/logic/data/CustomerAnalytics.java
+++ src/main/java/seedu/address/logic/data/CustomerAnalytics.java
@@ -5,7 +5,7 @@ import seedu.address.model.money.Money;
 import seedu.address.model.order.Order;
 import seedu.address.model.person.Person;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Collections;
 import java.time.LocalDateTime;
 import java.util.Comparator;
@@ -15,13 +15,13 @@ public class CustomerAnalytics extends Analytics {
      * Orders person list by total spending across all orders, from smallest to largest
      * @param personList
      */
-    public static void orderBySpending(ArrayList<Person> personList) {
+    public static void orderBySpending(List<Person> personList) {
         //Sort by spending, from smallest to biggest
         Collections.sort(personList, (person1, person2) -> {
             Money p1Spending = new Money();
             Money p2Spending = new Money();
-            ArrayList<Order> p1Orders = person1.getOrders();
-            ArrayList<Order> p2Orders = person2.getOrders();
+            List<Order> p1Orders = person1.getOrders();
+            List<Order> p2Orders = person2.getOrders();
 
             for(Order order : p1Orders)
                 p1Spending = p1Spending.plus(order.getOrderTotal());
@@ -37,13 +37,13 @@ public class CustomerAnalytics extends Analytics {
      * @param personList
      * @param time
      */
-    public static void orderBySpendingSinceTime(ArrayList<Person> personList, LocalDateTime time) {
+    public static void orderBySpendingSinceTime(List<Person> personList, LocalDateTime time) {
         //Sort by spending since time, from smallest to biggest
         Collections.sort(personList, (person1, person2) -> {
             Money p1Spending = new Money();
             Money p2Spending = new Money();
-            ArrayList<Order> p1Orders = person1.getOrders();
-            ArrayList<Order> p2Orders = person2.getOrders();
+            List<Order> p1Orders = person1.getOrders();
+            List<Order> p2Orders = person2.getOrders();
 
             for (Order order : p1Orders)
                 if (order.getTime().compareTo(time) >= 0)
@@ -60,7 +60,7 @@ public class CustomerAnalytics extends Analytics {
      * Orders person list by time since last order, from most recent to least recent
      * @param personList
      */
-    public static void orderByLastActive(ArrayList<Person> personList) {
+    public static void orderByLastActive(List<Person> personList) {
         //Sort by last order time, from biggest to smallest
         Collections.sort(personList, Comparator.comparing(Person::getLastOrderTime));
     }

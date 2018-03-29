9af38519f440b7b3cadd4de625a2c1785e59167e
Der-Erlkonig <wisuchoi@gmail.com>
2018-03-29 14:12:40 +0800

    add PersonPage.html in matching color and font, with a few fields

--- src/main/java/seedu/address/storage/HtmlWriter.java
+++ src/main/java/seedu/address/storage/HtmlWriter.java
@@ -9,16 +9,22 @@ public class HtmlWriter {
 
     public String writePerson() {
         String filepath = System.getProperty("user.dir") + File.separator + "PersonPage.html";
+        String absoluteFilepath;
         File file = new File(filepath);
         try {
             PrintWriter printWriter = new PrintWriter(file);
-            printWriter.print("<!DOCTYPE html><html>,<head><title>HELPLAH</title></head><body style=\"background-color:blue;\"></body></html>");
+            printWriter.print("<!DOCTYPE html><html><head><title>LoanSharkManager</title></head><body style=\"background-color:#383838;\">");
+            printWriter.print("<font face=\"Segoe UI Semibold\" size=\"20\" color=\"white\">");
+            printWriter.print("<p>Name:</p>");
+            printWriter.print("<p>Amount Owed:</p>");
+            printWriter.print("<p>Due Date:</p>");
+            printWriter.print("</body></html>");
             printWriter.close();
         } catch (FileNotFoundException e) {
 
         }
-        filepath = file.getAbsolutePath();
-        filepath = filepath.replaceAll("\"", "/");
-        return filepath;
+        absoluteFilepath = file.getAbsolutePath();
+        absoluteFilepath = absoluteFilepath.replaceAll("\"", "/");
+        return absoluteFilepath;
     }
 }

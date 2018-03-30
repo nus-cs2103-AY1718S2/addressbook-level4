ee6e7cc0603b2e0adecc12a4d4f96cf8386b07f7
luca590 <lucasgaylord@protonmail.com>
2018-03-30 13:42:46 +0800

    finished export functionality

--- src/main/java/seedu/address/logic/commands/ExportContactsCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportContactsCommand.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import java.util.Iterator;
 import javafx.collections.ObservableList;
@@ -24,30 +23,27 @@ import static java.util.Objects.requireNonNull;
 
 public class ExportContactsCommand extends UndoableCommand {
 
-    public static Path WRITE_TO_PATH; // This path must include filename at end
-
     public static final String SUCCESS = "Contacts successfully exported.\n";
 
     public static final String COMMAND_WORD = "export_contacts";
     public static final String COMMAND_ALIAS = "ec";
 
+    private Path WRITE_TO_PATH; // This path must include filename at end
 
     public ExportContactsCommand() {
+        System.out.println("Contstructor called without argument");
         WRITE_TO_PATH = getDefaultPath();
         System.out.println(WRITE_TO_PATH);
     }
 
     public ExportContactsCommand(String filePath) {
+        System.out.println("Contstructor called WTIH argument");
         requireNonNull(filePath);
-        //check path validity
-        WRITE_TO_PATH = FileSystems.getDefault().getPath(filePath);
-
-        //create file if doesn't exist
-        
+        WRITE_TO_PATH = FileSystems.getDefault().getPath(filePath.trim());
     }
 
     /*
-     * Takes a valid input file (if invalid getCSVToWriteTo will throw error)
+     * Takes a valid input file (if invalid getCsvToWriteTo will throw error)
      * and loops through the Persons in current address book, exporting
      * each one to file specified
      */
@@ -58,7 +54,7 @@ public class ExportContactsCommand extends UndoableCommand {
 
         //Write file to path and specify name
         try {
-            csvPrinter = getCSVToWriteTo();
+            csvPrinter = getCsvToWriteTo();
         } catch (java.lang.Exception e) {
             throw new CommandException(e.getMessage());
         }
@@ -88,22 +84,27 @@ public class ExportContactsCommand extends UndoableCommand {
     /**
      * @return gives the csv file to write to. The exported contacts will be in this file
      */
-    public CSVPrinter getCSVToWriteTo() throws IOException {
+    public CSVPrinter getCsvToWriteTo() throws IOException {
         CSVPrinter csvPrinter;
 
         try {
+            System.out.println("File path is: " + WRITE_TO_PATH);
             BufferedWriter writer = Files.newBufferedWriter(WRITE_TO_PATH);
             csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
                     .withHeader("Name", "Email", "Phone", "Address"));
         } catch (IOException ioe) {
-            throw new IOException("Failed to create writable path. Probably incorrect file path.");
+            throw new IOException("Failed to create writable path - in getCsvToWriteTo");
         }
 
         return csvPrinter;
     }
 
     public Path getDefaultPath() {
-        Path defaultPath = FileSystems.getDefault().getPath("data/exportTo.csv");
+        Path defaultPath = FileSystems.getDefault().getPath("data/exportToExisting.csv");
         return defaultPath;
+    }
+
+    public Path getWRITE_TO_PATH() {
+        return WRITE_TO_PATH;
     }
 }

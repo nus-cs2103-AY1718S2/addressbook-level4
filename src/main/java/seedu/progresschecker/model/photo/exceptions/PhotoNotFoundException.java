9e3a66b050a9b6e36f3db1eb25631528ccb46e3d
Liwen <e0134124@u.nus.edu>
2018-04-01 23:29:06 +0800

    add exceptions for photo path

---
+++ src/main/java/seedu/progresschecker/model/photo/exceptions/PhotoNotFoundException.java
@@ -0,0 +1,6 @@
+package seedu.progresschecker.model.photo.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified photo.
+ */
+public class PhotoNotFoundException extends Exception {}

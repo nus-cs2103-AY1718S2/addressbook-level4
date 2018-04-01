859263e3e8c053cfd7bb8543c0d3ba926f0bb0da
Kyholmes <karen_tankaiyong@hotmail.com>
2018-04-01 22:03:34 +0800

    remove unused methods

--- src/main/java/seedu/address/logic/parser/AddPatientQueueCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddPatientQueueCommandParser.java
@@ -34,12 +34,4 @@ public class AddPatientQueueCommandParser implements Parser<AddPatientQueueComma
 
         return new AddPatientQueueCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
     }
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
 }

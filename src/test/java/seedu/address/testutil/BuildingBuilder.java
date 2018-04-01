67586287006c0fa520423111fa5474e01eff4d83
Caijun7 <lohcaijun@gmail.com>
2018-04-01 22:38:48 +0800

    Add builder classes for Building, Room, Week, and WeekDay

---
+++ src/test/java/seedu/address/testutil/BuildingBuilder.java
@@ -0,0 +1,76 @@
+package seedu.address.testutil;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import seedu.address.model.building.Building;
+
+/**
+ * A utility class to help with building {@code Building} objects
+ */
+public class BuildingBuilder {
+
+    public static final String DEFAULT_BUILDINGNAME = "COM2";
+
+    private static HashMap<String, ArrayList<String>> nusBuildingsAndRooms = null;
+
+    private HashMap<String, ArrayList<String>> buildingsAndRooms;
+    private String buildingName;
+
+    public BuildingBuilder() {
+        buildingName = DEFAULT_BUILDINGNAME;
+        buildingsAndRooms = createValidNusBuildingsAndRooms();
+        nusBuildingsAndRooms = buildingsAndRooms;
+    }
+
+    /**
+     * Initializes the BuildingBuilder with the data of {@code buildingToCopy}.
+     */
+    public BuildingBuilder(Building buildingToCopy) {
+        buildingName = buildingToCopy.getBuildingName();
+        buildingsAndRooms = buildingToCopy.getBuildingsAndRooms();
+        nusBuildingsAndRooms = Building.getNusBuildingsAndRooms();
+    }
+
+    /**
+     * Creates a valid {@code nusBuildingsAndBuildings}
+     */
+    private HashMap<String, ArrayList<String>> createValidNusBuildingsAndRooms() {
+        HashMap<String, ArrayList<String>> validNusBuildingsAndRooms = new HashMap<>();
+        ArrayList<String> validRoomsInBuildings = new ArrayList<>();
+        validRoomsInBuildings.add(RoomBuilder.DEFAULT_ROOMNAME);
+        validNusBuildingsAndRooms.put(DEFAULT_BUILDINGNAME, validRoomsInBuildings);
+        return validNusBuildingsAndRooms;
+    }
+
+    /**
+     * Sets the {@code buildingName} into a {@code Building} that we are building.
+     */
+    public BuildingBuilder withBuildingName(String buildingName) {
+        this.buildingName = buildingName;
+        return this;
+    }
+
+    /**
+     * Sets the {@code nusBuildingsAndRooms} into a {@code Building} that we are building.
+     */
+    public BuildingBuilder withNusBuildingsAndRooms(HashMap<String, ArrayList<String>> nusBuildingsAndRooms) {
+        BuildingBuilder.nusBuildingsAndRooms = nusBuildingsAndRooms;
+        return this;
+    }
+
+    /**
+     * Sets the {@code buildingsAndRooms} into a {@code Building} that we are building.
+     */
+    public BuildingBuilder withBuildingsAndRooms(HashMap<String, ArrayList<String>> buildingsAndRooms) {
+        this.buildingsAndRooms = buildingsAndRooms;
+        return this;
+    }
+
+    public Building build() {
+        Building building = new Building(buildingName);
+        Building.setNusBuildingsAndRooms(nusBuildingsAndRooms);
+        building.setBuildingsAndRooms(buildingsAndRooms);
+        return building;
+    }
+}
---
+++ src/test/java/seedu/address/testutil/WeekDayBuilder.java
@@ -0,0 +1,86 @@
+package seedu.address.testutil;
+
+import java.util.HashMap;
+
+import seedu.address.model.building.WeekDay;
+
+/**
+ * A utility class to help with building {@code WeekDay} objects
+ */
+public class WeekDayBuilder {
+
+    public static final String DEFAULT_WEEKDAY = "Monday";
+    public static final String DEFAULT_ROOMNAME = "COM2-0108";
+
+    private HashMap<String, String> weekDaySchedule;
+    private String weekday;
+    private String roomName;
+
+    public WeekDayBuilder() {
+        weekday = DEFAULT_WEEKDAY;
+        roomName = DEFAULT_ROOMNAME;
+        weekDaySchedule = createValidWeekDaySchedule();
+    }
+
+    /**
+     * Initializes the WeekDayBuilder with the data of {@code weekDayToCopy}.
+     */
+    public WeekDayBuilder(WeekDay weekDayToCopy) {
+        weekday = weekDayToCopy.getWeekday();
+        roomName = weekDayToCopy.getRoomName();
+        weekDaySchedule = weekDayToCopy.getWeekDaySchedule();
+    }
+
+    /**
+     * Creates a valid {@code weekDaySchedule}
+     */
+    private HashMap<String, String> createValidWeekDaySchedule() {
+        HashMap<String, String> validWeekDaySchedule = new HashMap<>();
+        validWeekDaySchedule.put("0800", "vacant");
+        validWeekDaySchedule.put("0900", "vacant");
+        validWeekDaySchedule.put("1000", "vacant");
+        validWeekDaySchedule.put("1100", "vacant");
+        validWeekDaySchedule.put("1200", "vacant");
+        validWeekDaySchedule.put("1300", "vacant");
+        validWeekDaySchedule.put("1400", "vacant");
+        validWeekDaySchedule.put("1500", "vacant");
+        validWeekDaySchedule.put("1600", "vacant");
+        validWeekDaySchedule.put("1700", "vacant");
+        validWeekDaySchedule.put("1800", "vacant");
+        validWeekDaySchedule.put("1900", "vacant");
+        validWeekDaySchedule.put("2000", "vacant");
+        return validWeekDaySchedule;
+    }
+
+    /**
+     * Sets the {@code weekday} of the {@code WeekDay} that we are building.
+     */
+    public WeekDayBuilder withWeekDay(String weekday) {
+        this.weekday = weekday;
+        return this;
+    }
+
+    /**
+     * Sets the {@code roomName} into a {@code WeekDay} that we are building.
+     */
+    public WeekDayBuilder withRoomName(String roomName) {
+        this.roomName = roomName;
+        return this;
+    }
+
+    /**
+     * Sets the {@code weekDaySchedule} into a {@code WeekDay} that we are building.
+     */
+    public WeekDayBuilder withWeekDaySchedule(HashMap<String, String> weekDaySchedule) {
+        this.weekDaySchedule = weekDaySchedule;
+        return this;
+    }
+
+    public WeekDay build() {
+        WeekDay weekDay = new WeekDay();
+        weekDay.setWeekday(weekday);
+        weekDay.setRoomName(roomName);
+        weekDay.setWeekDaySchedule(weekDaySchedule);
+        return weekDay;
+    }
+}

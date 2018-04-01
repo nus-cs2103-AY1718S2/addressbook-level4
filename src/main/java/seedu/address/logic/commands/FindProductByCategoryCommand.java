3979f3764585281cd4d3a20b45099ca8bd91e029
Low Jia Jin <pixelrife@hotmail.com>
2018-04-01 20:07:09 +0800

    Added command to find product by category

--- src/main/java/seedu/address/logic/commands/FindProductByCategoryCommand.java
+++ src/main/java/seedu/address/logic/commands/FindProductByCategoryCommand.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.commands;
 
-import seedu.address.commons.core.Messages;
 import seedu.address.model.product.ProductCategoryContainsKeywordsPredicate;
 
 public class FindProductByCategoryCommand extends Command {
@@ -12,8 +11,6 @@ public class FindProductByCategoryCommand extends Command {
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " food";
 
-    private static final String message = Messages.MESSAGE_PRODUCTS_LISTED_OVERVIEW;
-
     private final ProductCategoryContainsKeywordsPredicate predicate;
 
     public FindProductByCategoryCommand(ProductCategoryContainsKeywordsPredicate predicate) {
@@ -23,7 +20,7 @@ public class FindProductByCategoryCommand extends Command {
     @Override
     public CommandResult execute() {
         model.updateFilteredProductList(predicate);
-        return new CommandResult(getMessageForListShownSummary(model.getFilteredProductList().size(), message));
+        return new CommandResult(getMessageForPersonListShownSummary(model.getFilteredProductList().size()));
     }
 
     @Override

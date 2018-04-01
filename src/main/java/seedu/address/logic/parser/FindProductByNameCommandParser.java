431d4dcd3e1ea545ed63d1eb1e727c9f2a320178
Low Jia Jin <pixelrife@hotmail.com>
2018-04-01 21:03:47 +0800

    Added findproductbyname command

---
+++ src/main/java/seedu/address/logic/parser/FindProductByNameCommandParser.java
@@ -0,0 +1,25 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.FindProductByNameCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.product.ProductNameContainsKeywordsPredicate;
+
+import java.util.Arrays;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+public class FindProductByNameCommandParser implements Parser<FindProductByNameCommand> {
+
+    public FindProductByNameCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindProductByNameCommand.MESSAGE_USAGE));
+        }
+
+        String[] categoryKeywords = trimmedArgs.split("\\s+");
+
+        return new FindProductByNameCommand(
+                new ProductNameContainsKeywordsPredicate(Arrays.asList(categoryKeywords)));
+    }
+}

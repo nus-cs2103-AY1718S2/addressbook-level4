caf1c873e9a4ca86faf0f033bdf89f833dc4fd7a
kengsengg <gks573@hotmail.com>
2018-04-03 01:27:23 +0800

    Calendar: implemented setSummary for createEvent() method

--- src/main/java/seedu/address/ui/Calendar.java
+++ src/main/java/seedu/address/ui/Calendar.java
@@ -20,33 +20,32 @@ import com.google.api.client.util.store.FileDataStoreFactory;
 
 import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
-import com.google.api.services.calendar.model.Events;
+import com.google.api.services.calendar.model.EventDateTime;
 
+import seedu.address.model.appointment.Appointment;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+
+/**
+ * Calendar of the App
+ */
 public class Calendar {
-    /** Application name. */
+
     private static final String APPLICATION_NAME =  "Google Calendar API Java Quickstart";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+    private static HttpTransport HTTP_TRANSPORT;
 
     /** Directory to store user credentials for this application. */
     private static final java.io.File DATA_STORE_DIR = new java.io.File(
             System.getProperty("user.home"), ".credentials/calendar-java-quickstart");
-
-    /** Global instance of the {@link FileDataStoreFactory}. */
-    private static FileDataStoreFactory DATA_STORE_FACTORY;
-
-    /** Global instance of the JSON factory. */
-    private static final JsonFactory JSON_FACTORY =
-            JacksonFactory.getDefaultInstance();
-
-    /** Global instance of the HTTP transport. */
-    private static HttpTransport HTTP_TRANSPORT;
 
     /** Global instance of the scopes required by this quickstart.
      *
      * If modifying these scopes, delete your previously saved credentials
      * at ~/.credentials/calendar-java-quickstart
      */
-    private static final List<String> SCOPES =
-            Arrays.asList(CalendarScopes.CALENDAR);
+    private static final List<String> SCOPES = Arrays.asList(CalendarScopes.CALENDAR);
 
     static {
         try {
@@ -71,16 +70,13 @@ public class Calendar {
                 GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
         // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow =
-                new GoogleAuthorizationCodeFlow.Builder(
-                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
                         .setDataStoreFactory(DATA_STORE_FACTORY)
                         .setAccessType("offline")
                         .build();
         Credential credential = new AuthorizationCodeInstalledApp(
                 flow, new LocalServerReceiver()).authorize("user");
-        System.out.println(
-                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
         return credential;
     }
 
@@ -89,43 +85,37 @@ public class Calendar {
      * @return an authorized Calendar client service
      * @throws IOException
      */
-    public static com.google.api.services.calendar.Calendar
-    getCalendarService() throws IOException {
+    public static com.google.api.services.calendar.Calendar getCalendarService() throws IOException {
         Credential credential = authorize();
-        return new com.google.api.services.calendar.Calendar.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, credential)
+        return new com.google.api.services.calendar.Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
                 .setApplicationName(APPLICATION_NAME)
                 .build();
     }
 
-    public static void main(String[] args) throws IOException {
-        // Build a new authorized API client service.
-        // Note: Do not confuse this class with the
-        //   com.google.api.services.calendar.model.Calendar class.
-        com.google.api.services.calendar.Calendar service =
-                getCalendarService();
+    /**
+     * Creates an event on the Google Calendar
+     * @throws IOException
+     */
+    public static void createEvent(Appointment toAdd, Person selectedPerson) throws IOException {
+        com.google.api.services.calendar.Calendar service = getCalendarService();
 
-        // List the next 10 events from the primary calendar.
-        DateTime now = new DateTime(System.currentTimeMillis());
-        Events events = service.events().list("primary")
-                .setMaxResults(10)
-                .setTimeMin(now)
-                .setOrderBy("startTime")
-                .setSingleEvents(true)
-                .execute();
-        List<Event> items = events.getItems();
-        if (items.size() == 0) {
-            System.out.println("No upcoming events found.");
-        } else {
-            System.out.println("Upcoming events");
-            for (Event event : items) {
-                DateTime start = event.getStart().getDateTime();
-                if (start == null) {
-                    start = event.getStart().getDate();
-                }
-                System.out.printf("%s (%s)\n", event.getSummary(), start);
-            }
-        }
+        String date = toAdd.getDate();
+        String startTime = toAdd.getStartTime();
+        String endTime = toAdd.getEndTime();
+        Name name = selectedPerson.getName();
+
+        Event event = new Event()
+                .setSummary(name.toString());
+
+        DateTime startDateTime = new DateTime("2018-04-03T09:00:00+08:00");
+        EventDateTime start = new EventDateTime().setDateTime(startDateTime);
+        event.setStart(start);
+
+        DateTime endDateTime = new DateTime("2018-04-03T17:00:00+08:00");
+        EventDateTime end = new EventDateTime().setDateTime(endDateTime);
+        event.setEnd(end);
+
+        String calendarId = "primary";
+        service.events().insert(calendarId, event).execute();
     }
-
 }

9da8777e95591e8c6518bde4b902bd611e8a6a52
Caijun7 <lohcaijun@gmail.com>
2018-04-02 23:59:13 +0800

    Add export current view of AddressBook feature

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -120,7 +120,7 @@ public class ModelManager extends ComponentManager implements Model {
     }
     //@@author
 
-    //@@author Caijun7
+    //@@author Caijun7-reused
     @Override
     public void deleteTag(Tag tag) {
         addressBook.removeTag(tag);
@@ -136,10 +136,23 @@ public class ModelManager extends ComponentManager implements Model {
     public void importAddressBook(String filepath, byte[] password) throws DataConversionException, IOException,
                                                                             WrongPasswordException {
         requireNonNull(filepath);
-        requireNonNull(password);
 
         XmlAddressBookStorage xmlAddressBook = new XmlAddressBookStorage(filepath);
         xmlAddressBook.importAddressBook(filepath, this.addressBook, password);
+        indicateAddressBookChanged();
+    }
+
+    /**
+     * Exports the current view of {@code AddressBook} to the filepath.
+     * @param filepath
+     */
+    @Override
+    public void exportAddressBook(String filepath, Password password) throws IOException, WrongPasswordException,
+                                                                                DuplicatePersonException {
+        requireNonNull(filepath);
+
+        XmlAddressBookStorage xmlAddressBook = new XmlAddressBookStorage(filepath);
+        xmlAddressBook.exportAddressBook(filepath, password, filteredPersons);
         indicateAddressBookChanged();
     }
     //@@author

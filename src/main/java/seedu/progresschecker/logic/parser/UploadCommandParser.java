772d63eebf6656f8872b5c7ea79c10f99560525d
Liwen <e0134124@u.nus.edu>
2018-04-03 02:56:35 +0800

    update parser

--- src/main/java/seedu/progresschecker/logic/parser/UploadCommandParser.java
+++ src/main/java/seedu/progresschecker/logic/parser/UploadCommandParser.java
@@ -3,11 +3,14 @@ package seedu.progresschecker.logic.parser;
 import static java.util.Objects.requireNonNull;
 import static seedu.progresschecker.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import java.io.IOException;
+
 import seedu.progresschecker.commons.core.index.Index;
 import seedu.progresschecker.commons.exceptions.IllegalValueException;
 import seedu.progresschecker.logic.commands.UploadCommand;
 import seedu.progresschecker.logic.parser.exceptions.ParseException;
 
+//@@author Livian1107
 /**
  * Parses input arguments and creates a new UploadCommand object
  */
@@ -20,13 +23,17 @@ public class UploadCommandParser  implements Parser<UploadCommand>  {
     public UploadCommand parse(String args) throws ParseException {
         requireNonNull(args);
         Index index;
+        String[] content = args.trim().split(" ");
         try {
-            String[] content = args.trim().split(" ");
             index = ParserUtil.parseIndex(content[0]);
             return new UploadCommand(index, content[1]);
         } catch (IllegalValueException ive) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, UploadCommand.MESSAGE_USAGE));
+        } catch (IOException e) {
+            throw new ParseException(
+                    UploadCommand.MESSAGE_IMAGE_NOT_FOUND);
         }
+
     }
 }

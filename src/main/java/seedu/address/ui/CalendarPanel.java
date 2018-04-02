9dba230a640f3cd28b6e4e6ea69432b8eea7e270
Kyholmes <karen_tankaiyong@hotmail.com>
2018-04-02 23:51:08 +0800

    add event to display calendar when command is entered

--- src/main/java/seedu/address/ui/CalendarPanel.java
+++ src/main/java/seedu/address/ui/CalendarPanel.java
@@ -3,12 +3,14 @@ package seedu.address.ui;
 import java.util.logging.Logger;
 
 import com.calendarfx.view.CalendarView;
+import com.google.common.eventbus.Subscribe;
 
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.ui.ShowCalendarViewRequestEvent;
 
 /**
  * Panel containing the calendar view displaying patients' appointment.
@@ -31,5 +33,10 @@ public class CalendarPanel extends UiPart<Region> {
 
     private void setConnections() {
         calendarView.setAlignment(calendar, Pos.CENTER);
+    }
+
+    @Subscribe
+    public void handleShowCalendarEvent(ShowCalendarViewRequestEvent scvre) {
+        setConnections();
     }
 }

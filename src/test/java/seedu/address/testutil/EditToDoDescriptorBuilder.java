9a6236405e3e73b49cb7ae47c05fe2543c0398eb
nhatquang3112 <trannhatquang3112@gmail.com>
2018-04-02 22:15:25 +0800

    include EditToDoCommandTest

---
+++ src/test/java/seedu/address/testutil/EditToDoDescriptorBuilder.java
@@ -0,0 +1,41 @@
+package seedu.address.testutil;
+
+import seedu.address.logic.commands.EditToDoCommand.EditToDoDescriptor;
+import seedu.address.model.todo.Content;
+import seedu.address.model.todo.ToDo;
+
+/**
+ * A utility class to help with building EditToDoDescriptor objects.
+ */
+public class EditToDoDescriptorBuilder {
+
+    private EditToDoDescriptor descriptor;
+
+    public EditToDoDescriptorBuilder() {
+        descriptor = new EditToDoDescriptor();
+    }
+
+    public EditToDoDescriptorBuilder(EditToDoDescriptor descriptor) {
+        this.descriptor = new EditToDoDescriptor(descriptor);
+    }
+
+    /**
+     * Returns an {@code EditToDoDescriptor} with fields containing {@code toDo}'s details
+     */
+    public EditToDoDescriptorBuilder(ToDo toDo) {
+        descriptor = new EditToDoDescriptor();
+        descriptor.setContent(toDo.getContent());
+    }
+
+    /**
+     * Sets the {@code Content} of the {@code EditToDoDescriptor} that we are building.
+     */
+    public EditToDoDescriptorBuilder withContent(String content) {
+        descriptor.setContent(new Content(content));
+        return this;
+    }
+
+    public EditToDoDescriptor build() {
+        return descriptor;
+    }
+}

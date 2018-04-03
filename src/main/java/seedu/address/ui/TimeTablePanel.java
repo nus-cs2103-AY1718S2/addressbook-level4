82847ed77b08fd7888f3a95a44005c91f6b22081
Yegg <yeggasd@gmail.com>
2018-04-03 19:06:39 +0800

    Beautify Timetable

--- src/main/java/seedu/address/ui/TimeTablePanel.java
+++ src/main/java/seedu/address/ui/TimeTablePanel.java
@@ -9,32 +9,51 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.Region;
-import javafx.scene.paint.Color;
+import seedu.address.commons.util.StringUtil;
 
 //@@author yeggasd
 /**
  * A ui for the info panel that is displayed when the timetable command is called.
  */
 public class TimeTablePanel extends UiPart<Region> {
-    private static final String OCCUPIED_STYLE_CLASS = "occupied";
-    private static final String VACANT_STYLE_CLASS = "vacant";
+    private static final String COLUMNHEADER_STYLE_CLASS = "column-header";
+    private static final String TABLECELL_STYLE_CLASS = "table-cell";
+    private static final String EMPTY_STYLE_CLASS = "timetable-cell-empty";
+    private static final String[] MOD_COLOR_STYLES = { "modteal", "modsandybrown", "modplum", "modyellow",
+                                                         "modyellow"};
     private static final String FXML = "TimeTablePanel.fxml";
 
     private ArrayList<TableColumn<ArrayList<String>, String>> columns;
     @FXML
     private TableView timeTable;
     @FXML
-    private TableColumn<ArrayList<String>, String> modId;
+    private TableColumn<ArrayList<String>, String> day;
     @FXML
-    private TableColumn<ArrayList<String>, String> monday;
+    private TableColumn<ArrayList<String>, String> eightAm;
     @FXML
-    private TableColumn<ArrayList<String>, String> tuesday;
+    private TableColumn<ArrayList<String>, String> nineAm;
     @FXML
-    private TableColumn<ArrayList<String>, String> wednesday;
+    private TableColumn<ArrayList<String>, String> tenAm;
     @FXML
-    private TableColumn<ArrayList<String>, String> thursday;
+    private TableColumn<ArrayList<String>, String> elevenAm;
     @FXML
-    private TableColumn<ArrayList<String>, String> friday;
+    private TableColumn<ArrayList<String>, String> twelvePm;
+    @FXML
+    private TableColumn<ArrayList<String>, String> onePm;
+    @FXML
+    private TableColumn<ArrayList<String>, String> twoPm;
+    @FXML
+    private TableColumn<ArrayList<String>, String> threePm;
+    @FXML
+    private TableColumn<ArrayList<String>, String> fourPm;
+    @FXML
+    private TableColumn<ArrayList<String>, String> fivePm;
+    @FXML
+    private TableColumn<ArrayList<String>, String> sixPm;
+    @FXML
+    private TableColumn<ArrayList<String>, String> sevenPm;
+    @FXML
+    private TableColumn<ArrayList<String>, String> eightPm;
 
 
     public TimeTablePanel() {
@@ -48,8 +67,8 @@ public class TimeTablePanel extends UiPart<Region> {
         timeTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
         initializeColumns();
         initializeTableColumns();
-        modId.setMinWidth(100);
-        modId.setMaxWidth(100);
+        day.setMinWidth(100);
+        day.setMaxWidth(100);
     }
 
     /**
@@ -57,12 +76,20 @@ public class TimeTablePanel extends UiPart<Region> {
      */
     private void initializeColumns() {
         columns = new ArrayList<>();
-        columns.add(modId);
-        columns.add(monday);
-        columns.add(tuesday);
-        columns.add(wednesday);
-        columns.add(thursday);
-        columns.add(friday);
+        columns.add(day);
+        columns.add(eightAm);
+        columns.add(nineAm);
+        columns.add(tenAm);
+        columns.add(elevenAm);
+        columns.add(twelvePm);
+        columns.add(onePm);
+        columns.add(twoPm);
+        columns.add(threePm);
+        columns.add(fourPm);
+        columns.add(fivePm);
+        columns.add(sixPm);
+        columns.add(sevenPm);
+        columns.add(eightPm);
     }
 
     /**
@@ -72,10 +99,12 @@ public class TimeTablePanel extends UiPart<Region> {
         for (int i = 0; i < columns.size(); i++) {
             final int j = i;
             columns.get(i).setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().get(j)));
+            columns.get(i).impl_setReorderable(false);
             if (j != 0) {
-                columns.get(i).setMinWidth(100);
-                columns.get(i).setMaxWidth(300);
+                columns.get(i).setMinWidth(75);
+                columns.get(i).setMaxWidth(200);
             }
+
         }
     }
 
@@ -95,17 +124,39 @@ public class TimeTablePanel extends UiPart<Region> {
                             setStyle("");
                         } else {
                             setText(item);
-                            if (getItem() == null) {
-                                setTextFill(Color.BLACK);
-                                setStyle("-fx-background-color: #D3D3D3");
+                            removeAllStyle(this);
+                            if ("".equals(getItem())) {
+                                getStyleClass().add(EMPTY_STYLE_CLASS);
+                            } else if (StringUtil.isDay(getItem())) {
+                                getStyleClass().add(COLUMNHEADER_STYLE_CLASS);
                             } else {
-                                setTextFill(Color.BLACK);
-                                setStyle("-fx-background-color: #17A589");
+                                getStyleClass().add(getColorStyleFor(getItem()));
                             }
                         }
                     }
                 };
             });
         }
+    }
+
+    /**
+     * Removes all styles present in cell
+     * @param tableCell Cell with its style to be removed
+     */
+    private static void removeAllStyle(TableCell<ArrayList<String>, String> tableCell) {
+        for (String color : MOD_COLOR_STYLES) {
+            tableCell.getStyleClass().remove(color);
+        }
+        tableCell.getStyleClass().remove(EMPTY_STYLE_CLASS);
+        tableCell.getStyleClass().remove(TABLECELL_STYLE_CLASS);
+    }
+
+    /**
+     * Returns a Color Style for the Module based on its hashcode.
+     * @param modName
+     * @return colorStyle for {@code modName}'s label.
+     */
+    private static String getColorStyleFor(String modName) {
+        return MOD_COLOR_STYLES[Math.abs(modName.hashCode()) % MOD_COLOR_STYLES.length];
     }
 }

e1cf35e7a4c45eb55ab53aedce8bc1a805bfcbbe
jas5469 <jas_p90@hotmail.com>
2018-04-03 18:43:02 +0800

    Fixed checkstyle

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -131,8 +131,8 @@ public class CommandTestUtil {
     public static final String VALID_INFORMATION = "GROUP A";
     public static final String INVALID_INFORMATION = "Group! A"; //'!' not supported in information
 
-    public static final String VALID_DESC_GROUP = " " + PREFIX_GROUP + VALID_INFORMATION + " " +
-            PREFIX_NAME + VALID_NAME_BOB;
+    public static final String VALID_DESC_GROUP = " " + PREFIX_GROUP + VALID_INFORMATION + " "
+            + PREFIX_NAME + VALID_NAME_BOB;
     public static final String INVALID_DESC_NO_GROUP = " " + PREFIX_NAME + VALID_NAME_BOB;
     public static final String INVALID_DESC_NO_NAME = " " + PREFIX_GROUP + "Group A";
 

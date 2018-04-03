7c081cb12aef9f5d3c045a67700da3209a33638a
Jing <ojyy73@gmail.com>
2018-04-03 15:16:04 +0800

    Link maps to building

--- src/main/java/seedu/address/model/building/Building.java
+++ src/main/java/seedu/address/model/building/Building.java
@@ -22,8 +22,16 @@ public class Building {
      */
     public static final String[] NUS_BUILDINGS = {
         "AS1", "AS2", "AS3", "AS4", "AS5", "AS6", "AS7", "AS8", "COM1", "COM2", "I3", "BIZ1", "BIZ2",
-        "SDE", "S1", "S1A", "S2", "S3", "S4", "S4A", "S5", "S8", "S11", "S12", "S13", "S14", "S16",
-        "S17", "E1", "E2", "E2A", "E3", "E3A", "E4", "E4A", "E5", "EA", "ERC", "UTSRC", "LT"
+        "SDE", "S1", "S1A", "S2", "S3", "S4", "S4A", "S5", "S8", "S10", "S11", "S12", "S13", "S14", "S15",
+        "S16", "S17", "E1", "E1A", "E2", "E2A", "E3", "E3A", "E4", "E4A", "E5", "E6", "EA", "ERC", "UTSRC"
+    };
+
+    public static final String[] NUS_BUILDINGS_ADDRESSES = {
+        "117570", "117570", "117570", "117570", "117570", "117416", "117570", "119260", "117417", "117417",
+        "119613", "119245", "119245", "117592", "117546", "117546", "117546", "117558", "117543", "117543",
+        "117543", "117548", "117546", "117553", "117550", "117550", "117542", "117541", "117546", "119076 S17",
+        "117575", "117575", "117575", "117361", "117581", "117574", "117583", "117583", "117583", "117608",
+        "117575", "139599", "138607"
     };
 
     private static HashMap<String, ArrayList<String>> nusBuildingsAndRooms;
@@ -67,6 +75,18 @@ public class Building {
             }
         }
         return false;
+    }
+
+    /**
+     * Returns the postal code if building is an NUS building.
+     */
+    public static String retrieveNusBuildingIfExist(String test) {
+        for (int i = 0; i < NUS_BUILDINGS.length; i++) {
+            if (NUS_BUILDINGS[i].equals(test)) {
+                return NUS_BUILDINGS_ADDRESSES[i];
+            }
+        }
+        return test;
     }
 
     public String getBuildingName() {

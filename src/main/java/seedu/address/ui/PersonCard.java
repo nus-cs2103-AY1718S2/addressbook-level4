060e4c7ec75de45012bb3b0e173b539c2a58f38d
Kevin Chin <KevinCJH@users.noreply.github.com>
2018-04-04 01:32:33 +0800

    Update PersonCard.java

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -57,7 +57,7 @@ public class PersonCard extends UiPart<Region> {
         email.setText(person.getEmail().value);
 
         if (person.getProfilePicture().filePath != null) {
-             imageView.setImage(person.getProfilePicture().getImage());
+            imageView.setImage(person.getProfilePicture().getImage());
          }
         //@@author KevinCJH
         initTags(person);

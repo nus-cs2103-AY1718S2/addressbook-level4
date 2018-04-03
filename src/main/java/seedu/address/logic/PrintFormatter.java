5067866dfaa68d8c5b9c4907e87ed39cb91775fa
cxingkai <cxingkai@yahoo.com.sg>
2018-04-03 18:31:53 +0800

    Add PrintFormatter, Update PrintCommand to let PrintFormatter handle records of patient

---
+++ src/main/java/seedu/address/logic/PrintFormatter.java
@@ -0,0 +1,5 @@
+package seedu.address.logic;
+
+public class PrintFormatter {
+
+}
--- src/main/java/seedu/address/logic/commands/PrintCommand.java
+++ src/main/java/seedu/address/logic/commands/PrintCommand.java
@@ -1,6 +1,15 @@
 package seedu.address.logic.commands;
 
+import java.util.List;
+
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.events.ui.JumpToListRequestEvent;
+import seedu.address.logic.PrintFormatter;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.patient.Patient;
+import seedu.address.model.patient.RecordList;
 
 /**
  * Selects a patient identified using it's last displayed index from the address book.
@@ -15,14 +24,38 @@ public class PrintCommand extends Command {
             + "Example: " + COMMAND_WORD + " INDEX";
 
     private final Index targetIndex;
+    private final PrintFormatter printFormatter;
+
+    private Patient patient;
 
     public PrintCommand(Index targetIndex) {
         this.targetIndex = targetIndex;
+        this.printFormatter = new PrintFormatter();
     }
 
     @Override
-    public CommandResult execute() {
-        return new CommandResult("Print records of patient " + targetIndex.getOneBased());
+    public CommandResult execute() throws CommandException {
+        List<Patient> lastShownList = model.getFilteredPersonList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        this.patient = lastShownList.get(targetIndex.getZeroBased());
+        formatRecords();
+
+        return new CommandResult("Printed records of patient " + targetIndex.getOneBased());
+    }
+
+    private void formatRecords () {
+        RecordList recordList = patient.getRecordList();
+        int recordListSize = recordList.getNumberOfRecords();
+
+        for (int recordIndex = 0; recordIndex < recordListSize; recordIndex++) {
+            // pass record to printFormatter
+            // test
+            System.out.println(recordList.getRecord(recordIndex).toString());
+        }
     }
 
     @Override

e3d0cbe9244b0e4d3501566e29181f64e7592067
ktingit <kting.it@gmail.com>
2018-04-04 01:18:40 +0800

    Collate code blocks

--- src/main/java/seedu/address/logic/commands/AddConditionCommand.java
+++ src/main/java/seedu/address/logic/commands/AddConditionCommand.java
@@ -1,3 +1,4 @@
+//@@author ktingit
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
--- src/main/java/seedu/address/logic/parser/AddConditionCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddConditionCommandParser.java
@@ -1,3 +1,4 @@
+//@@author ktingit
 package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;

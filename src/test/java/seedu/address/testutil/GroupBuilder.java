8ea6bafeac16a569a3b9ef01ee489a67ba4ee249
jas5469 <jas_p90@hotmail.com>
2018-04-03 20:17:55 +0800

    fixed checkstyle

--- src/test/java/seedu/address/testutil/GroupBuilder.java
+++ src/test/java/seedu/address/testutil/GroupBuilder.java
@@ -1,18 +1,8 @@
 package seedu.address.testutil;
 
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_DETAIL_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TIMETABLE_LINK_BOB;
-
 import seedu.address.model.group.Group;
 import seedu.address.model.group.Information;
-import seedu.address.model.person.Person;
 import seedu.address.model.person.UniquePersonList;
-import seedu.address.model.person.exceptions.DuplicatePersonException;
 
 /**
  * A utility class to help with building Group objects.

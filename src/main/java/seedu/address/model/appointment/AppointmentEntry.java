19377d57a40008a4d5e7867054792de7222b0ce5
Kyholmes <karen_tankaiyong@hotmail.com>
2018-04-03 14:56:58 +0800

    add new model

---
+++ src/main/java/seedu/address/model/appointment/AppointmentEntry.java
@@ -0,0 +1,24 @@
+package seedu.address.model.appointment;
+
+import com.calendarfx.model.Entry;
+
+/**
+ * Patient appointment in Imdb, displayed on calendar
+ * Gurantees: details are present and not null, field values are validated, immutable
+ */
+public class AppointmentEntry {
+    private static final int TIME_INTERVAL = 1800;
+    private Entry appointmentEntry;
+
+    public AppointmentEntry(Appointment appointment, String patientName) {
+        appointmentEntry = new Entry();
+        appointmentEntry.setTitle(patientName);
+        appointmentEntry.changeStartDate(appointment.getAppointmentDateTime().getLocalDate());
+        appointmentEntry.changeStartTime(appointment.getAppointmentDateTime().getLocalTime());
+        appointmentEntry.changeEndTime(appointment.getAppointmentDateTime().getEndLocalTime(TIME_INTERVAL));
+    }
+
+    public Entry getAppointmentEntry() {
+        return appointmentEntry;
+    }
+}

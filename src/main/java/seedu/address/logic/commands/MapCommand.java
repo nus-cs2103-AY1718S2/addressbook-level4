f9614891c3c1064c056a85628912acd389a6faef
Jing <ojyy73@gmail.com>
2018-04-03 15:15:55 +0800

    Add maps command and parser

--- src/main/java/seedu/address/logic/commands/MapCommand.java
+++ src/main/java/seedu/address/logic/commands/MapCommand.java
@@ -1,56 +1,58 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.model.building.Building.retrieveNusBuildingIfExist;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.events.ui.GoogleMapsEvent;
-import seedu.address.commons.events.ui.VenueTableEvent;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.building.Building;
-import seedu.address.model.building.exceptions.BuildingNotFoundException;
 
 /**
- * Retrieves all vacant rooms in a given building
+ * Launches Google Maps with specified location(s)
  */
 public class MapCommand extends Command {
     public static final String COMMAND_WORD = "map";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds the location of the specified address(es) \n"
-            + "Parameters: [LOCATION] or\n"
-            + "Parameters: [LOCATION]/[DESTINATION]\n"
+            + "Parameters: [ADDRESS] or \n"
+            + "Parameters: [ADDRESS_START]/[ADDRESS_DESTINATION] \n"
+            + "Example: " + COMMAND_WORD + " Tampines Mall/COM2 \n"
             + "Example: " + COMMAND_WORD + " 119077/117417 ";
 
-    public static final String MESSAGE_SUCCESS = "Google Maps is successful.";
-    public static final String MESSAGE_INVALID_BUILDING =
-            "Building is not in the list of NUS Buildings given below: \n"
-                    + Arrays.toString(Building.NUS_BUILDINGS);
+    public static final String MESSAGE_SUCCESS = "Launching Google Maps ...";
 
-    private final Building building;
+    private String locations;
+    private boolean isOneLocation;
 
     /**
-     * Creates a VacantCommand to retrieve all vacant rooms in a given building
+     * Creates a MapCommand to pass locations to Google Maps
      */
-    public MapCommand(Building building) {
-        requireNonNull(building);
-        this.building = building;
+    public MapCommand(String locations) {
+        requireNonNull(locations);
+        this.locations = locations;
     }
 
     @Override
     public CommandResult execute() throws CommandException {
         requireNonNull(model);
-        try {
-            ArrayList<ArrayList<String>> allRoomsSchedule = model.getAllRoomsSchedule(building);
-            ObservableList<ArrayList<String>> schedule = FXCollections.observableArrayList(allRoomsSchedule);
-            EventsCenter.getInstance().post(new GoogleMapsEvent());
-            return new CommandResult(String.format(MESSAGE_SUCCESS));
-        } catch (BuildingNotFoundException e) {
-            throw new CommandException(MESSAGE_INVALID_BUILDING);
+        String[] locationsArray = locations.split("/");
+        for (int i = 0; i < locationsArray.length; i++) {
+            locationsArray[i] = locationsArray[i].trim();
+            locationsArray[i] = retrieveNusBuildingIfExist(locationsArray[i]);
         }
+
+        if (locationsArray.length >= 2) {
+            locations = String.join("/", locationsArray);
+            isOneLocation = false;
+        } else {
+            locations = Arrays.toString(locationsArray);
+            isOneLocation = true;
+        }
+
+        EventsCenter.getInstance().post(new GoogleMapsEvent(locations, isOneLocation));
+        return new CommandResult(String.format(MESSAGE_SUCCESS));
 
     }
 
@@ -58,6 +60,6 @@ public class MapCommand extends Command {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof MapCommand // instanceof handles nulls
-                && building.equals(((MapCommand) other).building));
+                && locations.equals(((MapCommand) other).locations));
     }
 }
--- src/main/java/seedu/address/logic/parser/MapCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MapCommandParser.java
@@ -2,41 +2,26 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.MapCommand;
-import seedu.address.logic.commands.VacantCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.building.Building;
 
 /**
- * Parses input arguments and creates a new VacantCommand object
+ * Parses input arguments and creates a new MapCommand object
  */
 public class MapCommandParser implements Parser<MapCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the VacantCommand
-     * and returns a VacantCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the MapCommand
+     * and returns a MapCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public MapCommand parse(String args) throws ParseException {
         args = args.trim();
-        int length;
-        String[] buildingName = args.split("\\s+");
-        if ("".equals(args) || "\\s+".equals(args)) {
-            length = 0;
-        } else {
-            length = buildingName.length;
-        }
-        if (length != 1) {
+        if (args.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, MapCommand.MESSAGE_USAGE));
-        }
-
-        try {
-            Building building = ParserUtil.parseBuilding(buildingName[0]);
-            return new MapCommand(building);
-        } catch (IllegalValueException ive) {
-            throw new ParseException(ive.getMessage(), ive);
+        } else {
+            return new MapCommand(args);
         }
     }
 }

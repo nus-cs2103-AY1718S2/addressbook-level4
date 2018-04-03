17c83a01b056c4d21ce3f192a10e27ffd4d8c4a1
ktingit <kting.it@gmail.com>
2018-04-04 01:19:03 +0800

    Create DeleteConditionCommand and its parser

---
+++ src/main/java/seedu/address/logic/commands/DeleteConditionCommand.java
@@ -0,0 +1,287 @@
+//@@author ktingit
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.CollectionUtil;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.patient.Address;
+import seedu.address.model.patient.BloodType;
+import seedu.address.model.patient.DateOfBirth;
+import seedu.address.model.patient.Email;
+import seedu.address.model.patient.Name;
+import seedu.address.model.patient.Nric;
+import seedu.address.model.patient.Patient;
+import seedu.address.model.patient.Phone;
+import seedu.address.model.patient.RecordList;
+import seedu.address.model.patient.Remark;
+import seedu.address.model.patient.exceptions.DuplicatePatientException;
+import seedu.address.model.patient.exceptions.PatientNotFoundException;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Adds conditions to the list of conditions that an existing patient in the address book already has.
+ */
+public class DeleteConditionCommand extends UndoableCommand {
+
+    public static final String COMMAND_WORD = "delc";
+    public static final String COMMAND_ALIAS = "dc";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Deletes conditions from the list of conditions of the "
+            + "patient identified by the index number used in the last patient listing. "
+            + "If a condition does not exist, it will be ignored. \n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + PREFIX_TAG + "CONDITION...\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_TAG + "aspirin";
+
+    public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Patient: %1$s";
+    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This patient already exists in the address book.";
+
+    private final Index index;
+    private final EditPersonDescriptor editPersonDescriptor;
+
+    private Patient patientToEdit;
+    private Patient editedPatient;
+
+    /**
+     * @param index of the patient in the filtered patient list to edit
+     * @param editPersonDescriptor details to edit the patient with
+     */
+    public DeleteConditionCommand(Index index, EditPersonDescriptor editPersonDescriptor) {
+        requireNonNull(index);
+        requireNonNull(editPersonDescriptor);
+
+        this.index = index;
+        this.editPersonDescriptor = new EditPersonDescriptor(editPersonDescriptor);
+    }
+
+    @Override
+    public CommandResult executeUndoableCommand() throws CommandException {
+        try {
+            model.updatePerson(patientToEdit, editedPatient);
+        } catch (DuplicatePatientException dpe) {
+            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        } catch (PatientNotFoundException pnfe) {
+            throw new AssertionError("The target patient cannot be missing");
+        }
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, editedPatient));
+    }
+
+    @Override
+    protected void preprocessUndoableCommand() throws CommandException {
+        List<Patient> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        patientToEdit = lastShownList.get(index.getZeroBased());
+        editedPatient = createEditedPerson(patientToEdit, editPersonDescriptor);
+    }
+
+    /**
+     * Creates and returns a {@code Patient} with the details of {@code patientToEdit}
+     * edited with {@code editPersonDescriptor}.
+     */
+    private static Patient createEditedPerson(Patient patientToEdit, EditPersonDescriptor editPersonDescriptor) {
+        assert patientToEdit != null;
+
+        Name updatedName = editPersonDescriptor.getName().orElse(patientToEdit.getName());
+        Nric updatedNric = editPersonDescriptor.getNric().orElse(patientToEdit.getNric());
+        Phone updatedPhone = editPersonDescriptor.getPhone().orElse(patientToEdit.getPhone());
+        Email updatedEmail = editPersonDescriptor.getEmail().orElse(patientToEdit.getEmail());
+        Address updatedAddress = editPersonDescriptor.getAddress().orElse(patientToEdit.getAddress());
+        DateOfBirth updatedDob = editPersonDescriptor.getDob().orElse(patientToEdit.getDob());
+        BloodType updatedBloodType = editPersonDescriptor.getBloodType().orElse(patientToEdit.getBloodType());
+        Remark updatedRemark = patientToEdit.getRemark(); //edit command cannot change remarks
+        RecordList updatedRecord = patientToEdit.getRecordList(); //edit command cannot change record
+        Set<Tag> updatedTags = editPersonDescriptor.getTags().orElse(patientToEdit.getTags());
+        if (patientToEdit.getTags() != null) {
+            updatedTags = new HashSet<>();
+            updatedTags.addAll(patientToEdit.getTags());
+            updatedTags.removeAll(editPersonDescriptor.getModifiableTags());
+        }
+
+        return new Patient(updatedName, updatedNric, updatedPhone, updatedEmail, updatedAddress, updatedDob,
+                updatedBloodType, updatedRemark, updatedRecord, updatedTags, patientToEdit.getAppointments());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof DeleteConditionCommand)) {
+            return false;
+        }
+
+        // state check
+        DeleteConditionCommand e = (DeleteConditionCommand) other;
+        return index.equals(e.index)
+                && editPersonDescriptor.equals(e.editPersonDescriptor)
+                && Objects.equals(patientToEdit, e.patientToEdit);
+    }
+
+    /**
+     * Stores the details to edit the patient with. Each non-empty field value will replace the
+     * corresponding field value of the patient.
+     */
+    public static class EditPersonDescriptor {
+        private Name name;
+        private Nric nric;
+        private Phone phone;
+        private Email email;
+        private Address address;
+        private DateOfBirth dob;
+        private BloodType bloodType;
+        private Set<Tag> tags;
+
+        public EditPersonDescriptor() {}
+
+        /**
+         * Copy constructor.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public EditPersonDescriptor(EditPersonDescriptor toCopy) {
+            setName(toCopy.name);
+            setNric(toCopy.nric);
+            setPhone(toCopy.phone);
+            setEmail(toCopy.email);
+            setAddress(toCopy.address);
+            setDob(toCopy.dob);
+            setBloodType(toCopy.bloodType);
+            setTags(toCopy.tags);
+        }
+
+        /**
+         * Returns true if at least one field is edited.
+         */
+        public boolean isAnyFieldEdited() {
+            return CollectionUtil.isAnyNonNull(this.name, this.nric, this.phone, this.email,
+                    this.address, this.dob, this.bloodType, this.tags);
+        }
+
+        public void setName(Name name) {
+            this.name = name;
+        }
+
+        public Optional<Name> getName() {
+            return Optional.ofNullable(name);
+        }
+
+        public void setNric(Nric nric) {
+            this.nric = nric;
+        }
+
+        public Optional<Nric> getNric() {
+            return Optional.ofNullable(nric);
+        }
+
+        public void setPhone(Phone phone) {
+            this.phone = phone;
+        }
+
+        public Optional<Phone> getPhone() {
+            return Optional.ofNullable(phone);
+        }
+
+        public void setEmail(Email email) {
+            this.email = email;
+        }
+
+        public Optional<Email> getEmail() {
+            return Optional.ofNullable(email);
+        }
+
+        public void setAddress(Address address) {
+            this.address = address;
+        }
+
+        public Optional<Address> getAddress() {
+            return Optional.ofNullable(address);
+        }
+
+        public void setDob(DateOfBirth dob) {
+            this.dob = dob;
+        }
+
+        public Optional<DateOfBirth> getDob() {
+            return Optional.ofNullable(dob);
+        }
+
+        public void setBloodType(BloodType bloodType) {
+            this.bloodType = bloodType;
+        }
+
+        public Optional<BloodType> getBloodType() {
+            return Optional.ofNullable(bloodType);
+        }
+
+
+        /**
+         * Sets {@code tags} to this object's {@code tags}.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public void setTags(Set<Tag> tags) {
+            this.tags = (tags != null) ? new HashSet<>(tags) : null;
+        }
+
+        /**
+         * Returns an unmodifiable tag set, which throws {@code UnsupportedOperationException}
+         * if modification is attempted.
+         * Returns {@code Optional#empty()} if {@code tags} is null.
+         */
+        public Optional<Set<Tag>> getTags() {
+            return (tags != null) ? Optional.of(Collections.unmodifiableSet(tags)) : Optional.empty();
+        }
+
+        /**
+         * Returns a modifiable tag set for internal use
+         */
+        public Set<Tag> getModifiableTags() {
+            return tags;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            // short circuit if same object
+            if (other == this) {
+                return true;
+            }
+
+            // instanceof handles nulls
+            if (!(other instanceof EditPersonDescriptor)) {
+                return false;
+            }
+
+            // state check
+            EditPersonDescriptor e = (EditPersonDescriptor) other;
+
+            return getName().equals(e.getName())
+                    && getNric().equals(e.getNric())
+                    && getPhone().equals(e.getPhone())
+                    && getEmail().equals(e.getEmail())
+                    && getAddress().equals(e.getAddress())
+                    && getDob().equals(e.getDob())
+                    && getBloodType().equals(e.getBloodType())
+                    && getTags().equals(e.getTags());
+        }
+    }
+}

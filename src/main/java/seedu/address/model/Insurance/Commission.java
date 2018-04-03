ebd4fca31f3b16cd212f8ffa5d0dc610619c9fff
Sebry9 <e0031720@u.nus.edu>
2018-04-03 15:00:17 +0800

    created commission class

---
+++ src/main/java/seedu/address/model/Insurance/Commission.java
@@ -0,0 +1,71 @@
+package seedu.address.model.Insurance;
+
+
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents the commission recieved from a insurance plan in reInsurance.
+ * Guarantees: immutable;
+ */
+public class Commission {
+
+    private final Insurance insurance;
+
+    private final String commission;
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Commission(Insurance insurance) {
+        requireNonNull(insurance);
+        this.insurance = insurance;
+        String insuranceName = insurance.toString();
+        String commission = new String("0");
+        Pattern p = Pattern.compile("\\[(.*?)\\]");
+        Matcher m = p.matcher(insuranceName);
+
+        while (m.find()) {
+            if (commission.equals("0")) {
+                commission = m.group();
+            } else {
+                commission += m.group();
+            }
+        }
+        this.commission = commission;
+
+    }
+
+    public String getCommission() {
+        return commission;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Commission)) {
+            return false;
+        }
+
+        Commission otherCommission= (Commission) other;
+        return otherCommission.getCommission().equals(this.getCommission());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(commission);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getCommission());
+        return builder.toString();
+    }
+}

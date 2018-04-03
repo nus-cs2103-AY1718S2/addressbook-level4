222662ed6cc182fb51bc146443962984dc10d091
Kyholmes <karen_tankaiyong@hotmail.com>
2018-04-04 01:34:56 +0800

    add add appointment command test

---
+++ src/test/java/seedu/address/logic/commands/AddAppointmentCommandTest.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.testutil.TypicalPatients.getTypicalAddressBook;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+public class AddAppointmentCommandTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void constructor_nullObject_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        new AddAppointmentCommand(null, null, null);
+    }
+}

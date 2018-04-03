95b117e725da6e877d2acb91f5f33f4a233b38a0
wynonaK <35650845+wynonaK@users.noreply.github.com>
2018-04-04 03:41:54 +0800

    Implemented the logic for the find command for owners. Users can now find by owners: find -o n/NAME nr/NRIC, both together and individually. The list of pet patients tied to the owners shown are also automatically updated.

---
+++ src/main/java/seedu/address/model/person/NricContainsKeywordsPredicate.java
@@ -0,0 +1,32 @@
+package seedu.address.model.person;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+
+//@@Author wynonaK
+/**
+ * Tests that a {@code Person}'s {@code Nric} matches any of the keywords given.
+ */
+public class NricContainsKeywordsPredicate implements Predicate<Person> {
+    private final List<String> keywords;
+
+    public NricContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getNric().toString(), keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof NricContainsKeywordsPredicate // instanceof handles nulls
+                && this.keywords.equals(((NricContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
+}

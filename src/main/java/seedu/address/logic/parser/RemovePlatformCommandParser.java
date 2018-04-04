2682253401849ec67002b30ace0400e43949d52c
Tan Wei Jie <xwei_jiex@hotmail.com>
2018-04-04 23:54:57 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/logic/parser/RemovePlatformCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemovePlatformCommandParser.java
@@ -4,13 +4,13 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SOCIAL_MEDIA_PLATFORM;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.RemovePlatformCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-
-import java.util.HashSet;
-import java.util.Set;
 
 //@@author Nethergale
 /**

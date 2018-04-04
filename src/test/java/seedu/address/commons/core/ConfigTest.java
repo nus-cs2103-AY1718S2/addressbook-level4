2577df115342dc9233faf57209e77821f6282297
IzHoBX <hobingxuan97@gmail.com>
2018-04-04 12:08:06 +0800

    Updates test with the new app title

--- src/test/java/seedu/address/commons/core/ConfigTest.java
+++ src/test/java/seedu/address/commons/core/ConfigTest.java
@@ -14,7 +14,7 @@ public class ConfigTest {
 
     @Test
     public void toString_defaultObject_stringReturned() {
-        String defaultConfigAsString = "App title : E.T.\n"
+        String defaultConfigAsString = "App title : ET - Employees Tracker\n"
                 + "Current log level : INFO\n"
                 + "Preference file Location : preferences.json";
 

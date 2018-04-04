c834ed338d177fe57b213aec79b1b1da0db95ee6
Kyholmes <karen_tankaiyong@hotmail.com>
2018-04-04 11:40:16 +0800

    fix checkstyle error

--- src/test/java/seedu/address/logic/parser/AddAppointmentCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddAppointmentCommandParserTest.java
@@ -16,7 +16,8 @@ public class AddAppointmentCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddAppointmentCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                AddAppointmentCommand.MESSAGE_USAGE));
     }
 
     @Test

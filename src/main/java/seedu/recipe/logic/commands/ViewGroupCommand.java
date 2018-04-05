a54665c77a144f6937a80b38dd8f7ef08adc164f
hoangduong1607 <hoangduong1607@gmail.com>
2018-04-05 18:51:58 +0800

    View Group command

--- src/main/java/seedu/recipe/logic/commands/ViewGroupCommand.java
+++ src/main/java/seedu/recipe/logic/commands/ViewGroupCommand.java
@@ -20,7 +20,7 @@ public class ViewGroupCommand extends Command {
             + "Parameters: GROUP_NAME\n"
             + "Example: " + COMMAND_WORD + " Best";
     public static final String MESSAGE_FAILURE = "Recipe group not found!";
-    public static final String MESSAGE_SUCCESS = "Listed all recipes";
+    public static final String MESSAGE_SUCCESS = "Listed all recipes in [%s]";
 
     private GroupName groupName;
 
@@ -35,10 +35,18 @@ public class ViewGroupCommand extends Command {
         for (Recipe recipe : model.getRecipeBook().getRecipeList()) {
             if (recipe.getGroupNames().contains(groupName)) {
                 newFilteredRecipeList.add(recipe);
+                System.out.println(recipe);
             }
         }
 
         model.updateFilteredRecipeList(PREDICATE_SHOW_ALL_RECIPES);
-        return new CommandResult(MESSAGE_SUCCESS);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, groupName));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ViewGroupCommand // instanceof handles nulls
+                && this.groupName.equals(((ViewGroupCommand) other).groupName)); // state check
     }
 }

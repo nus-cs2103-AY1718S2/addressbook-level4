4cf4c024ab20fee04e450bbe824c748f04d6db1d
Chuang Cheng Heng <mukevin66@gmail.com>
2018-04-05 15:38:55 +0800

    SearchAllCommandParser.java: Add implementation of SearchAllCommandParser. The SearchAllCommandParser now performs the same task as SelectCommand, except that it returns a SearchAllCommand object instead of a SelectCommand Object.

--- src/main/java/seedu/address/logic/parser/SearchAllCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SearchAllCommandParser.java
@@ -1,4 +1,29 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.SearchAllCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new SearchAllCommand object
+ */
 public class SearchAllCommandParser {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the SearchAllCommand
+     * and returns an SearchAllCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public SearchAllCommand parse(String args) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            return new SearchAllCommand(index);
+        } catch (IllegalValueException ive) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, SearchAllCommand.MESSAGE_USAGE));
+        }
+    }
 }

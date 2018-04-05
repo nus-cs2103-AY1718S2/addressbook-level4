8a2a861594cab0310550567aabf014fd526d11f4
Chuang Cheng Heng <mukevin66@gmail.com>
2018-04-06 00:52:13 +0800

    SearchAllCommand.java: Implement the actual Implementation of the SearchAllCommand.

--- src/main/java/seedu/address/logic/commands/SearchAllCommand.java
+++ src/main/java/seedu/address/logic/commands/SearchAllCommand.java
@@ -1,13 +1,8 @@
 package seedu.address.logic.commands;
 
-import java.util.List;
-
 import seedu.address.commons.core.EventsCenter;
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
-import seedu.address.commons.events.ui.JumpToListRequestEvent;
+import seedu.address.commons.events.ui.SearchPersonOnAllPlatformEvent;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.person.Person;
 
 /**
  * Search for a person with the input name on all available social media platform.
@@ -23,23 +18,17 @@ public class SearchAllCommand extends Command {
 
     public static final String MESSAGE_SEARCH_PERSON_SUCCESS = "Searched Person: %1$s";
 
-    private final Index targetIndex;
+    private final String targetName;
 
-    public SearchAllCommand(Index targetIndex) {
-        this.targetIndex = targetIndex;
+    public SearchAllCommand(String targetName) {
+        this.targetName = targetName;
     }
 
     @Override
     public CommandResult execute() throws CommandException {
 
-        List<Person> lastShownList = model.getFilteredPersonList();
-
-        if (targetIndex.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-        }
-
-        EventsCenter.getInstance().post(new JumpToListRequestEvent(targetIndex));
-        return new CommandResult(String.format(MESSAGE_SEARCH_PERSON_SUCCESS, targetIndex.getOneBased()));
+        EventsCenter.getInstance().post(new SearchPersonOnAllPlatformEvent(targetName));
+        return new CommandResult(String.format(MESSAGE_SEARCH_PERSON_SUCCESS, targetName));
 
     }
 
@@ -47,6 +36,6 @@ public class SearchAllCommand extends Command {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof SelectCommand // instanceof handles nulls
-                && this.targetIndex.equals(((SearchAllCommand) other).targetIndex)); // state check
+                && this.targetName.equals(((SearchAllCommand) other).targetName)); // state check
     }
 }

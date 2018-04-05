9b354456ab17fbb5a11537cce351bc9ac980a7ef
Chuang Cheng Heng <mukevin66@gmail.com>
2018-04-06 00:51:38 +0800

    SearchPersonOnAllPlatformEvent.java: Create new SearchPersonOnAllPlatformEvent class for SearchAllCommand.

---
+++ src/main/java/seedu/address/commons/events/ui/SearchPersonOnAllPlatformEvent.java
@@ -0,0 +1,25 @@
+package seedu.address.commons.events.ui;
+
+import seedu.address.commons.events.BaseEvent;
+
+/**
+ * Represents a search for person on all available social media platforms.
+ */
+public class SearchPersonOnAllPlatformEvent extends BaseEvent{
+
+    private final String searchName;
+
+    public SearchPersonOnAllPlatformEvent(String searchName) {
+        this.searchName = searchName;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+
+    public String getSearchName() {
+        return searchName;
+    }
+
+}

d732ab9b65bc760e53793a2f5036e4a7fc1c05cf
Chu Qing Hao <6337103+qinghao1@users.noreply.github.com>
2018-04-06 15:41:55 +0800

    Edit MainApp

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -54,7 +54,7 @@ public class MainApp extends Application {
 
     @Override
     public void init() throws Exception {
-        logger.info("=============================[ Initializing AddressBook ]===========================");
+        logger.info("=============================[ Initializing Retail Analytics ]===========================");
         super.init();
 
         config = initConfig(getApplicationParameter("config"));
@@ -189,7 +189,7 @@ public class MainApp extends Application {
 
     @Override
     public void stop() {
-        logger.info("============================ [ Stopping Address Book ] =============================");
+        logger.info("============================ [ Stopping Retail Analytics ] =============================");
         ui.stop();
         try {
             storage.saveUserPrefs(userPrefs);

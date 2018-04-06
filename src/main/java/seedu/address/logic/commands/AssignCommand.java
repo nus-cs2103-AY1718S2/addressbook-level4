13b3e616e0eaca3c3d9a0b974adf96207bbd1eaf
jonleeyz <jonleeyz@outlook.com>
2018-04-06 11:13:13 +0800

    [AssignCommand.java] Fix checkstyle violation: remove redundant imports.

--- src/main/java/seedu/address/logic/commands/AssignCommand.java
+++ src/main/java/seedu/address/logic/commands/AssignCommand.java
@@ -2,12 +2,7 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.commands.EditCommand.MESSAGE_DUPLICATE_PERSON;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_CUSTOMERS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.ArrayList;

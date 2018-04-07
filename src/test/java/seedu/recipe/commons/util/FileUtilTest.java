e04b8a2b932448ee2ed7649906798b375f3ee3be
RyanAngJY <ryanang.jy@gmail.com>
2018-04-07 15:57:26 +0800

    remove unused import

--- src/test/java/seedu/recipe/commons/util/FileUtilTest.java
+++ src/test/java/seedu/recipe/commons/util/FileUtilTest.java
@@ -1,19 +1,12 @@
 package seedu.recipe.commons.util;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.io.IOException;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-
-import com.sun.source.tree.AssertTree;
-
-import seedu.recipe.testutil.Assert;
 
 public class FileUtilTest {
 

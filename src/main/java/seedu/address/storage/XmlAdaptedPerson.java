633976440ce160917ed6e9c40f645102727841c1
Lim Zheng Kai <limzk1994@hotmail.com>
2018-04-07 19:53:28 +0800

    Fixed Indentations

--- src/main/java/seedu/address/storage/XmlAdaptedPerson.java
+++ src/main/java/seedu/address/storage/XmlAdaptedPerson.java
@@ -10,11 +10,11 @@ import javax.xml.bind.annotation.XmlElement;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.Insurance.Insurance;
-import seedu.address.model.person.Group;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Appointment;
 import seedu.address.model.person.Birthday;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.Group;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;

7da17c71e3c2782d5408a75c21aee10c74bb9955
Caijun7 <lohcaijun@gmail.com>
2018-04-08 00:43:18 +0800

    Fix bug due to null password

--- src/main/java/seedu/address/storage/XmlAddressBookStorage.java
+++ src/main/java/seedu/address/storage/XmlAddressBookStorage.java
@@ -163,7 +163,9 @@ public class XmlAddressBookStorage implements AddressBookStorage {
         AddressBook addressBook = new AddressBook();
         addressBook.setPersons(filteredPersons);
         XmlFileStorage.saveDataToFile(file, new XmlSerializableAddressBook(addressBook));
-        SecurityUtil.encryptFile(file, password);
+        if (password != null) {
+            SecurityUtil.encryptFile(file, password);
+        }
     }
     //@@author
 

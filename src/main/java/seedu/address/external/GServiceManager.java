6eed744c8e0bd3e4dad04f54dcbdf3c4e76b9957
demitycho <tanweihao94@gmail.com>
2018-04-08 02:40:00 +0800

    Maybe just this

--- src/main/java/seedu/address/external/GServiceManager.java
+++ src/main/java/seedu/address/external/GServiceManager.java
@@ -1,5 +1,9 @@
 package seedu.address.external;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,10 +11,12 @@ import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.CalendarScopes;
 
 import seedu.address.external.exceptions.CredentialsException;
@@ -35,17 +41,24 @@ public class GServiceManager {
     private static final String CLIENT_ID = "126472549776-8cd9bk56sfubm9rkacjivecikppte982.apps.googleusercontent.com";
     private static final String CLIENT_SECRET = "nyBpzm1OjnKNZOd0-kT1uo7W";
 
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
     private static final JsonFactory JSON_FACTORY =
             JacksonFactory.getDefaultInstance();
 
     /** Global instance of the HTTP transport. */
     private static HttpTransport httpTransport;
 
+    private static GoogleClientSecrets clientSecrets;
+
+    private static final File DATA_STORE_DIR = new java.io.File(
+            "./credentials");
+
     private Credential credential;
 
     static {
         try {
             httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
         } catch (Throwable t) {
             t.printStackTrace();
         }
@@ -64,7 +77,7 @@ public class GServiceManager {
         // Build flow and trigger user authorization request.
         GoogleAuthorizationCodeFlow flow =
                 new GoogleAuthorizationCodeFlow.Builder(
-                        httpTransport, JSON_FACTORY, CLIENT_ID, CLIENT_SECRET, SCOPES)
+                        httpTransport, JSON_FACTORY, clientSecrets, SCOPES)
                         .setAccessType("offline")
                         .build();
         try {

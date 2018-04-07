5914582f234054742590a12592367b3e50eb51bd
melvintzw <melvintan123@gmail.com>
2018-04-08 01:19:47 +0800

    Modify delete command so that it ensures customers' or runners' associations are deleted when the customer or runner itself is deleted from addressbook. However, there is a bug with the undo command.

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.commands.EditCommand.createEditedPerson;
 
 import java.util.List;
 import java.util.Objects;
@@ -9,7 +10,10 @@ import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.customer.Customer;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
+import seedu.address.model.person.runner.Runner;
 
 /**
  * Deletes a person identified using it's last displayed index from the address book.
@@ -43,12 +47,23 @@ public class DeleteCommand extends UndoableCommand implements PopulatableCommand
 
 
     @Override
-    public CommandResult executeUndoableCommand() {
+    public CommandResult executeUndoableCommand() throws CommandException {
         requireNonNull(personToDelete);
         try {
+            //if personToDelete is customer, delete his associated runner's customer
+            if (personToDelete instanceof Customer) {
+                deleteAssocRunnersCustomer();
+            }
+            //if personToDelete is runner, delete all his customer's runner
+            if (personToDelete instanceof Runner) {
+                deleteAssocCustomersRunner();
+            }
+
             model.deletePerson(personToDelete);
         } catch (PersonNotFoundException pnfe) {
             throw new AssertionError("The target person cannot be missing");
+        } catch (DuplicatePersonException dpe) {
+            throw new AssertionError("duplicate person found");
         }
 
         return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
@@ -63,6 +78,75 @@ public class DeleteCommand extends UndoableCommand implements PopulatableCommand
         }
 
         personToDelete = lastShownList.get(targetIndex.getZeroBased());
+    }
+
+    /**
+     * Pre-condition: personToDelete is a Runner.
+     * This method finds associated customers from the addressbook and deletes those customer's runner
+     */
+    private void deleteAssocCustomersRunner() throws CommandException, DuplicatePersonException,
+            PersonNotFoundException {
+        assert personToDelete instanceof Runner;
+        List<Person> customers = ((Runner) personToDelete).getCustomers();
+        for (Person c : customers) {
+
+            EditCommand.EditPersonDescriptor custDesc = new EditCommand.EditPersonDescriptor();
+
+            custDesc.setRunner(new Runner());
+
+            custDesc.setName(c.getName());
+            custDesc.setPhone(c.getPhone());
+            custDesc.setEmail(c.getEmail());
+            custDesc.setAddress(c.getAddress());
+            custDesc.setTags(c.getTags());
+
+            custDesc.setMoneyBorrowed(((Customer) c).getMoneyBorrowed());
+            custDesc.setOweStartDate(((Customer) c).getOweStartDate());
+            custDesc.setOweDueDate(((Customer) c).getOweDueDate());
+            custDesc.setStandardInterest(((Customer) c).getStandardInterest());
+            custDesc.setLateInterest(((Customer) c).getLateInterest());
+
+            Person editedCust = createEditedPerson(c, custDesc);
+            model.updatePerson(c, editedCust);
+        }
+    }
+
+    /**
+     * Pre-condition: personToDelete is a Customer.
+     * This method finds this customer's associated Runner from the addressbook and deletes this customer from that
+     * runner.
+     */
+    private void deleteAssocRunnersCustomer() throws CommandException, DuplicatePersonException,
+            PersonNotFoundException {
+        assert personToDelete instanceof Customer;
+
+        List<Person> pl = model.getAddressBook().getPersonList();
+
+        Person r = ((Customer) personToDelete).getRunner(); //not getting a runner from pl but an incomplete copy
+        int indexOfActualRunner = pl.indexOf(r);
+
+        if (indexOfActualRunner >= 0) {
+            //the conditional check is necessary so that I'm only modifying valid existing runners
+
+            Person actualRunner = pl.get(indexOfActualRunner); //getting the actual complete runner from pl
+
+            //generate editPersonDescriptor with customer removed from runner's customer list
+            EditCommand.EditPersonDescriptor runnerDescWCustRemoved = new EditCommand.EditPersonDescriptor();
+
+            runnerDescWCustRemoved.setName(actualRunner.getName());
+            runnerDescWCustRemoved.setPhone(actualRunner.getPhone());
+            runnerDescWCustRemoved.setEmail(actualRunner.getEmail());
+            runnerDescWCustRemoved.setAddress(actualRunner.getAddress());
+            runnerDescWCustRemoved.setTags(actualRunner.getTags());
+
+            List<Person> newList = ((Runner) actualRunner).getCustomers();
+            newList.remove(personToDelete);
+            runnerDescWCustRemoved.setCustomers(newList);
+
+            Person editedRunner = createEditedPerson((Runner) actualRunner, runnerDescWCustRemoved);
+            model.updatePerson(actualRunner, editedRunner);
+        }
+
     }
 
     @Override

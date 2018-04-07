ea8dcc76354cc56b6472fb0bb6e502e3528e42ab
Lim Zheng Kai <limzk1994@hotmail.com>
2018-04-07 18:57:13 +0800

    Added group predicate

---
+++ src/main/java/seedu/address/model/person/PersonContainsGroupsPredicate.java
@@ -0,0 +1,32 @@
+package seedu.address.model.person;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+
+/**
+ * Tests that a {@code Person}'s {@code Group} matches any of the keywords given.
+ */
+public class PersonContainsGroupsPredicate implements Predicate<Group> {
+
+    private final List<String> keywords;
+
+    public PersonContainsGroupsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getGroup().groupName,keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof PersonContainsGroupsPredicate // instanceof handles nulls
+                && this.keywords.equals(((PersonContainsGroupsPredicate) other).keywords)); // state check
+    }
+}
+

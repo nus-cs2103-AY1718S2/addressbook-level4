560961312215151ecb0f4892bba12e9bbc0befc5
demitycho <tanweihao94@gmail.com>
2018-04-08 00:54:56 +0800

    Add code to check if Student group has been created in Contacts. If not, create a new Student group in Contacts

--- src/main/java/seedu/address/external/GContactsService.java
+++ src/main/java/seedu/address/external/GContactsService.java
@@ -5,8 +5,11 @@ import java.net.URL;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.gdata.client.contacts.ContactsService;
+import com.google.gdata.data.PlainTextConstruct;
 import com.google.gdata.data.contacts.ContactEntry;
 import com.google.gdata.data.contacts.ContactFeed;
+import com.google.gdata.data.contacts.ContactGroupEntry;
+import com.google.gdata.data.contacts.ContactGroupFeed;
 import com.google.gdata.data.contacts.GroupMembershipInfo;
 import com.google.gdata.data.extensions.Email;
 import com.google.gdata.data.extensions.FullName;
@@ -21,7 +24,9 @@ import seedu.address.model.student.Student;
  * Constructs a new GContactsService object to communicate with Google's APIs
  */
 public class GContactsService {
-    private Credential credential;
+    public static final String STRING_STUDENT_GROUP_NAME = "Students";
+    public static final String STRING_BASE_GROUP_ATOM_ID =
+            "http://www.google.com/m8/feeds/groups/codeducatorapp%40gmail.com/base/6";
 
     public GContactsService() {}
 
@@ -36,6 +41,8 @@ public class GContactsService {
     public Credential getCredential() {
         return credential;
     }
+
+    private Credential credential;
 
     /**
      * Converts a Student object to a ContactEntry for insertion
@@ -63,6 +70,9 @@ public class GContactsService {
         primaryMail.setPrimary(true);
         contact.addEmailAddress(primaryMail);
 
+        contact.addGroupMembershipInfo(
+                new GroupMembershipInfo(false, STRING_BASE_GROUP_ATOM_ID));
+
         return contact;
     }
 
@@ -77,96 +87,65 @@ public class GContactsService {
 
         ContactsService myService = new ContactsService(GServiceManager.APPLICATION_NAME);
         myService.setOAuth2Credentials(credential);
-        String groupAtomId = "http://www.google.com/m8/feeds/groups/codeducatorapp%40gmail.com/base/6";
         URL postUrl = new URL("https://www.google.com/m8/feeds/contacts/default/full");
-        printAllContacts(myService);
-//        for (Student student : addressBook.getStudentList()) {
-//            ContactEntry toBeInserted = studentToContactEntry(student);
-//            toBeInserted.addGroupMembershipInfo(new GroupMembershipInfo(false, groupAtomId));
-//
-//            myService.insert(postUrl, toBeInserted);
-//        }
+
+        ContactGroupEntry studentGroupEntry =
+                getStudentGroupEntry(myService) != null
+                        ? getStudentGroupEntry(myService)
+                        : createStudentGroupEntry(myService);
+
+
+        for (Student student : addressBook.getStudentList()) {
+            ContactEntry toBeInserted = studentToContactEntry(student);
+            toBeInserted.addGroupMembershipInfo(
+                    new GroupMembershipInfo(false, studentGroupEntry.getId()));
+
+            myService.insert(postUrl, toBeInserted);
+        }
     }
 
-    public static void printAllContacts(ContactsService myService)
+    /**
+     * Gets the Atomic Id {@code ContactGroupEntry.getId()} of the "Students" group
+     * that the contacts will be uploaded to
+     * @param myService
+     * @return
+     * @throws ServiceException
+     * @throws IOException
+     */
+    public static ContactGroupEntry getStudentGroupEntry(ContactsService myService)
             throws ServiceException, IOException {
         // Request the feed
-        URL feedUrl = new URL("https://www.google.com/m8/feeds/contacts/default/full");
-        ContactFeed resultFeed = myService.getFeed(feedUrl, ContactFeed.class);
-        System.out.println(resultFeed.getTitle().getPlainText());
-        for (ContactEntry entry : resultFeed.getEntries()) {
-            if (entry.hasName()) {
-                Name name = entry.getName();
-                if (name.hasFullName()) {
-                    String fullNameToDisplay = name.getFullName().getValue();
-                    if (name.getFullName().hasYomi()) {
-                        fullNameToDisplay += " (" + name.getFullName().getYomi() + ")";
-                    }
-                    System.out.println("\t\t" + fullNameToDisplay);
-                } else {
-                    System.out.println("\t\t (no full name found)");
-                }
-                if (name.hasNamePrefix()) {
-                    System.out.println("\t\t" + name.getNamePrefix().getValue());
-                } else {
-                    System.out.println("\t\t (no name prefix found)");
-                }
-                if (name.hasGivenName()) {
-                    String givenNameToDisplay = name.getGivenName().getValue();
-                    if (name.getGivenName().hasYomi()) {
-                        givenNameToDisplay += " (" + name.getGivenName().getYomi() + ")";
-                    }
-                    System.out.println("\t\t" + givenNameToDisplay);
-                } else {
-                    System.out.println("\t\t (no given name found)");
-                }
-                if (name.hasAdditionalName()) {
-                    String additionalNameToDisplay = name.getAdditionalName().getValue();
-                    if (name.getAdditionalName().hasYomi()) {
-                        additionalNameToDisplay += " (" + name.getAdditionalName().getYomi() + ")";
-                    }
-                    System.out.println("\t\t" + additionalNameToDisplay);
-                } else {
-                    System.out.println("\t\t (no additional name found)");
-                }
-                if (name.hasFamilyName()) {
-                    String familyNameToDisplay = name.getFamilyName().getValue();
-                    if (name.getFamilyName().hasYomi()) {
-                        familyNameToDisplay += " (" + name.getFamilyName().getYomi() + ")";
-                    }
-                    System.out.println("\t\t" + familyNameToDisplay);
-                } else {
-                    System.out.println("\t\t (no family name found)");
-                }
-                if (name.hasNameSuffix()) {
-                    System.out.println("\t\t" + name.getNameSuffix().getValue());
-                } else {
-                    System.out.println("\t\t (no name suffix found)");
-                }
-            } else {
-                System.out.println("\t (no name found)");
+        URL feedUrl = new URL("https://www.google.com/m8/feeds/groups/default/full");
+        ContactGroupFeed resultFeed = myService.getFeed(feedUrl, ContactGroupFeed.class);
+
+        for (ContactGroupEntry groupEntry : resultFeed.getEntries()) {
+            if (groupEntry.getTitle().getPlainText().equals(STRING_STUDENT_GROUP_NAME)) {
+                return groupEntry;
             }
-            System.out.println("Email addresses:");
-            for (Email email : entry.getEmailAddresses()) {
-                System.out.print(" " + email.getAddress());
-                if (email.getRel() != null) {
-                    System.out.print(" rel:" + email.getRel());
-                }
-                if (email.getLabel() != null) {
-                    System.out.print(" label:" + email.getLabel());
-                }
-                if (email.getPrimary()) {
-                    System.out.print(" (primary) ");
-                }
-                System.out.print("\n");
-            }
-            System.out.println("Groups:");
-            for (GroupMembershipInfo group : entry.getGroupMembershipInfos()) {
-                String groupHref = group.getHref();
-                System.out.println("  Id: " + groupHref);
-            }
-            System.out.println("Contact's ETag: " + entry.getEtag());
         }
+
+        return null;
+    }
+
+    /**
+     * Creates a new ContactGroupEntry called "Students"
+     * Gets the Atomic Id {@code ContactGroupEntry.getId()} that the contacts will be uploaded to
+     * @param myService
+     * @return
+     * @throws ServiceException
+     * @throws IOException
+     */
+    public static ContactGroupEntry createStudentGroupEntry(ContactsService myService)
+            throws ServiceException, IOException {
+        // Create the entry to insert
+        ContactGroupEntry newGroup = new ContactGroupEntry();
+        newGroup.setTitle(new PlainTextConstruct(STRING_STUDENT_GROUP_NAME));
+
+        // Ask the service to insert the new entry
+        URL postUrl = new URL("https://www.google.com/m8/feeds/groups/default/full");
+        ContactGroupEntry createdGroup = myService.insert(postUrl, newGroup);
+
+        return createdGroup;
     }
 }
 

a2a5f1b593ac658e32a36e25df2317e1f9e9d425
kush1509 <kushagra.goel.15@gmail.com>
2018-04-09 23:47:33 +0800

    add tests

--- src/test/java/seedu/address/logic/parser/job/JobEditCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/job/JobEditCommandParserTest.java
@@ -1,4 +1,4 @@
-package seedu.address.logic.parser;
+package seedu.address.logic.parser.job;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_LOCATION_DESC;

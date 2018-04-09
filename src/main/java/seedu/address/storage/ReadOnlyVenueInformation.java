6074f6a67072e706f07fda4f2eb1f2af9ba4079d
Caijun7 <lohcaijun@gmail.com>
2018-04-10 05:00:10 +0800

    Improve javadoc

--- src/main/java/seedu/address/storage/ReadOnlyVenueInformation.java
+++ src/main/java/seedu/address/storage/ReadOnlyVenueInformation.java
@@ -19,12 +19,20 @@ public interface ReadOnlyVenueInformation {
     String getVenueInformationFilePath();
 
     /**
-     * Returns VenueInformation data from storage.
-     *   Returns {@code Optional.empty()} if storage file is not found.
+     * Reads VenueInformation data from storage.
+     * Returns {@code Optional.empty()} if storage file is not found.
+     *
      * @throws DataConversionException if the data in storage is not in the expected format.
      * @throws IOException if there was any problem when reading from the storage.
      */
     Optional<Room> readVenueInformation() throws DataConversionException, IOException;
 
+    /**
+     * Reads BuildingsAndRooms data from storage.
+     * Returns {@code Optional.empty()} if storage file is not found.
+     *
+     * @throws DataConversionException if the data in storage is not in the expected format.
+     * @throws IOException if there was any problem when reading from the storage.
+     */
     Optional<Building> readBuildingsAndRoomsInformation() throws DataConversionException, IOException;
 }

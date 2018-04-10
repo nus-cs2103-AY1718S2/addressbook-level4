ec7cb31de9820192fa61ef8b02fc58ee4722268e
glorialaw <gloria.law@u.nus.edu>
2018-04-10 21:57:18 +0800

    Can open email now

---
+++ src/main/java/seedu/address/ui/OpenEmailWindow.java
@@ -0,0 +1,167 @@
+package seedu.address.ui;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import jdk.nashorn.internal.runtime.regexp.joni.exception.SyntaxException;
+import seedu.address.MainApp;
+
+/**
+ * Generates the full email that was received
+ */
+public class OpenEmailWindow {
+    private static final String openWindow = "/view/emailOpen.fxml";
+    private Stage puWindow = new Stage();
+
+    @FXML
+    private AnchorPane emailComposePopup;
+
+    @FXML
+    private Label fromContent;
+
+    @FXML
+    private Label subjectContent;
+
+    @FXML
+    private Label msgContent;
+
+    @FXML
+    private Button newEmailButton;
+
+    @FXML
+    private Button replyButton;
+
+    @FXML
+    private Button forwardButton;
+
+    @FXML
+    private Button closeButton;
+
+    /**
+     * Open the email message window
+     */
+    public OpenEmailWindow(String email, String subject, Message msg) throws IOException, SyntaxException {
+        //get URL
+        FXMLLoader fxmlLoader = loadScene(openWindow);
+        Parent root = (Parent) fxmlLoader.load();
+        fromContent.setText(email);
+        subjectContent.setText(subject);
+        setContent(msg);
+        puWindow.initModality(Modality.APPLICATION_MODAL);
+        puWindow.initStyle(StageStyle.UNDECORATED);
+        puWindow.setTitle("Compose Email");
+        puWindow.setScene(new Scene(root));
+        puWindow.show();
+    }
+
+    /**
+     * Parses the message content into a string.
+     * @param message is the email message packet
+     * @return String with message contents
+     */
+    private void setContent (Message message) {
+        String content = "";
+        try {
+            if (message.getContent() instanceof String) {
+                msgContent.setText((String) message.getContent());
+            } else {
+                Multipart multipart = (Multipart) message.getContent();
+                if (multipart.getCount() > 0) {
+                    int i;
+                    for( i = 0; i < multipart.getCount(); i++ ) {
+                        content += multipart.getBodyPart(i).getContent().toString();
+                    }
+                    msgContent.setText(content);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("ioexception");
+        } catch (MessagingException e) {
+            System.out.println("messaging exception");
+        }
+
+    }
+
+    /**
+     * Closes window with ESC key
+     */
+    @FXML
+    private void handleKeyPress(KeyEvent keyEvent) {
+        switch (keyEvent.getCode()) {
+            case ESCAPE:
+                puWindow.close();
+                break;
+            default:
+                // let JavaFx handle the keypress
+        }
+    }
+
+    /**
+     * opens a new email window
+     */
+    @FXML
+    private void openComposeWindow() {
+        try {
+            ComposeEmailWindow cew = new ComposeEmailWindow("");
+        } catch (IOException e) {
+            System.out.println("IOException");
+        }
+    }
+
+    /**
+     * opens a new reply email window
+     */
+    @FXML
+    private void openReplyWindow() {
+        try {
+            ComposeEmailWindow cew = new ComposeEmailWindow(this.fromContent.getText());
+        } catch (IOException e) {
+            System.out.println("IOException");
+        }
+    }
+
+    /**
+     * opens a new forward email window
+     */
+    @FXML
+    private void openForwardWindow() {
+        try {
+            ComposeEmailWindow cew = new ComposeEmailWindow("");
+        } catch (IOException e) {
+            System.out.println("IOException");
+        }
+    }
+
+    @FXML
+    private void closeWindow () {
+        puWindow.close();
+    }
+
+    /**
+     *loads the scene
+     */
+    private FXMLLoader loadScene (String fxml) {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        URL fxmlFileUrl = MainApp.class.getResource(fxml);
+        fxmlLoader.setLocation(fxmlFileUrl);
+        fxmlLoader.setController(this);
+        fxmlLoader.setRoot(null);
+        return fxmlLoader;
+    }
+
+}

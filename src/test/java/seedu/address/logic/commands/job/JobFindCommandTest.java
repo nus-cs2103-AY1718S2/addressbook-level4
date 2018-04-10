458e4a6525d419c2bafa0809ff1906320fc74f1e
kush1509 <kushagra.goel.15@gmail.com>
2018-04-11 05:49:14 +0800

    fix bug

--- src/test/java/seedu/address/logic/commands/job/JobFindCommandTest.java
+++ src/test/java/seedu/address/logic/commands/job/JobFindCommandTest.java
@@ -19,7 +19,7 @@ import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
-import seedu.address.logic.commands.job.JobFindCommand;
+import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;

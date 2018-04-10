3127f8bc561c02576d4de010f91c93b532333474
Kyholmes <karen_tankaiyong@hotmail.com>
2018-04-10 23:08:55 +0800

    collate code

--- src/main/java/seedu/address/commons/events/ui/ShowPatientAppointmentRequestEvent.java
+++ src/main/java/seedu/address/commons/events/ui/ShowPatientAppointmentRequestEvent.java
@@ -1,3 +1,4 @@
+//@@author Kyholmes
 package seedu.address.commons.events.ui;
 
 import seedu.address.commons.events.BaseEvent;
--- src/main/java/seedu/address/logic/commands/ViewAppointmentCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewAppointmentCommand.java
@@ -1,3 +1,4 @@
+//@@author Kyholmes
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;

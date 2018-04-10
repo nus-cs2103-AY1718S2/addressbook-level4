c79f2f0736547e1626719e7a189bb5f20afe70aa
jas5469 <jas_p90@hotmail.com>
2018-04-10 21:26:06 +0800

    Updated checkstyle

--- src/test/java/seedu/address/logic/commands/ListGroupMembersCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListGroupMembersCommandTest.java
@@ -60,7 +60,7 @@ public class ListGroupMembersCommandTest {
     }
 
     @Test
-    public void execute_GroupFound_EmptyList_Success() {
+    public void execute_GroupFoundEmptyListSuccess() {
 
         Group groupToList = new Group(new Information("Group A"));
         String expectedMessage = String.format("0 persons listed!");

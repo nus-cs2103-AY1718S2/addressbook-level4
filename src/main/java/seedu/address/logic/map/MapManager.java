ab35fbf557aba9fc232e0dfb5c442a2340f5ac93
jaronchan <jaron.chan.jin.jia@gmail.com>
2018-04-11 17:30:09 +0800

    Added functionality to remove directions marker on new navigate execution

--- src/main/java/seedu/address/logic/map/MapManager.java
+++ src/main/java/seedu/address/logic/map/MapManager.java
@@ -24,11 +24,13 @@ public class MapManager {
     private DirectionsService directionsService;
     private DirectionsRequest directionsRequest;
     private DirectionsRenderer directionsRenderer;
+    private DirectionsPane directionsPane;
 
     public MapManager(GoogleMap map, DirectionsPane directionsPane) {
         this.map = map;
         geocodingService = new GeocodingService();
         directionsService = new DirectionsService();
+        this.directionsPane = directionsPane;
         directionsRenderer = new DirectionsRenderer(true, map, directionsPane);
     }
 
@@ -44,7 +46,9 @@ public class MapManager {
      *  Sets the directions between addresses on the map.
      */
     public void setDirectionsOnMap(String addressOrigin, String addressDestination) {
+        directionsRenderer.clearDirections();
         directionsRequest = new DirectionsRequest(addressOrigin, addressDestination, TravelModes.DRIVING);
+        directionsRenderer = new DirectionsRenderer(true, map, directionsPane);
         directionsService.getRoute(directionsRequest, new MyDirectionsServiceCallback(map), directionsRenderer);
     }
 }

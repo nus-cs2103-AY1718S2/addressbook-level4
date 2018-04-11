62a4fe581e28bf99c32b5c7d7ce6fc9b0930f205
Nicholas Ang <nicholasang67@gmail.com>
2018-04-11 13:19:51 +0800

    Updated DG (#135)
    * updated dg
    
    * more updates
    
    * changed to DG
    
    * done changing to sentences for implementation part

--- src/main/java/seedu/recipe/ui/BrowserPanel.java
+++ src/main/java/seedu/recipe/ui/BrowserPanel.java
@@ -209,7 +209,7 @@ public class BrowserPanel extends UiPart<Region> {
     //@@author nicholasangcx
     @Subscribe
     private void handleUploadRecipesEvent(UploadRecipesEvent event) {
-        loadPageExternalBrowser(CloudStorageUtil.getAuthorizationUrl());
+        loadPage(CloudStorageUtil.getAuthorizationUrl());
     }
     //@@author
 }

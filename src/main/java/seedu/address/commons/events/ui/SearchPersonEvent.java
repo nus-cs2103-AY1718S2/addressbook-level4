445564c5dcd12804ec23de2068abb6f4e16afa88
Chuang Cheng Heng <mukevin66@gmail.com>
2018-04-11 21:10:55 +0800

    SearchPersonEvent.java: Implement the SearchPersonEvent

--- src/main/java/seedu/address/commons/events/ui/SearchPersonEvent.java
+++ src/main/java/seedu/address/commons/events/ui/SearchPersonEvent.java
@@ -1,4 +1,32 @@
 package seedu.address.commons.events.ui;
 
-public class SearchPersonEvent {
+import seedu.address.commons.events.BaseEvent;
+
+//@@author KevinChuangCH
+/**
+ * Represents a search for person.
+ */
+public class SearchPersonEvent extends BaseEvent {
+
+    private final String searchName;
+    private final String platformToSearch;
+
+    public SearchPersonEvent(String platformToSearch, String searchName) {
+        this.searchName = searchName;
+        this.platformToSearch = platformToSearch;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+
+    public String getSearchName() {
+        return searchName;
+    }
+
+    public String getPlatform() {
+        return platformToSearch;
+    }
+
 }

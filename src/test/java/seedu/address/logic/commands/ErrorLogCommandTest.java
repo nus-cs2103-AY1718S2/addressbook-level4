4b5c81c52030ba08d837cb16566fa210a451bf8b
Jason Manson-Hing <jason.mansonhing@gmail.com>
2018-04-12 03:37:55 +0800

    Added ErrorLog Command headless test

---
+++ src/test/java/seedu/address/logic/commands/ErrorLogCommandTest.java
@@ -0,0 +1,28 @@
+//@@author ifalluphill-reused
+//{Based on HelpCommandTest}
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.ErrorLogCommand.MESSAGE_SHOWING_ERRORLOG;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import seedu.address.commons.events.ui.ShowErrorsRequestEvent;
+import seedu.address.ui.testutil.EventsCollectorRule;
+
+public class ErrorLogCommandTest {
+    @Rule
+    public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
+
+    @Test
+    public void execute_help_success() {
+        CommandResult result = new ErrorLogCommand().execute();
+        assertEquals(MESSAGE_SHOWING_ERRORLOG, result.feedbackToUser);
+        assertTrue(eventsCollectorRule.eventsCollector.getMostRecent() instanceof ShowErrorsRequestEvent);
+        assertTrue(eventsCollectorRule.eventsCollector.getSize() == 1);
+    }
+}
+
+//@@author

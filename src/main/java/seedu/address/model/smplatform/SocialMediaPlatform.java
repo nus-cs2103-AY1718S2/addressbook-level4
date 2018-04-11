b3ed5944456d3fc1139486057e9e25b11bfe006a
Chuang Cheng Heng <mukevin66@gmail.com>
2018-04-11 21:14:35 +0800

    SocialMediaPlatform.java: Add MESSAGE_PLATFORM_CONSTRAINTS and isValidPlatform for checking of input platform in search command.

--- src/main/java/seedu/address/model/smplatform/SocialMediaPlatform.java
+++ src/main/java/seedu/address/model/smplatform/SocialMediaPlatform.java
@@ -5,9 +5,28 @@ package seedu.address.model.smplatform;
  * Represents a social media platform, which can take many forms.
  */
 public abstract class SocialMediaPlatform {
+
+    public static final String MESSAGE_PLATFORM_CONSTRAINTS =
+            "Platforms available are : \n"
+                    + "1) " + Facebook.PLATFORM_KEYWORD + " (alias: " + Facebook.PLATFORM_ALIAS + ")\n"
+                    + "2) " + Twitter.PLATFORM_KEYWORD + " (alias: " + Twitter.PLATFORM_ALIAS + ")\n";
+    
     protected Link link;
 
     public Link getLink() {
         return link;
+    }
+
+    /**
+     * Returns true if a given string is a valid social platform name.
+     */
+    public static boolean isValidPlatform(String test) {
+        if (test.equals(Facebook.PLATFORM_KEYWORD) ||
+                test.equals(Facebook.PLATFORM_ALIAS) ||
+                test.equals(Twitter.PLATFORM_KEYWORD) ||
+                test.equals(Twitter.PLATFORM_ALIAS)) {
+            return true;
+        }
+        return false;
     }
 }

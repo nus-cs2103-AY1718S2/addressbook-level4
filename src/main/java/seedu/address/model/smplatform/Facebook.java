a9c5762aad913015ca99b89aa99c8e1dcabd7662
Chuang Cheng Heng <mukevin66@gmail.com>
2018-04-11 21:15:38 +0800

    Facebook.java: Add PLATFORM_KEYWORD and PLATFORM_ALIAS to be use in a search person event.

--- src/main/java/seedu/address/model/smplatform/Facebook.java
+++ src/main/java/seedu/address/model/smplatform/Facebook.java
@@ -5,6 +5,11 @@ package seedu.address.model.smplatform;
  * Represents a facebook object.
  */
 public class Facebook extends SocialMediaPlatform {
+
+    public static final String PLATFORM_KEYWORD = "facebook";
+
+    public static final String PLATFORM_ALIAS = "fb";
+
     //Code adapted from https://stackoverflow.com/questions/5205652/facebook-profile-url-regular-expression
     public static final String LINK_VALIDATION_REGEX = "(?:https?:\\/\\/)?(?:www\\.|m\\.)?facebook\\.com\\/"
             + "(?:profile.php\\?id=(?=\\d.*))?"

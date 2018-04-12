9a009a770d11aeb68145602eb4fd6ea1c273a91c
ZhangYijiong <zhang.yijiong@dhs.sg>
2018-04-12 21:05:14 +0800

    Update AddOrderCommandTest

--- src/test/java/seedu/address/logic/commands/AddOrderCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddOrderCommandTest.java
@@ -22,6 +22,7 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.dish.exceptions.DishNotFoundException;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
@@ -142,6 +143,10 @@ public class AddOrderCommandTest {
             fail("This method should not be called.");
         }
 
+        @Override
+        public void checkOrder(Person target) throws DishNotFoundException{
+            fail("This method should not be called.");
+        }
 
         @Override
         public void addTask(Task task) throws DuplicateTaskException {

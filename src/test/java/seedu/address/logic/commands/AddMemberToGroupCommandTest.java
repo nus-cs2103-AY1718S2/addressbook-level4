9b4c721818f038ede60c5a4b6dd804833b0c0fd1
jas5469 <jas_p90@hotmail.com>
2018-04-12 19:29:21 +0800

    Checkstyle

--- src/test/java/seedu/address/logic/commands/AddMemberToGroupCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddMemberToGroupCommandTest.java
@@ -21,7 +21,6 @@ import javafx.collections.ObservableList;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;

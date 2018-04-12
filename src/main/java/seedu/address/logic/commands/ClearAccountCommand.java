60f28d7e8b8154b9a4a76bb6fb89eaacd759b0c9
victortardieu <tardieu97@gmail.com>
2018-04-12 15:12:19 +0800

    Fix Travis 3

--- src/main/java/seedu/address/logic/commands/ClearAccountCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearAccountCommand.java
@@ -4,13 +4,15 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.account.Account;
 import seedu.address.model.account.PrivilegeLevel;
-import static java.util.Objects.requireNonNull;
 import seedu.address.model.account.UniqueAccountList;
 import seedu.address.model.account.exceptions.DuplicateAccountException;
 
 
-public class ClearAccountCommand extends UndoableCommand{
+public class ClearAccountCommand extends UndoableCommand {
 
+    /**
+     * Checks if user is a librarian. If yes, it clears the list of accounts and logs out the current account.
+     */
     public static final String COMMAND_WORD = "cleara";
     public static final String MESSAGE_SUCCESS = "AccountList has been cleared, and you are logged out!";
     public static final PrivilegeLevel PRIVILEGE_LEVEL = Model.PRIVILEGE_LEVEL_LIBRARIAN;

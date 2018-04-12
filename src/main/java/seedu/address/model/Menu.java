3d1cb8e0cd0b41384faf404edcfda8cc5dd67a15
ZacZequn <zhangzequn96@gmail.com>
2018-04-13 00:16:25 +0800

    Change back to testing implementation

--- src/main/java/seedu/address/model/Menu.java
+++ src/main/java/seedu/address/model/Menu.java
@@ -1,10 +1,10 @@
 //@@author ZacZequn
 package seedu.address.model;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+//import java.io.File;
+//import java.io.FileNotFoundException;
 import java.util.HashMap;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 import seedu.address.model.dish.Dish;
 import seedu.address.model.dish.Name;
@@ -27,7 +27,9 @@ public class Menu implements ReadOnlyMenu {
      */
     {
         dishes = new HashMap<>();
-        File file = new File("data/menu.txt");
+        //only for testing
+        dishes.put("Chicken Rice", new Dish(new Name("Chicken Rice"), new Price("3")));
+        /*File file = new File("data/menu.txt");
         try {
             Scanner sc = new Scanner(file);
             while (sc.hasNextLine()) {
@@ -37,7 +39,7 @@ public class Menu implements ReadOnlyMenu {
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
     public Menu() {}

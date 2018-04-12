10e1ec7e4ebb887db99cfd7cad2832adcd0d91bb
jas5469 <jas_p90@hotmail.com>
2018-04-13 04:17:47 +0800

    Added Test Case for UniqueGroupList

--- src/test/java/seedu/address/model/UniqueGroupListTest.java
+++ src/test/java/seedu/address/model/UniqueGroupListTest.java
@@ -3,11 +3,15 @@ package seedu.address.model;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Iterator;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import seedu.address.model.group.Group;
 import seedu.address.model.group.UniqueGroupList;
+import seedu.address.testutil.GroupBuilder;
 
 public class UniqueGroupListTest {
     @Rule
@@ -32,6 +36,30 @@ public class UniqueGroupListTest {
         UniqueGroupList uniqueGroupList1 = new UniqueGroupList();
         UniqueGroupList uniqueGroupList2 = new UniqueGroupList();
         assertEquals(uniqueGroupList1, uniqueGroupList2);
+    }
 
+    @Test
+    public void set_sameGroup_Success() throws Exception {
+        UniqueGroupList uniqueGroupList1 = new UniqueGroupList();
+        UniqueGroupList uniqueGroupList2 = new UniqueGroupList();
+        Group group1 = new GroupBuilder().withInformation("Group A").build();
+        Group group2 = new GroupBuilder().withInformation("Group B").build();
+        uniqueGroupList1.add(group1);
+        uniqueGroupList2.add(group2);
+        uniqueGroupList1.setGroup(group1, group2);
+        assertEquals(uniqueGroupList1, uniqueGroupList2);
+    }
+
+    @Test
+    public void iterator() throws Exception {
+        UniqueGroupList uniqueGroupList1 = new UniqueGroupList();
+        UniqueGroupList uniqueGroupList2 = new UniqueGroupList();
+        uniqueGroupList1.add(new GroupBuilder().build());
+
+        Iterator<Group> iter = uniqueGroupList1.iterator();
+        while (iter.hasNext()) {
+            uniqueGroupList2.add(iter.next());
+        }
+        assertEquals(uniqueGroupList1, uniqueGroupList2);
     }
 }

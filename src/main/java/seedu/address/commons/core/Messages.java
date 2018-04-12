65d0dddacc9a4a79b279ab10806842be3eb21282
IzHoBX <hobingxuan97@gmail.com>
2018-04-12 23:13:24 +0800

    Makes the error message of dismiss command more consistent

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -9,7 +9,9 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The employee index provided is invalid";
     public static final String MESSAGE_INVALID_NOTIFICATION_CARD_INDEX = "The notification card index provided is "
-            + "invalid";
+            + "invalid. Please refer to index of notification cards in the Notification Center";
+    public static final String MESSAGE_INVALID_NOTIFICATION_CARD_INDEX_NON_POSITIVE = "The notification card index " +
+            "provided is invalid. Indexes must be more than zero, e.g. 1, 2, 3, ...";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d employees listed!";
 
 }

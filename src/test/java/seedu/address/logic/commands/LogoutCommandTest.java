61d052d7a52c1b8cc3b45f0a596f903ba870eb33
cxingkai <cxingkai@yahoo.com.sg>
2018-04-13 00:34:57 +0800

    Add test for LogoutCommand

---
+++ src/test/java/seedu/address/logic/commands/LogoutCommandTest.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import seedu.address.logic.login.LoginManager;
+
+public class LogoutCommandTest {
+
+    @Test
+    public void execute_alreadyLoggedIn_successfulLogout() {
+        LoginManager.authenticate("alice", "password123");
+
+        LogoutCommand logoutCommand = new LogoutCommand();
+        logoutCommand.execute();
+
+        assertEquals(LoginManager.getUserState(), LoginManager.NO_USER_STATE);
+    }
+
+    @Test
+    public void equals() {
+        LogoutCommand logoutCommand = new LogoutCommand();
+        LogoutCommand logoutCommandCopy = new LogoutCommand();
+
+        assertTrue(logoutCommand.equals(logoutCommandCopy));
+    }
+
+}

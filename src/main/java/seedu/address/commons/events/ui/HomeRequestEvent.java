343e59233f99488d2e59f28493ea51bcfb3f9907
jonleeyz <jonleeyz@outlook.com>
2018-04-12 22:36:19 +0800

    Add home ResultDisplay message.

--- src/main/java/seedu/address/commons/events/ui/HomeRequestEvent.java
+++ src/main/java/seedu/address/commons/events/ui/HomeRequestEvent.java
@@ -8,6 +8,12 @@ import seedu.address.commons.events.BaseEvent;
  * Indicates a request to execute the home command
  */
 public class HomeRequestEvent extends BaseEvent {
+    public static final String MESSAGE_HOME =
+            "Home view displayed. "
+            + "\n\n"
+            + "Utilise one of the keyboard shortcuts below to get started!"
+            + "\n"
+            + "Alternatively, press \"F12\" or type \"help\" to view the User Guide!";
 
     @Override
     public String toString() {

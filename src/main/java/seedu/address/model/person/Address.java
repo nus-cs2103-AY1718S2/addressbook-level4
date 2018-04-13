d77143cb1d8ce47393f39095bc2e02fde04ab4bd
jonleeyz <jonleeyz@outlook.com>
2018-04-13 20:26:00 +0800

    [ParserUtil.java] Tweak implementation: parseAddress().

--- src/main/java/seedu/address/model/person/Address.java
+++ src/main/java/seedu/address/model/person/Address.java
@@ -39,7 +39,7 @@ public class Address {
      * Returns true if a given string is a valid person email.
      */
     public static boolean isValidAddress(String test) {
-        return test.matches(ADDRESS_VALIDATION_REGEX) && !test.equals("");
+        return test.matches(ADDRESS_VALIDATION_REGEX);
     }
 
     @Override

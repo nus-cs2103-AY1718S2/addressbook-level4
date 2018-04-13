0deea0f9be05ae06fcb463e6e308793102f30dbf
Tan Wei Jie <xwei_jiex@hotmail.com>
2018-04-13 23:02:45 +0800

    Revert changes to parseUrl

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -147,12 +147,12 @@ public class BrowserPanel extends UiPart<Region> {
      */
     public static String parseUrl(String url) {
         if (!url.contains("://")) {
-            if (!url.contains("www.")) {
+            if (!url.contains("www")) {
                 return "https://www." + url;
             }
             return "https://" + url;
         } else {
-            if (!url.contains("www.")) {
+            if (!url.contains("www")) {
                 String[] splitUrl = url.split("://");
                 return "https://www." + splitUrl[1];
             }

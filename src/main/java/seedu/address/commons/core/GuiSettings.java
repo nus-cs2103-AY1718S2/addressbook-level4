b111fb665b43f7566cfd540e928724816daaec94
Jacqueline Cheong <jacquelinecjn@gmail.com>
2018-04-14 01:08:51 +0800

    Update application size

--- src/main/java/seedu/address/commons/core/GuiSettings.java
+++ src/main/java/seedu/address/commons/core/GuiSettings.java
@@ -9,8 +9,8 @@ import java.util.Objects;
  */
 public class GuiSettings implements Serializable {
 
-    private static final double DEFAULT_HEIGHT = 600;
-    private static final double DEFAULT_WIDTH = 740;
+    private static final double DEFAULT_HEIGHT = 1732;
+    private static final double DEFAULT_WIDTH = 1000;
     private static final String DEFAULT_THEME = "/view/LightTheme.css";
 
     private Double windowWidth;
@@ -44,7 +44,7 @@ public class GuiSettings implements Serializable {
         return windowCoordinates;
     }
 
-    public String getApplicationTheme() {
+    public String getCurrentTheme() {
         return currentTheme;
     }
 

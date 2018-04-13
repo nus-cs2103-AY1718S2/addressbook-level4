e9659e4c428fbb8d0796bf6740bb2b6ea5cce3c4
IzHoBX <hobingxuan97@gmail.com>
2018-04-14 02:29:57 +0800

    Adds tests for Notification

---
+++ src/test/java/seedu/address/model/notification/NotificationTimeTest.java
@@ -0,0 +1,29 @@
+package seedu.address.model.notification;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+
+import org.junit.Test;
+
+public class NotificationTimeTest {
+
+    @Test
+    public void isTodayTest_pastDate_fail() {
+        assertFalse((new NotificationTime(2014, 2, 1, 10, 12, 13)).isToday());
+    }
+
+    @Test
+    public void isTodayTest_todayDate_success() {
+        assertTrue((new NotificationTime(Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH),
+                12, 12, 12)).isToday());
+    }
+
+    @Test
+    public void isTodayTest_futureDate_fail() {
+        assertFalse((new NotificationTime(2024, 2, 1, 10, 12, 13)).isToday());
+    }
+
+}

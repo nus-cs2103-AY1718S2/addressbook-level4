c85f0026452d889ec15c7a683b5dee905e7ffd06
Kyholmes <karen_tankaiyong@hotmail.com>
2018-04-14 01:45:43 +0800

    edit comments

--- src/main/java/seedu/address/ui/QueueCard.java
+++ src/main/java/seedu/address/ui/QueueCard.java
@@ -7,7 +7,7 @@ import javafx.scene.layout.Region;
 import seedu.address.model.patient.Patient;
 
 /**
- * An UI component that displays information of a {@code queue}.
+ * An UI component that displays {@code Patient}'s name in the visiting queue.
  */
 public class QueueCard extends UiPart<Region> {
 

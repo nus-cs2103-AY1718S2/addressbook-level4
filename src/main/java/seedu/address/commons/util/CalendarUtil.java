788f0da5a219ea97ae90291ebcfc89abcf282718
Isaaaca <isaaaca@gmail.com>
2018-04-14 02:46:50 +0800

    Changed CalenderUtil to be testable

--- src/main/java/seedu/address/commons/util/CalendarUtil.java
+++ src/main/java/seedu/address/commons/util/CalendarUtil.java
@@ -2,32 +2,38 @@ package seedu.address.commons.util;
 
 import java.time.LocalDate;
 
+//@@author Isaaaca
 /**
  * A container for calendar related utility functions
  */
 public class CalendarUtil {
     private static final int JAN = 1;
     private static final int JUN = 6;
-    private static final int JUL = 7;
-    private static final int DEC = 12;
 
     private static LocalDate today = LocalDate.now();
 
     public static int getCurrentSemester() {
-        int currMonth = today.getMonthValue();
-        if (currMonth >= JAN && currMonth <= JUN) {
-            return 2;
-        } else if (currMonth >= JUL && currMonth <= DEC) {
-            return 1;
-        }
-        return 0;
+        return getSem(today);
     }
 
-    public static int getAcadYear() {
-        if (getCurrentSemester() == 1) {
-            return today.getYear();
+    public static int getSem(LocalDate localDate) {
+        int currMonth = localDate.getMonthValue();
+        if (currMonth >= JAN && currMonth <= JUN) {
+            return 2;
         } else {
-            return today.getYear() - 1;
+            return 1;
+        }
+    }
+
+    public static int getCurrAcadYear() {
+        return  getAcadYear(today);
+    }
+
+    public static int getAcadYear(LocalDate localDate) {
+        if (getSem(localDate) == 1) {
+            return localDate.getYear();
+        } else {
+            return localDate.getYear() - 1;
         }
     }
 }

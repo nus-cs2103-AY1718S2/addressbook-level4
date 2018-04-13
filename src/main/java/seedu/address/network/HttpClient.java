e8da19d6c71e475dd2d3ae92371f1c2eaa4d68b8
Qiu Siyu (Daniel) <danielteo@pm.me>
2018-04-13 15:36:31 +0800

    Add delay to makeGetRequest on event of exception (#183)

--- src/main/java/seedu/address/network/HttpClient.java
+++ src/main/java/seedu/address/network/HttpClient.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.logging.Logger;
 
 import org.asynchttpclient.AsyncHttpClient;
@@ -21,6 +22,7 @@ public class HttpClient {
     private static final int CONNECTION_TIMEOUT_MILLIS = 1000 * 10; // 10 seconds
     private static final int READ_TIMEOUT_MILLIS = 1000 * 10; // 10 seconds
     private static final int REQUEST_TIMEOUT_MILLIS = 1000 * 10; // 10 seconds
+    private static final long DELAY_ON_EXCEPTION = 300L;
 
     private final AsyncHttpClient asyncHttpClient;
 
@@ -44,7 +46,24 @@ public class HttpClient {
                 .prepareGet(url)
                 .execute()
                 .toCompletableFuture()
-                .thenApply(HttpResponse::new);
+                .thenApply(HttpResponse::new)
+                .handleAsync(HttpClient::waitOnException);
+    }
+
+    /**
+     * Waits for a short period of time in the event of exception.
+     * Prevents API methods from completing before necessary pre-processing is completed.
+     */
+    private static <T> T waitOnException(T result, Throwable error) {
+        if (error != null) {
+            try {
+                Thread.sleep(DELAY_ON_EXCEPTION);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+            throw new CompletionException(error);
+        }
+        return result;
     }
 
     /**

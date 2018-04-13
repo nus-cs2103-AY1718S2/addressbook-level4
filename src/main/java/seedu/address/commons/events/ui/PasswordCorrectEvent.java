a1d2e4f6c2063a3bf7585934393444f4d0c574b7
Yegg <yeggasd@gmail.com>
2018-04-13 16:03:06 +0800

    Fix comments for PasswordCorrectEvent

--- src/main/java/seedu/address/commons/events/ui/PasswordCorrectEvent.java
+++ src/main/java/seedu/address/commons/events/ui/PasswordCorrectEvent.java
@@ -4,7 +4,7 @@ import seedu.address.commons.events.BaseEvent;
 
 //@@author yeggasd
 /**
- * Indicates a request for App termination
+ * Indicates a request for App start for Correct Password
  */
 public class PasswordCorrectEvent extends BaseEvent {
 

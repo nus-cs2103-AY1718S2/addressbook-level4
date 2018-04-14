946785059a9cdf4fb25812ba84dc7796d263e832
XavierMaYuqian <32877925+XavierMaYuqian@users.noreply.github.com>
2018-04-14 23:30:00 +0800

    Update AddCommandTest.java

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -170,6 +170,19 @@ public class AddCommandTest {
             fail("This method should not be called.");
         }
 
+        //@@author XavierMaYuqian
+        @Override
+        public String getPassword() {
+            fail("This method should not be called.");
+            return null;
+        }
+
+        //@@author XavierMaYuqian
+        @Override
+        public void setPassword(String e) {
+            fail("This method should not be called.");
+        }
+
         @Override
         public void deleteTag(Tag tag) {
             fail("This method should not be called.");

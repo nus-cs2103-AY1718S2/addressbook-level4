86dbc91b6b252c3a319f0e993ea659152da35af0
Jason1im <ezstead@gmail.com>
2018-04-15 04:03:19 +0800

    fix errors

--- src/main/java/seedu/address/commons/events/model/AccountUpdateEvent.java
+++ src/main/java/seedu/address/commons/events/model/AccountUpdateEvent.java
@@ -3,6 +3,9 @@ package seedu.address.commons.events.model;
 import seedu.address.commons.events.BaseEvent;
 import seedu.address.model.Account;
 
+/**
+ * Indicates the user account has changed.
+ */
 public class AccountUpdateEvent extends BaseEvent {
     public final Account data;
 

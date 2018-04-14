fc28b825bdbf41c6913f918211867bad9fc660c6
ZacZequn <zhangzequn96@gmail.com>
2018-04-14 20:22:24 +0800

    Fix the variable names

--- src/test/java/seedu/address/testutil/TypicalDishes.java
+++ src/test/java/seedu/address/testutil/TypicalDishes.java
@@ -13,12 +13,12 @@ import seedu.address.model.dish.Dish;
  */
 public class TypicalDishes {
 
-    public static final Dish ChickenRice = new DishBuilder().withName("Chicken Rice").withPrice("3").build();
-    public static final Dish CurryChicken = new DishBuilder().withName("Curry Chicken").withPrice("4").build();
-    public static final Dish ChickenChop = new DishBuilder().withName("Chicken Chop").withPrice("5").build();
-    public static final Dish BanMian = new DishBuilder().withName("Ban Mian").withPrice("4").build();
-    public static final Dish IceMilo = new DishBuilder().withName("Ice Milo").withPrice("2").build();
-    public static final Dish Coffee = new DishBuilder().withName("Coffee").withPrice("2").build();
+    public static final Dish CHICKENRICE = new DishBuilder().withName("Chicken Rice").withPrice("3").build();
+    public static final Dish CURRYCHICKEN = new DishBuilder().withName("Curry Chicken").withPrice("4").build();
+    public static final Dish CHICKENCHOP = new DishBuilder().withName("Chicken Chop").withPrice("5").build();
+    public static final Dish BANMIAN = new DishBuilder().withName("Ban Mian").withPrice("4").build();
+    public static final Dish ICEMILO = new DishBuilder().withName("Ice Milo").withPrice("2").build();
+    public static final Dish COFFEE = new DishBuilder().withName("Coffee").withPrice("2").build();
 
 
     private TypicalDishes() {} // prevents instantiation
@@ -35,6 +35,6 @@ public class TypicalDishes {
     }
 
     public static List<Dish> getTypicalDishes() {
-        return new ArrayList<>(Arrays.asList(ChickenRice, CurryChicken, ChickenChop, BanMian, IceMilo, Coffee));
+        return new ArrayList<>(Arrays.asList(CHICKENRICE, CURRYCHICKEN, CHICKENCHOP, BANMIAN, ICEMILO, COFFEE));
     }
 }

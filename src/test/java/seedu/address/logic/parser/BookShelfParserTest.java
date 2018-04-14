e915fcc2505f869a02e0dec74c8307eaf0a422e7
Liu Jianghao <35656507+fishTT@users.noreply.github.com>
2018-04-14 21:14:32 +0800

    Add test for case-insensitive commands (#192)

--- src/test/java/seedu/address/logic/parser/BookShelfParserTest.java
+++ src/test/java/seedu/address/logic/parser/BookShelfParserTest.java
@@ -28,6 +28,7 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.HistoryCommand;
 import seedu.address.logic.commands.LibraryCommand;
+import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.ReviewsCommand;
 import seedu.address.logic.commands.UndoCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -154,6 +155,31 @@ public class BookShelfParserTest {
         assertTrue(parser.parseCommand(UndoCommand.COMMAND_WORD) instanceof UndoCommand);
         assertTrue(parser.parseCommand("undo 3") instanceof UndoCommand);
     }
+
+    //@@author fishTT
+    @Test
+    public void parseCommand_mixedCaseCommandWord_success() throws Exception {
+        /* Case: first character is uppercase */
+        char[] commandWord = ListCommand.COMMAND_WORD.toCharArray();
+        commandWord[0] = Character.toUpperCase(commandWord[0]);
+        String firstCharUppercaseCommand = String.copyValueOf(commandWord);
+        assertTrue(parser.parseCommand(firstCharUppercaseCommand) instanceof ListCommand);
+
+        /* Case: last character is uppercase */
+        commandWord[commandWord.length - 1] = Character.toUpperCase(commandWord[commandWord.length - 1]);
+        String lastCharUppercaseCommand = String.copyValueOf(commandWord);
+        assertTrue(parser.parseCommand(lastCharUppercaseCommand) instanceof ListCommand);
+
+        /* Case: middle character is uppercase */
+        commandWord[commandWord.length / 2] = Character.toUpperCase(commandWord[commandWord.length / 2]);
+        String middleCharUppercaseCommand = String.copyValueOf(commandWord);
+        assertTrue(parser.parseCommand(middleCharUppercaseCommand) instanceof ListCommand);
+
+        /* Case: all character is uppercase */
+        String allCharUppercaseCommand = ListCommand.COMMAND_WORD.toUpperCase();
+        assertTrue(parser.parseCommand(allCharUppercaseCommand) instanceof ListCommand);
+    }
+    //@@author
 
     @Test
     public void parseCommand_unrecognisedInput_throwsParseException() throws Exception {

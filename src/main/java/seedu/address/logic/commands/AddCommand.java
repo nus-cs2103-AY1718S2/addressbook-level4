10192e8cadc1cb31bed1a7b6a00261f9dd1fd4af
XavierMaYuqian <32877925+XavierMaYuqian@users.noreply.github.com>
2018-04-15 03:19:17 +0800

    Update AddCommand.java

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -37,7 +37,7 @@ public class AddCommand extends UndoableCommand {
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
             + PREFIX_TIMEZONE + "PST "
-            + PREFIX_COMMENT + "He likes music. "
+            + PREFIX_COMMENT + "NIL "
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney";
 

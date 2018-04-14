7e777f70c59d61815cdc76f0b2a9cbacb1290791
XavierMaYuqian <32877925+XavierMaYuqian@users.noreply.github.com>
2018-04-14 23:13:19 +0800

    Update AddressBook.java

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -31,6 +31,7 @@ public class AddressBook implements ReadOnlyAddressBook {
     private final UniquePersonList persons;
     private final UniqueTagList tags;
     private final UniqueAppointmentList appointments;
+    private String password;
 
     /*
      * The 'unusual' code block below is an non-static initialization block, sometimes used to avoid duplication
@@ -43,6 +44,7 @@ public class AddressBook implements ReadOnlyAddressBook {
         persons = new UniquePersonList();
         tags = new UniqueTagList();
         appointments = new UniqueAppointmentList();
+        password = "123456";
     }
 
     public AddressBook() {}
@@ -78,6 +80,7 @@ public class AddressBook implements ReadOnlyAddressBook {
         List<Person> syncedPersonList = newData.getPersonList().stream()
                 .map(this::syncWithMasterTagList)
                 .collect(Collectors.toList());
+        this.password = newData.getPassword();
 
         try {
             setPersons(syncedPersonList);
@@ -291,6 +294,17 @@ public class AddressBook implements ReadOnlyAddressBook {
             throw new AssertionError("Modifying a person's tags only should not result in a duplicate. "
                      + "See Person#equals(Object).");
         }
+    }
+
+    //@@author XavierMaYuqian
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    //@@author XavierMaYuqian
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     //// util methods

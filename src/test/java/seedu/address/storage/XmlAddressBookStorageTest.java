4a3ad84b41c664eb169ab9267400c85e185a80d8
Caijun7 <lohcaijun@gmail.com>
2018-04-14 17:52:08 +0800

    Update test password

--- src/test/java/seedu/address/storage/XmlAddressBookStorageTest.java
+++ src/test/java/seedu/address/storage/XmlAddressBookStorageTest.java
@@ -34,13 +34,13 @@ import seedu.address.testutil.AddressBookBuilder;
 
 public class XmlAddressBookStorageTest {
     private static final String TEST_DATA_FOLDER = FileUtil.getPath("./src/test/data/XmlAddressBookStorageTest/");
-    private static final String TEST_PASSWORD = "test";
+    private static final String TEST_PASSWORD = "test!!!!";
     private static final String TEST_DATA_FILE = FileUtil.getPath("src/test/data/sandbox/temp.xml");
     private static final String TEST_DATA_FILE_ALICE_BENSON = TEST_DATA_FOLDER + "aliceBensonAddressBook.xml";
     private static final String TEST_DATA_FILE_ALICE_BENSON_ENCRYPTED = TEST_DATA_FOLDER
-            + "encryptedAliceBensonAddressBook";
+            + "encryptedAliceBensonAddressBook.xml";
     private static final String TEST_DATA_FILE_ALICE_BENSON_ENCRYPTED_BACKUP = TEST_DATA_FOLDER
-            + "encryptedAliceBensonAddressBookBackup";
+            + "encryptedAliceBensonAddressBookBackup.xml";
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();

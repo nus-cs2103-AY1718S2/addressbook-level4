86de31d76e19ad67f6ecec7418d70c12e6021f21
zhangriqi <zhangruiqi@Riqis-MacBook.local>
2018-04-14 15:33:49 +0800

    Add more tests for LocateCommand

--- src/test/java/seedu/address/logic/commands/LocateCommandTest.java
+++ src/test/java/seedu/address/logic/commands/LocateCommandTest.java
@@ -4,11 +4,8 @@ package seedu.address.logic.commands;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
-import static seedu.address.logic.commands.LocateCommand.MESSAGE_LOCATE_SELECT;
-import static seedu.address.testutil.TypicalPersons.CARL;
-import static seedu.address.testutil.TypicalPersons.DANIEL;
-import static seedu.address.testutil.TypicalPersons.ELLE;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.util.Arrays;
@@ -22,16 +19,13 @@ import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.AddressContainsKeywordsPredicate;
-import seedu.address.model.person.EmailContainsKeywordsPredicate;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.PersonContainsKeywordsPredicate;
-import seedu.address.model.person.PhoneContainsKeywordsPredicate;
 import seedu.address.ui.testutil.EventsCollectorRule;
 
 public class LocateCommandTest {
@@ -67,7 +61,6 @@ public class LocateCommandTest {
         assertFalse(locateFirstCommand.equals(locateSecondCommand));
     }
 
-    //@@author zhangriqi-unused
     @Test
     public void execute_zeroKeywords_noPersonFound() {
         String arguments = " ";
@@ -78,50 +71,6 @@ public class LocateCommandTest {
         assertCommandSuccess(command, expectedMessage, Collections.emptyList());
     }
 
-    @Test
-    public void execute_multipleKeywords_multiplePersonsFound() {
-
-        int targetOne = 1;
-        //test LocateCommand object that uses the PersonContainsKeyWordsPredicate
-        String arguments = "carl daniel elle";
-        String[] splitArguments = arguments.split("\\s+");
-        List<String> list = Arrays.asList(splitArguments);
-        String expectedMessage = String.format(MESSAGE_LOCATE_SELECT, targetOne);
-        LocateCommand command = prepareCommand(new PersonContainsKeywordsPredicate(list));
-        assertCommandSuccess(command, expectedMessage, Arrays.asList(CARL, DANIEL, ELLE));
-
-        //test LocateCommand object that uses the NameContainsKeyWordsPredicate
-        arguments = "carl daniel elle";
-        splitArguments = arguments.split("\\s+");
-        list = Arrays.asList(splitArguments);
-        expectedMessage = String.format(MESSAGE_LOCATE_SELECT, targetOne);
-        command = prepareCommand(new NameContainsKeywordsPredicate(list));
-        assertCommandSuccess(command, expectedMessage, Arrays.asList(CARL, DANIEL, ELLE));
-
-        //test Command object that uses the PhoneContainsKeyWordsPredicate
-        arguments = "95352563 87652533 9482224";
-        splitArguments = arguments.split("\\s+");
-        list = Arrays.asList(splitArguments);
-        expectedMessage = String.format(MESSAGE_LOCATE_SELECT, targetOne);
-        command = prepareCommand(new PhoneContainsKeywordsPredicate(list));
-        assertCommandSuccess(command, expectedMessage, Arrays.asList(CARL, DANIEL, ELLE));
-
-        //test LocateCommand object that uses the EmailContainsKeyWordsPredicate
-        arguments = "heinz@example.com cornelia@example.com werner@example.com";
-        splitArguments = arguments.split("\\s+");
-        list = Arrays.asList(splitArguments);
-        expectedMessage = String.format(MESSAGE_LOCATE_SELECT, targetOne);
-        command = prepareCommand(new EmailContainsKeywordsPredicate(list));
-        assertCommandSuccess(command, expectedMessage, Arrays.asList(CARL, DANIEL, ELLE));
-
-        //test LocateCommand object that uses the AddressContainsKeyWordsPredicate
-        arguments = "wall 10th michegan";
-        splitArguments = arguments.split("\\s+");
-        list = Arrays.asList(splitArguments);
-        expectedMessage = String.format(MESSAGE_LOCATE_SELECT, targetOne);
-        command = prepareCommand(new AddressContainsKeywordsPredicate(list));
-        assertCommandSuccess(command, expectedMessage, Arrays.asList(CARL, DANIEL, ELLE));
-    }
     /**
      * Parses {@code userInput} into a {@code LocateCommand}.
      */
@@ -130,6 +79,28 @@ public class LocateCommandTest {
                 new LocateCommand(predicate);
         command.setData(model, new CommandHistory(), new UndoRedoStack());
         return command;
+    }
+
+    /**
+     * Executes a {@code SelectCommand} with the given {@code index},
+     * the command feedback is equal to {@code expectedMessage}<br>
+     * the {@code FilteredList<Person>} is equal to {@code expectedList}<br>
+     * the {@code AddressBook} in model remains the same after executing the {@code command}
+     * and checks that a {@code CommandException}
+     * is thrown with the {@code expectedMessage}.
+     */
+    private void assertExecutionFailure(LocateCommand command, String expectedMessage, List<Person> expectedList) {
+        String arguments = " ";
+        String[] splitArguments = arguments.split("\\s+");
+        List<String> list = Arrays.asList(splitArguments);
+        LocateCommand locateCommand = prepareCommand(new PersonContainsKeywordsPredicate(list));
+        try {
+            locateCommand.execute();
+            fail("The expected CommandException was not thrown.");
+        } catch (CommandException ce) {
+            assertEquals(expectedMessage, ce.getMessage());
+            assertTrue(eventsCollectorRule.eventsCollector.isEmpty());
+        }
     }
 
     /**

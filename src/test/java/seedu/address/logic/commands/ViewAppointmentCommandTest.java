283b007a00372fabec4468f564d3e4d4a2700312
Kyholmes <karen_tankaiyong@hotmail.com>
2018-04-15 00:00:35 +0800

    add more tests in view appointment command test

--- src/test/java/seedu/address/logic/commands/ViewAppointmentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewAppointmentCommandTest.java
@@ -4,18 +4,19 @@ package seedu.address.logic.commands;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PERSON;
 import static seedu.address.testutil.TypicalPatients.getTypicalAddressBook;
-
-import javax.swing.text.View;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.events.ui.ShowCalendarViewRequestEvent;
 import seedu.address.commons.events.ui.ShowPatientAppointmentRequestEvent;
@@ -64,6 +65,24 @@ public class ViewAppointmentCommandTest {
     }
 
     @Test
+    public void execute_listIsFilteredViewAppointmentWithValidIndex_success() {
+        showPersonAtIndex(model, INDEX_SECOND_PERSON);
+        assertExecutionSuccessWithIndex(INDEX_FIRST_PERSON);
+    }
+
+    @Test
+    public void execute_listIsNotFilteredViewAppointmentWithInvalidIndex_failure() {
+        Index outOfBoundsIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        assertExecutionFailure(outOfBoundsIndex, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_listIsFilteredViewAppointmentWithInvalidIndex_failure() {
+        showPersonAtIndex(model, INDEX_THIRD_PERSON);
+        assertExecutionFailure(INDEX_THIRD_PERSON, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
     public void equals() throws IllegalValueException {
         Index firstIndex = ParserUtil.parseIndex(INDEX_FIRST_PERSON.getOneBased() + "");
         Index secondIndex = ParserUtil.parseIndex(INDEX_SECOND_PERSON.getOneBased() + "");
@@ -76,7 +95,7 @@ public class ViewAppointmentCommandTest {
 
         //same values -> returns true
         ViewAppointmentCommand viewAppointmentFirstCommandCopy = new ViewAppointmentCommand(firstIndex);
-        assertTrue(viewAppointmentFirstCommand.equals(viewAppointmentFirstCommand));
+        assertTrue(viewAppointmentFirstCommand.equals(viewAppointmentFirstCommandCopy));
 
         //null -> returns false
         assertFalse(viewAppointmentFirstCommand.equals(null));
@@ -84,15 +103,6 @@ public class ViewAppointmentCommandTest {
         //different patient -> returns false
         assertFalse(viewAppointmentFirstCommand.equals(viewAppointmentSecondCommand));
     }
-
-//    @Test
-//    public void execute_patientNotExist_noPersonFound() throws Exception {
-//        ViewAppointmentCommand command = prepareCommand(INDEX_THIRD_PERSON);
-//        thrown.expect(CommandException.class);
-//        thrown.expectMessage(ViewAppointmentCommand.MESSAGE_PERSON_NOT_FOUND);
-//
-//        command.execute();
-//    }
 
     /**
      * Executes a {@code ViewAppointmentCommand} without index as parameter and checks that
@@ -113,7 +123,7 @@ public class ViewAppointmentCommandTest {
     }
 
     /**
-     * Executes a {@code ViewAppointmentCommand} with index as parameter and checks that
+     * Executes a {@code ViewAppointmentCommand} with the given {@code index} and checks that
      * {@code ShowPatientAppointmentRequestEvent} is raised with the patient appointments
      */
     private void assertExecutionSuccessWithIndex(Index index) {
@@ -129,6 +139,23 @@ public class ViewAppointmentCommandTest {
         ShowPatientAppointmentRequestEvent lastEvent = (ShowPatientAppointmentRequestEvent) eventsCollectorRule
                 .eventsCollector.getMostRecent();
         assertEquals(targetPatient, lastEvent.data);
+    }
+
+    /**
+     * Executes a {@code ViewAppointmentCommand} with the given {@code index}, and checks a {@code CommandException} is
+     * thrown with the {@code expectedMessage}.
+     */
+    private void assertExecutionFailure(Index index, String expectedMessage) {
+        ViewAppointmentCommand command = prepareCommand(index);
+
+        try {
+            command.execute();
+            fail("The expected CommandException was not thrown.");
+        } catch (CommandException ce) {
+            assertEquals(expectedMessage, ce.getMessage());
+            assertTrue(eventsCollectorRule.eventsCollector.isEmpty());
+        }
+
     }
 
     /**

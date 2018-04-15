8e3359aef6af7b94609d831e1583822c8b7a3e3d
jonleeyz <jonleeyz@outlook.com>
2018-04-15 17:08:46 +0800

    [StandardInterestTest.java] Add new tests.
    testToString_success(), testHashcode_symmetric().

---
+++ src/test/java/seedu/address/model/person/Customer/StandardInterestTest.java
@@ -0,0 +1,32 @@
+package seedu.address.model.person.Customer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+
+import seedu.address.model.person.customer.StandardInterest;
+
+//@@author jonleeyz
+public class StandardInterestTest {
+    @Test
+    public void testToString_success() {
+        assertEquals("9.71", new StandardInterest(9.71).toString());
+    }
+
+    @Test
+    public void testHashcode_symmetric() {
+        StandardInterest standardInterestA = new StandardInterest();
+        StandardInterest standardInterestB = new StandardInterest();
+        StandardInterest standardInterestC = new StandardInterest(9.71);
+        StandardInterest standardInterestD = new StandardInterest(9.71);
+
+        assertEquals(standardInterestA.hashCode(), standardInterestB.hashCode());
+        assertEquals(standardInterestC.hashCode(), standardInterestD.hashCode());
+        assertNotEquals(standardInterestA.hashCode(), standardInterestC.hashCode());
+        assertNotEquals(standardInterestA.hashCode(), standardInterestD.hashCode());
+        assertNotEquals(standardInterestB.hashCode(), standardInterestC.hashCode());
+        assertNotEquals(standardInterestB.hashCode(), standardInterestD.hashCode());
+    }
+}
+//@@author

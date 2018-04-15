a9b35265dd4e0097d7e0df535d364b97237761fc
jonleeyz <jonleeyz@outlook.com>
2018-04-15 17:08:31 +0800

    [MoneyBorrowedTest.java] Add new tests.
    testToString_success(), testHashcode_symmetric().

---
+++ src/test/java/seedu/address/model/person/Customer/MoneyBorrowedTest.java
@@ -0,0 +1,32 @@
+package seedu.address.model.person.Customer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+
+import seedu.address.model.person.customer.MoneyBorrowed;
+
+//@@author jonleeyz
+public class MoneyBorrowedTest {
+    @Test
+    public void testToString_success() {
+        assertEquals("9.71", new MoneyBorrowed(9.71).toString());
+    }
+
+    @Test
+    public void testHashcode_symmetric() {
+        MoneyBorrowed moneyBorrowedA = new MoneyBorrowed();
+        MoneyBorrowed moneyBorrowedB = new MoneyBorrowed();
+        MoneyBorrowed moneyBorrowedC = new MoneyBorrowed(9.71);
+        MoneyBorrowed moneyBorrowedD = new MoneyBorrowed(9.71);
+
+        assertEquals(moneyBorrowedA.hashCode(), moneyBorrowedB.hashCode());
+        assertEquals(moneyBorrowedC.hashCode(), moneyBorrowedD.hashCode());
+        assertNotEquals(moneyBorrowedA.hashCode(), moneyBorrowedC.hashCode());
+        assertNotEquals(moneyBorrowedA.hashCode(), moneyBorrowedD.hashCode());
+        assertNotEquals(moneyBorrowedB.hashCode(), moneyBorrowedC.hashCode());
+        assertNotEquals(moneyBorrowedB.hashCode(), moneyBorrowedD.hashCode());
+    }
+}
+//@@author

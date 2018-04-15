94a60d79ceb60e9483c7e3a1d1f869dc49a95b82
Damien <damien_sim@hotmail.com>
2018-04-15 15:16:20 +0800

    update GoogleWebServices

--- src/main/java/seedu/address/logic/map/GoogleWebServices.java
+++ src/main/java/seedu/address/logic/map/GoogleWebServices.java
@@ -24,20 +24,12 @@ public class GoogleWebServices {
             + "please wait till 3pm SGT for it to be reset";
 
     private static GeoApiContext context;
-    private String [] apiKeys;
     private boolean isInitialised;
 
     /**
      * Initialises access to google server using Api keys
      */
     public GoogleWebServices() {
-        apiKeys = new String [5];
-        apiKeys[0] = API_KEY_1;
-        apiKeys[1] = API_KEY_2;
-        apiKeys[2] = API_KEY_3;
-        apiKeys[3] = API_KEY_4;
-        apiKeys[4] = API_KEY_5;
-
         initialiseConnection();
     }
 
@@ -45,21 +37,16 @@ public class GoogleWebServices {
      * Initialise with valid Api key and test connection to google server
      */
     private void initialiseConnection() {
-        for (int i = 0; i < 5; i++) {
-            isInitialised = true;
-            context = new GeoApiContext.Builder()
-                    .apiKey(apiKeys[i])
-                    .build();
-            try {
-                GeocodingResult[] results = GeocodingApi.geocode(context,
-                        "Punggol").await();
-                LatLng location = results[0].geometry.location;
-            } catch (ApiException | InterruptedException | IOException | IndexOutOfBoundsException e) {
-                isInitialised = false;
-            }
-            if (isInitialised) {
-                break;
-            }
+        isInitialised = true;
+        context = new GeoApiContext.Builder()
+                .apiKey(API_KEY_1)
+                .build();
+        try {
+            GeocodingResult[] results = GeocodingApi.geocode(context,
+                    "Punggol").await();
+            LatLng location = results[0].geometry.location;
+        } catch (ApiException | InterruptedException | IOException | IndexOutOfBoundsException e) {
+            isInitialised = false;
         }
     }
 

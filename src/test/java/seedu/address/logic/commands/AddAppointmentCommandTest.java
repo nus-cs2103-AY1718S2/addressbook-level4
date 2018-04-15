cce1972a4ef483f97b7e983045ad6b582ad0b533
Kyholmes <karen_tankaiyong@hotmail.com>
2018-04-15 15:53:12 +0800

    add more tests in add appointment command test

--- src/test/java/seedu/address/logic/commands/AddAppointmentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddAppointmentCommandTest.java
@@ -2,20 +2,30 @@
 package seedu.address.logic.commands;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPatients.getTypicalAddressBook;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.ParserUtil;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.appointment.DateTime;
+import seedu.address.model.appointment.UniqueAppointmentList;
 
 public class AddAppointmentCommandTest {
     @Rule
@@ -27,6 +37,92 @@ public class AddAppointmentCommandTest {
     public void constructor_nullObject_throwsNullPointerException() {
         thrown.expect(NullPointerException.class);
         new AddAppointmentCommand(null, null);
+    }
+
+    @Test
+    public void execute_unfilteredList_addSuccessful() throws Exception {
+        Index testIndex = INDEX_FIRST_PERSON;
+        DateTime testDateTime = ParserUtil.parseDateTime("8/8/2018 1400");
+        AddAppointmentCommand command = prepareCommand(testIndex, testDateTime);
+        CommandResult commandResult = command.execute();
+        assertEquals(AddAppointmentCommand.MESSAGE_SUCCESS, commandResult.feedbackToUser);
+    }
+
+    @Test
+    public void execute_filteredList_addSuccessful() throws Exception {
+        showPersonAtIndex(model, INDEX_SECOND_PERSON);
+        Index testIndex = INDEX_FIRST_PERSON;
+        DateTime testDateTime = ParserUtil.parseDateTime("8/8/2018 1400");
+        AddAppointmentCommand command = prepareCommand(testIndex, testDateTime);
+        CommandResult commandResult = command.execute();
+        assertEquals(AddAppointmentCommand.MESSAGE_SUCCESS, commandResult.feedbackToUser);
+    }
+
+    @Test
+    public void execute_unfilteredListDuplicateAppointment_throwsCommandException() throws Exception {
+        Index testIndex = INDEX_FIRST_PERSON;
+        DateTime testDateTime = ParserUtil.parseDateTime("8/8/2018 1400");
+        model.addPatientAppointment(model.getPatientFromListByIndex(testIndex), testDateTime);
+        AddAppointmentCommand command = prepareCommand(testIndex, testDateTime);
+        thrown.expect(CommandException.class);
+        thrown.expectMessage(AddAppointmentCommand.MESSAGE_DUPLICATE_APPOINTMENT);
+        command.execute();
+    }
+
+    @Test
+    public void execute_unfilteredListDuplicateAppointmentMadeByOtherPatient_throwsCommandException() throws Exception {
+        Index testIndex = INDEX_FIRST_PERSON;
+        DateTime testDateTime = ParserUtil.parseDateTime("8/8/2018 1400");
+        model.addPatientAppointment(model.getPatientFromListByIndex(INDEX_SECOND_PERSON), testDateTime);
+        AddAppointmentCommand command = prepareCommand(testIndex, testDateTime);
+        thrown.expect(CommandException.class);
+        thrown.expectMessage(AddAppointmentCommand.MESSAGE_DUPLICATE_APPOINTMENT);
+        command.execute();
+    }
+
+    @Test
+    public void execute_filteredListDuplicateAppointment_throwsCommandException() throws Exception {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        Index testIndex = INDEX_FIRST_PERSON;
+        DateTime testDateTime = ParserUtil.parseDateTime("8/8/2018 1400");
+        model.addPatientAppointment(model.getPatientFromListByIndex(testIndex), testDateTime);
+        AddAppointmentCommand command = prepareCommand(testIndex, testDateTime);
+        thrown.expect(CommandException.class);
+        thrown.expectMessage(AddAppointmentCommand.MESSAGE_DUPLICATE_APPOINTMENT);
+        command.execute();
+    }
+
+    @Test
+    public void execute_filteredListDuplicateAppointmentMadeByOtherPatient_throwsCommandException() throws Exception {
+        Index testIndex = INDEX_FIRST_PERSON;
+        DateTime testDateTime = ParserUtil.parseDateTime("8/8/2018 1400");
+        model.addPatientAppointment(model.getPatientFromListByIndex(INDEX_SECOND_PERSON), testDateTime);
+        AddAppointmentCommand command = prepareCommand(testIndex, testDateTime);
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        thrown.expect(CommandException.class);
+        thrown.expectMessage(AddAppointmentCommand.MESSAGE_DUPLICATE_APPOINTMENT);
+        command.execute();
+    }
+
+    @Test
+    public void execute_unfilteredListInvalidIndex_throwsCommandException() throws Exception {
+        Index testIndex = ParserUtil.parseIndex(model.getFilteredPersonList().size() + 1 + "");
+        DateTime testDateTime = ParserUtil.parseDateTime("8/8/2018 1400");
+        AddAppointmentCommand command = prepareCommand(testIndex, testDateTime);
+        thrown.expect(CommandException.class);
+        thrown.expectMessage(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        command.execute();
+    }
+
+    @Test
+    public void execute_filteredListInvalidIndex_throwsCommandException() throws Exception {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        Index testIndex = INDEX_SECOND_PERSON;
+        DateTime testDateTime = ParserUtil.parseDateTime("8/8/2018 1400");
+        AddAppointmentCommand command = prepareCommand(testIndex, testDateTime);
+        thrown.expect(CommandException.class);
+        thrown.expectMessage(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        command.execute();
     }
 
     @Test
@@ -72,5 +168,11 @@ public class AddAppointmentCommandTest {
                 .equals(addAppointmentSecondIndexFirstDateTimeCommand));
         assertFalse(addAppointmentFirstIndexFirstDateTimeCommand
                 .equals(addAppointmentSecondIndexSecondDateTimeCommand));
+    }
+
+    private AddAppointmentCommand prepareCommand(Index index, DateTime dateTime) {
+        AddAppointmentCommand command = new AddAppointmentCommand(index, dateTime);
+        command.setData(model, new CommandHistory(), new UndoRedoStack());
+        return command;
     }
 }

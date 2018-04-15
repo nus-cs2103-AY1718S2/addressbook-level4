93bc64c3d96a48b7b3124920b55472cdbdf6698c
Yegg <yeggasd@gmail.com>
2018-04-15 16:37:48 +0800

    Make capitalize to only first letter Caps

--- src/main/java/seedu/address/commons/util/StringUtil.java
+++ src/main/java/seedu/address/commons/util/StringUtil.java
@@ -123,7 +123,7 @@ public class StringUtil {
      */
     public static String capitalize(String s) {
         requireNonNull(s);
-        return s.substring(0, 1).toUpperCase() + s.substring(1);
+        return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
     }
     //@@author
 }

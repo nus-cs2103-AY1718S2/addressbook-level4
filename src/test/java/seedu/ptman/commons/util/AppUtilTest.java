983681e30562aa84a64f8abaff3c353900c2791b
Caryn Heng <heng.zhixuan.caryn@dhs.sg>
2018-04-15 23:09:08 +0800

    Update DG and AboutUs (#243)
    * Update AboutUs
    
    * Remove irrelevant appendices in DG
    
    * Update jar name and icon

--- src/test/java/seedu/ptman/commons/util/AppUtilTest.java
+++ src/test/java/seedu/ptman/commons/util/AppUtilTest.java
@@ -15,7 +15,7 @@ public class AppUtilTest {
 
     @Test
     public void getImage_exitingImage() {
-        assertNotNull(AppUtil.getImage("/images/address_book_32.png"));
+        assertNotNull(AppUtil.getImage("/icons/ptman_logo_icon.png"));
     }
 
 
